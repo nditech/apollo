b0334f3149988a9ae44ef7d0a058660cb1b3d4a1
Tim Akinbo <takinbo@gmail.com>
2010-12-23 17:47:18 +0100

    added stubs for a couple of vr and dco views
    also added blocks in the layout

--- psc/urls.py
+++ psc/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    (r'^/?', views.home),
+    (r'^/?$', views.home),
+    (r'^vr/?$', views.vr_list),
+    (r'^vr/(?P<checklist_id>\d+)/?$', views.vr_checklist),
+    (r'^vr/in/(?P<incident_id>\d+)/?$', views.vr_incident),
+    (r'^dco/?$', views.dco_list),
+    (r'^dco/(?P<checklist_id>\d+)/?$', views.dco_checklist),
+    (r'^dco/in/(?P<incident_id>\d+)/?$', views.dco_incident),
 )
--- psc/views.py
+++ psc/views.py
@@ -1,5 +1,25 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 def home(request):
     return render_to_response('psc/layout.html')
+
+def vr_list(request):
+    return HttpResponse('OK')
+
+def vr_checklist(request, checklist_id=0):
+    return HttpResponse('OK')
+
+def vr_incident(request, incident_id=0):
+    return HttpResponse('OK')
+
+def dco_list(request):
+    return HttpResponse('OK')
+
+def dco_checklist(request, checklist_id=0):
+    return HttpResponse('OK')
+
+def dco_incident(request, incident_id=0):
+    return HttpResponse('OK')
+
