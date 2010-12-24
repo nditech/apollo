499a3592d6be9bd40fbe89a8569a352e399bd71d
Damilare Onajole <donajole@timbaobjects.com>
2010-12-24 07:16:07 +0100

    Added dco and vr forms and modified stylesheet

--- psc/views.py
+++ psc/views.py
@@ -9,17 +9,17 @@ def vr_list(request):
     return render_to_response('psc/psc_list.html')
 
 def vr_checklist(request, checklist_id=0):
-    return HttpResponse('OK')
+    return render_to_response('psc/vr_checklist_form.html', {'page_title': 'Voters Registration Checklist'})
 
 def vr_incident(request, incident_id=0):
-    return HttpResponse('OK')
+    return render_to_response('psc/vr_incident_form.html', {'page_title': 'Voters Registration Critrical Incidents'})
 
 def dco_list(request):
     return HttpResponse('OK')
 
 def dco_checklist(request, checklist_id=0):
-    return HttpResponse('OK')
+    return render_to_response('psc/dco_checklist_form.html', {'page_title': 'Display & Claims Checklist'})
 
 def dco_incident(request, incident_id=0):
-    return HttpResponse('OK')
+    return render_to_response('psc/dco_incident_form.html', {'page_title': 'Display & Claims Critical Incidents'})
 
