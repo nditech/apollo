999e9c157695ab623c97700e98f2b7896009c1ed
Damilare Onajole <donajole@timbaobjects.com>
2010-12-24 11:22:11 +0100

    Modified form  layouts to look more like real forms

--- psc/views.py
+++ psc/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
 
 def home(request):
     return render_to_response('psc/layout.html')
@@ -17,9 +18,13 @@ def vr_incident(request, incident_id=0):
 def dco_list(request):
     return HttpResponse('OK')
 
-def dco_checklist(request, checklist_id=0):
-    return render_to_response('psc/dco_checklist_form.html', {'page_title': 'Display & Claims Checklist'})
+def dco_checklist(request, checklist_id=0):   
+    if request.POST:
+        gotcha = request.POST['PSC'];
+        return HttpResponse(gotcha)
+    else:
+        return render_to_response('psc/dco_checklist_form.html', {'page_title': 'Display/Claims & Objections Checklist'}, context_instance=RequestContext(request))
 
 def dco_incident(request, incident_id=0):
-    return render_to_response('psc/dco_incident_form.html', {'page_title': 'Display & Claims Critical Incidents'})
+    return render_to_response('psc/dco_incident_form.html', {'page_title': 'Display/Claims & Objections Critical Incidents'})
 
