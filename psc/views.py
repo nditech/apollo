e4e5fe8a8441ff749256169fbe16128bc6474c4a
Tim Akinbo <takinbo@gmail.com>
2011-01-05 18:13:27 +0100

    typo fixes and a little rejigging of the views

--- psc/views.py
+++ psc/views.py
@@ -4,13 +4,12 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from models import *
-from datetime import datetime
 from django.db.models import Q
 from django.views.decorators.csrf import csrf_view_exempt
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from rapidsms.contrib.messagelog.tables import MessageTable
 from rapidsms.contrib.messagelog.models import Message
-from forms import VRChecklistForm, VRIncidentForm, DCOIncidentForm, VRChecklistFilterForm
+from forms import VRChecklistForm, VRIncidentForm, DCOIncidentForm, VRChecklistFilterForm, DCOChecklistFilterForm
 
 # paginator settings
 items_per_page = 25
@@ -38,71 +37,24 @@ def vr_checklist_list(request):
                 if int(data['status']) == 1 or int(data['status']) == 2: # no texts received or missing first text
                     qs &= Q(submitted=False)
                 elif int(data['status']) == 3: # missing second text
-                    qs &= Q(A__isnull=True) | \
-                          Q(B=0) | \
-                          Q(C__isnull=True) | \
-                          Q(F__isnull=True) | \
-                          Q(G=0) | \
-                          (Q(D1__isnull=True) & \
-                          Q(D2__isnull=True) & \
-                          Q(D3__isnull=True) & \
-                          Q(D4__isnull=True)) | \
-                          (Q(E1__isnull=True) & \
-                          Q(E2__isnull=True) & \
-                          Q(E3__isnull=True) & \
-                          Q(E4__isnull=True) & \
+                    qs &= Q(A__isnull=True) | Q(B=0) | Q(C__isnull=True) | Q(F__isnull=True) | Q(G=0) | \
+                          (Q(D1__isnull=True) & Q(D2__isnull=True) & Q(D3__isnull=True) & Q(D4__isnull=True)) | \
+                          (Q(E1__isnull=True) & Q(E2__isnull=True) & Q(E3__isnull=True) & Q(E4__isnull=True) & \
                           Q(E5__isnull=True))
                 elif int(data['status']) == 4: # missing third text
-                    qs &= Q(H__isnull=True) | \
-                          Q(J__isnull=True) | \
-                          Q(K__isnull=True) | \
-                          Q(M__isnull=True) | \
-                          Q(N__isnull=True) | \
-                          Q(P__isnull=True) | \
-                          Q(Q__isnull=True) | \
-                          Q(R__isnull=True) | \
-                          Q(S__isnull=True) | \
-                          Q(T=0) | \
-                          Q(U=0) | \
-                          Q(V=0) | \
-                          Q(W=0) | \
-                          Q(X=0) | \
-                          Q(Y__isnull=True) | \
-                          Q(Z__isnull=True) | \
-                          Q(AA__isnull=True)
+                    qs &= Q(H__isnull=True) | Q(J__isnull=True) | Q(K__isnull=True) | Q(M__isnull=True) | \
+                          Q(N__isnull=True) | Q(P__isnull=True) | Q(Q__isnull=True) | Q(R__isnull=True) | \
+                          Q(S__isnull=True) | Q(T=0) | Q(U=0) | Q(V=0) | Q(W=0) | Q(X=0) | Q(Y__isnull=True) | \
+                          Q(Z__isnull=True) | Q(AA__isnull=True)
                 elif int(data['status']) == 5: # missing any text
-                    qs &= Q(submitted=False) | \
-                          Q(A__isnull=True) | \
-                          Q(B=0) | \
-                          Q(C__isnull=True) | \
-                          Q(F__isnull=True) | \
-                          Q(G=0) | \
-                          (Q(D1__isnull=True) & \
-                          Q(D2__isnull=True) & \
-                          Q(D3__isnull=True) & \
-                          Q(D4__isnull=True)) | \
-                          (Q(E1__isnull=True) & \
-                          Q(E2__isnull=True) & \
-                          Q(E3__isnull=True) & \
-                          Q(E4__isnull=True) & \
-                          Q(E5__isnull=True)) | \
-                          Q(H__isnull=True) | \
-                          Q(J__isnull=True) | \
-                          Q(K__isnull=True) | \
-                          Q(M__isnull=True) | \
-                          Q(N__isnull=True) | \
-                          Q(P__isnull=True) | \
-                          Q(Q__isnull=True) | \
-                          Q(R__isnull=True) | \
-                          Q(S__isnull=True) | \
-                          Q(T=0) | \
-                          Q(U=0) | \
-                          Q(V=0) | \
-                          Q(W=0) | \
-                          Q(X=0) | \
-                          Q(Y__isnull=True) | \
-                          Q(Z__isnull=True) | \
-                          Q(AA__isnull=True)
+                    qs &= Q(submitted=False) | Q(A__isnull=True) | Q(B=0) | Q(C__isnull=True) | \
+                          Q(F__isnull=True) | Q(G=0) | \
+                          (Q(D1__isnull=True) & Q(D2__isnull=True) & Q(D3__isnull=True) & Q(D4__isnull=True)) | \
+                          (Q(E1__isnull=True) & Q(E2__isnull=True) & Q(E3__isnull=True) & Q(E4__isnull=True) & \
+                          Q(E5__isnull=True)) | Q(H__isnull=True) | Q(J__isnull=True) | Q(K__isnull=True) | \
+                          Q(M__isnull=True) | Q(N__isnull=True) | Q(P__isnull=True) | Q(Q__isnull=True) | \
+                          Q(R__isnull=True) | Q(S__isnull=True) | Q(T=0) | Q(U=0) | Q(V=0) | Q(W=0) | \
+                          Q(X=0) | Q(Y__isnull=True) | Q(Z__isnull=True) | Q(AA__isnull=True)
                 elif int(data['status']) == 6: # all texts received
                     qs &= Q(submitted=True,A__isnull=False,B__gte=1,C__isnull=False,F__isnull=False,G__gte=1) & \
                           Q(H__isnull=False,J__isnull=False,K__isnull=False,M__isnull=False,N__isnull=False) & \
@@ -137,6 +89,42 @@ def vr_checklist_list(request):
 
     return render_to_response('psc/vr_checklist_list.html', {'page_title': "Voter's Registration Data Management", 'checklists': checklists, 'filter_form': filter_form }, context_instance=RequestContext(request))
 
+def dco_checklist_list(request):
+    qs = Q()
+
+    if request.method == 'GET':
+        filter_form = DCOChecklistFilterForm(request.GET)
+
+        if filter_form.is_valid():
+            data = filter_form.cleaned_data
+            if data['zone']:
+                qs &= Q(observer__location_id__in=LGA.objects.filter(parent__parent__parent__code__iexact=data['zone']).values_list('id', flat=True))
+            if data['state']:
+                qs &= Q(observer__location_id__in=LGA.objects.filter(parent__parent__code__exact=data['state']).values_list('id', flat=True))
+            if data['district']:
+                qs &= Q(observer__location_id__in=LGA.objects.filter(parent__code__exact=data['district']).values_list('id', flat=True))
+            if data['day']:
+                qs &= Q(date=data['day'])
+            if data['observer_id']:
+                qs &= Q(observer__observer_id__exact=data['observer_id'])
+    else:
+        filter_form = DCOChecklistFilterForm()
+
+    paginator = Paginator(DCOChecklist.objects.filter(qs), items_per_page)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    # an invalid range will retrieve the last page of results
+    try:
+        checklists = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        checklists = paginator.page(paginator.num_pages)
+
+    return render_to_response('psc/dco_checklist_list.html', {'page_title': "Display, Claims & Objections Data Management", 'checklists': checklists, 'filter_form': filter_form }, context_instance=RequestContext(request))
+
 @csrf_view_exempt
 def vr_checklist(request, checklist_id=0):
     checklist = get_object_or_404(VRChecklist, pk=checklist_id)
@@ -149,6 +137,13 @@ def vr_checklist(request, checklist_id=0):
         f = VRChecklistForm(instance=checklist)
         return render_to_response('psc/vr_checklist_form.html', {'page_title': 'Voters Registration Checklist', 'checklist': checklist, 'form': f })
 
+def dco_checklist(request, checklist_id=0):   
+    if request.POST:
+        gotcha = request.POST['PSC'];
+        return HttpResponse(gotcha)
+    else:
+        return render_to_response('psc/dco_checklist_form.html', {'page_title': 'Display/Claims & Objections Checklist'}, context_instance=RequestContext(request))
+
 def vr_incident_update(request, incident_id=0):
     incident = get_object_or_404(VRIncident, pk=incident_id)
     if request.POST:        
@@ -159,10 +154,32 @@ def vr_incident_update(request, incident_id=0):
         return HttpResponseRedirect(reverse('psc.views.vr_incident_list'))    
     else:
         f = VRIncidentForm(instance=incident)   
-        return render_to_response('psc/vr_incident_update_form.html', {'page_title': 'Voters Registration Critrical Incidents', 'incident': incident, 'form': f })
+        return render_to_response('psc/vr_incident_update_form.html', {'page_title': 'Voters Registration Critrical Incident', 'incident': incident, 'form': f })
+
+@csrf_view_exempt
+def dco_incident_update(request, incident_id=0):
+    
+    incident = get_object_or_404(DCOIncident, pk=incident_id)
+    if request.POST:
+        f = DCOIncidentForm(request.POST, instance=incident)    
+        f.save()
+        return HttpResponseRedirect(reverse('psc.views.dco_incident_list'))
+    else:
+        f = DCOIncidentForm(instance=incident)
+        return render_to_response('psc/dco_incident_update_form.html', {'page_title': 'Display, Claims & Objections Critical Incident', 'incident': incident, 'form': f })
 
 def vr_incident_add(request):
     return render_to_response('psc/layout.html')
+
+@csrf_view_exempt
+def dco_incident_add(request):
+    if request.POST:
+        f = DCOIncidentForm(request.POST)                    
+        f.save()
+        return HttpResponseRedirect(reverse('psc.views.dco_incident_list'))
+    else:
+        f = DCOIncidentForm()
+        return render_to_response('psc/dco_incident_add_form.html', {'page_title': 'Voters Registration Critrical Incidents', 'form': f })
 
 def vr_incident_list(request):
     paginator = Paginator(VRIncident.objects.all(), items_per_page)
@@ -178,34 +195,7 @@ def vr_incident_list(request):
     except (EmptyPage, InvalidPage):
         checklists = paginator.page(paginator.num_pages)
 
-    return render_to_response('psc/vr_incident_list.html', {'page_title': "Voter's Registration Incidents", 'checklists': checklists})
-
-
-def dco_list(request):
-    paginator = Paginator(DCOChecklist.objects.all(), items_per_page)
-
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
-
-    # an invalid range will retrieve the last page of results
-    try:
-        checklists = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        checklists = paginator.page(paginator.num_pages)
-
-    return render_to_response('psc/dco_checklist_list.html', {'checklists': checklists})
-
-def dco_checklist(request, checklist_id=0):   
-    if request.POST:
-        gotcha = request.POST['PSC'];
-        return HttpResponse(gotcha)
-    else:
-        return render_to_response('psc/dco_checklist_form.html', {'page_title': 'Display/Claims & Objections Checklist'}, context_instance=RequestContext(request))
-
-def dco_incident(request, incident_id=0):
-    return render_to_response('psc/dco_incident_form.html', {'page_title': 'Display/Claims & Objections Critical Incidents'})
+    return render_to_response('psc/vr_incident_list.html', {'page_title': "Voter's Registration Critical Incidents", 'checklists': checklists})
 
 def dco_incident_list(request):
     paginator = Paginator(DCOIncident.objects.all(), items_per_page)
@@ -221,32 +211,13 @@ def dco_incident_list(request):
     except (EmptyPage, InvalidPage):
         checklists = paginator.page(paginator.num_pages)
 
-    return render_to_response('psc/dco_incident_list.html', {'page_title': "DCO Incidents", 'checklists': checklists})
+    return render_to_response('psc/dco_incident_list.html', {'page_title': "Display, Claims & Objections Critical Incidents", 'checklists': checklists})
 
-@csrf_view_exempt
-def dco_incident_update(request, incident_id=0):
-    
-    incident = get_object_or_404(DCOIncident, pk=incident_id)
-    if request.POST:
-        f = DCOIncidentForm(request.POST, instance=incident)    
-        f.save()
-        return HttpResponseRedirect(reverse('psc.views.dco_incident_list'))
-    else:
-        f = DCOIncidentForm(instance=incident)
-        return render_to_response('psc/dco_incident_update_form.html', {'page_title': 'DCO Incidents', 'incident': incident, 'form': f })
-
-@csrf_view_exempt
-def dco_incident_add(request):
-    if request.POST:
-        f = DCOIncidentForm(request.POST)                    
-        f.save()
-        return HttpResponseRedirect(reverse('psc.views.dco_incident_list'))
-    else:
-        f = DCOIncidentForm()
-        return render_to_response('psc/dco_incident_add_form.html', {'page_title': 'Voters Registration Critrical Incidents', 'form': f })
+def dco_incident(request, incident_id=0):
+    return render_to_response('psc/dco_incident_form.html', {'page_title': 'Display, Claims & Objections Critical Incident'})
 
 def message_log(request):
-    messages =   MessageTable(Message.objects.all(), request=request)
+    messages = MessageTable(Message.objects.all(), request=request)
     return render_to_response('psc/msg_log.html', { 'messages_list' : messages }, context_instance=RequestContext(request))
 
 def action_log(request):
@@ -255,8 +226,5 @@ def action_log(request):
     vr_incident_log = VRIncident.audit_log.all()
     dco_checklist_log = DCOChecklist.audit_log.all()
     dco_incident_log = DCOIncident.audit_log.all()
-
-    #all logs
-    #logs = {'vr_checklist_log_list' : vr_checklist_log, 'vr_incident_log_list': vr_incident_log, 'dco_checklist_log_list': dco_checklist_log, 'dco_incident_log_list': dco_incident_log}
 
     return render_to_response('psc/action_log.html', {'vr_checklist_log_list' : vr_checklist_log, 'vr_incident_log_list': vr_incident_log, 'dco_checklist_log_list': dco_checklist_log, 'dco_incident_log_list': dco_incident_log})
