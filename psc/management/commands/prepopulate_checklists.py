51808b9ceb988d88989dd0965d4d8a99f811330e
Tim Akinbo <takinbo@gmail.com>
2011-01-15 03:14:43 +0100

    bug fixes in the checklists prepopulation logic

--- psc/management/commands/prepopulate_checklists.py
+++ psc/management/commands/prepopulate_checklists.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from psc.models import VRChecklist, DCOChecklist, Observer, LGA
+from psc.models import VRChecklist, DCOChecklist, Observer, RegistrationCenter
 from django.contrib.contenttypes.models import ContentType
 from psc.forms import VR_DAYS, DCO_DAYS
 
@@ -9,16 +9,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # prepopulate vr checklists for LGA supervisors
         vr_reports_created = dco_reports_created = 0
-        lga_supervisors = Observer.objects.filter(location_type=ContentType.objects.get_for_model(LGA))
+        lga_supervisors = Observer.objects.filter(role__iexact='LGA')
         for ls in lga_supervisors:
             for day in VR_DAYS:
                 if day[0]:
                     report_date = day[0]
                     # check if there's a report already created for the observer
+                    lga = ls.location
+                    # set default location
+                    location = RegistrationCenter.objects.get(parent=lga,code__exact="999")
                     try:
                         vr = VRChecklist.objects.get(date=report_date, observer=ls)
                     except VRChecklist.DoesNotExist:
                         vr = VRChecklist(date=report_date, observer=ls)
+                        vr.location_type = ContentType.objects.get_for_model(location)
+                        vr.location_id = location.pk
                         vr.save()
                         vr_reports_created += 1
 
@@ -26,12 +31,17 @@ class Command(BaseCommand):
                 if day[0]:
                     report_date = day[0]
                     # check if there's a report already created for the observer
+                    lga = ls.location
+                    # set default location
+                    location = RegistrationCenter.objects.get(parent=lga,code__exact="999")
                     counter = 1
                     while counter <= 3:
                         try:
                             dco = DCOChecklist.objects.get(date=report_date, observer=ls, sms_serial=counter)
                         except DCOChecklist.DoesNotExist:
                             dco = DCOChecklist(date=report_date, observer=ls, sms_serial=counter)
+                            dco.location_type = ContentType.objects.get_for_model(location)
+                            dco.location_id = location.pk
                             dco.save()
                             dco_reports_created += 1
                         counter += 1
