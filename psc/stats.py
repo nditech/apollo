dbec692c35b7cc4be3e77fd7b02bad9e61ba16d6
Damilare Onajole <donajole@timbaobjects.com>
2011-01-18 20:07:31 +0100

    Fine tuned the almight model_sieve

--- psc/stats.py
+++ psc/stats.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from django.db import models
 
-def get_models_fields_exists(model, fields):
+def model_sieve(model, fields, exclude=False):
     if issubclass(model, models.Model):         
         def process_fields(fields, cond='AND'):
             query = Q()
@@ -10,7 +10,9 @@ def get_models_fields_exists(model, fields):
                     query &= process_fields(field, 'OR')
                 else:
                     if cond == 'AND':
-                        if issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField) and \
+                        if type(field) == tuple:
+                            query &= eval('Q(%s=%s)' % field)
+                        elif issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField) and \
                             type(model._meta.get_field_by_name(field)[0].default) == int:
                             query &= eval('~Q(%s=%d)' % (field, model._meta.get_field_by_name(field)[0].default))
                         elif issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField):
@@ -20,7 +22,9 @@ def get_models_fields_exists(model, fields):
                         else:
                             query &= eval('Q(%s__isnull=False)' % field)
                     else:
-                        if issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField) and \
+                        if type(field) == tuple:
+                            query &= eval('Q(%s=%s)' % field)
+                        elif issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField) and \
                             type(model._meta.get_field_by_name(field)[0].default) == int:
                             query |= eval('~Q(%s=%d)' % (field, model._meta.get_field_by_name(field)[0].default))
                         elif issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField):
@@ -31,23 +35,8 @@ def get_models_fields_exists(model, fields):
                             query |= eval('Q(%s__isnull=False)' % field)
             return query
         
-        return model.objects.filter(process_fields(fields))
+        if exclude:
+            return model.objects.exclude(process_fields(fields))
+        else:
+            return model.objects.filter(process_fields(fields))
 
-def get_models_fields_missing(model, fields):
-    if issubclass(model, models.Model):
-        def process_fields(fields):
-            query = Q()
-            for field in fields:
-                if type(field) == 'list':
-                    process_fields(field)
-                else:
-                    if issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.IntegerField) and \
-                        type(model._meta.get_field_by_name(field)[0].default) == int:
-                        query &= eval('Q(%s=%d)' % (field, model._meta.get_field_by_name(field)[0].default))
-                    elif issubclass(model._meta.get_field_by_name(field)[0].__class__, models.fields.BooleanField):
-                        query &= eval('Q(%s=False)' % field)
-                    else:
-                        query &= eval('Q(%s__isnull=True)' % field)
-            return query
-        
-        return model.objects.filter(process_fields(fields))
