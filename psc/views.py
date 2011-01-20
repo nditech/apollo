8bd1afa6ff0a4cb7c4068ebb19c41aac36670696
Tim Akinbo <takinbo@timbaobjects.com>
2011-01-20 11:34:52 +0100

    updating computation for figures for the third sms on the dashboard

--- psc/views.py
+++ psc/views.py
@@ -37,6 +37,21 @@ def home(request):
     context['complete_third_sms'] += stats.model_sieve(VRChecklist, ['H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', ('A', 4), ('verified_third', True)]).filter(date=datetime.date(datetime.today())).count()
     context['missing_third_sms'] = context['complete_second_sms'] - context['complete_third_sms'] 
     context['blank_third_sms'] = stats.model_sieve(VRChecklist, ['H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA'], exclude=True).filter(A=4).filter(date=datetime.date(datetime.today())).count()
+    third_partial_qs_include = Q(H__isnull=False) | Q(J__isnull=False) | Q(K__isnull=False) | Q(M__isnull=False) | \
+          Q(N__isnull=False) | Q(P__isnull=False) | Q(Q__isnull=False) | Q(R__isnull=False) | \
+          Q(S__isnull=False) | Q(T__gt=0) & Q(U__gt=0) | Q(V__gt=0) | Q(W__gt=0) | Q(X__gt=0) | Q(Y__isnull=False) | \
+          Q(Z__isnull=False) | Q(AA__isnull=False) 
+    third_partial_qs_exclude = ~Q(A=4) & Q(H__isnull=False) & Q(J__isnull=False) & Q(K__isnull=False) & Q(M__isnull=False) & \
+          Q(N__isnull=False) & Q(P__isnull=False) & Q(Q__isnull=False) & Q(R__isnull=False) & \
+          Q(S__isnull=False) & Q(T__gt=0) & Q(U__gt=0) & Q(V__gt=0) & Q(W__gt=0) & Q(X__gt=0) & Q(Y__isnull=False) & \
+          Q(Z__isnull=False) & Q(AA__isnull=False) | Q(A=4)
+    context['partial_third_sms'] = VRChecklist.objects.filter(date=datetime.date(datetime.today())).filter(third_partial_qs_include).exclude(third_partial_qs_exclude).count()
+    third_unverified_qs = Q(A=4) & Q(verified_third=False) & (Q(H__isnull=False) | Q(J__isnull=False) | Q(K__isnull=False) | Q(M__isnull=False) | \
+          Q(N__isnull=False) | Q(P__isnull=False) | Q(Q__isnull=False) | Q(R__isnull=False) | \
+          Q(S__isnull=False) | Q(T__gt=0) & Q(U__gt=0) | Q(V__gt=0) | Q(W__gt=0) | Q(X__gt=0) | Q(Y__isnull=False) | \
+          Q(Z__isnull=False) | Q(AA__isnull=False))
+    context['unverified_third_sms'] = VRChecklist.objects.filter(date=datetime.date(datetime.today())).filter(third_unverified_qs).count()
+    context['not_open_third_sms'] = stats.model_sieve(VRChecklist, [('A', 4), ('verified_third', True)]).filter(date=datetime.date(datetime.today())).count()
 
     context['vr_incidents_count'] = VRIncident.objects.all().count()
     context['vr_incidents_today'] = VRIncident.objects.filter(date=datetime.date(datetime.today())).count()
@@ -130,14 +145,14 @@ def vr_checklist_list(request):
                       Q(N__isnull=False) & Q(P__isnull=False) & Q(Q__isnull=False) & Q(R__isnull=False) & \
                       Q(S__isnull=False) & Q(T__gt=0) & Q(U__gt=0) & Q(V__gt=0) & Q(W__gt=0) & Q(X__gt=0) & Q(Y__isnull=False) & \
                       Q(Z__isnull=False) & Q(AA__isnull=False) | Q(A=4)
-            elif data['third'] == u'4':
+            elif data['third'] == u'4': # not open unverified
                 qs_include &= Q(A=4) & Q(verified_third=False) & (Q(H__isnull=False) | Q(J__isnull=False) | Q(K__isnull=False) | Q(M__isnull=False) | \
                       Q(N__isnull=False) | Q(P__isnull=False) | Q(Q__isnull=False) | Q(R__isnull=False) | \
                       Q(S__isnull=False) | Q(T__gt=0) & Q(U__gt=0) | Q(V__gt=0) | Q(W__gt=0) | Q(X__gt=0) | Q(Y__isnull=False) | \
                       Q(Z__isnull=False) | Q(AA__isnull=False))
-            elif data['third'] == u'5':
+            elif data['third'] == u'5': # not open verified
                 qs_include &= Q(A=4) & Q(verified_third=True)
-            elif data['third'] == u'6':
+            elif data['third'] == u'6': # blank
                 qs_include &= Q(A=4) & Q(H__isnull=True) & Q(J__isnull=True) & Q(K__isnull=True) & Q(M__isnull=True) & \
                       Q(N__isnull=True) & Q(P__isnull=True) & Q(Q__isnull=True) & Q(R__isnull=True) & \
                       Q(S__isnull=True) & Q(T=0) & Q(U=0) & Q(V=0) & Q(W=0) & Q(X=0) & Q(Y__isnull=True) & \
