53f7c67c35bf14449465d12785449c55b3e17ebc
Dipo Elegbede <delegbede@timbaobjects.com>
2011-03-07 11:39:41 +0100

    File modification. The prepopulate_checklists.py was modified to select particular days of election and simulation based on the type of observer.

--- psc/management/commands/prepopulate_checklists.py
+++ psc/management/commands/prepopulate_checklists.py
@@ -46,7 +46,7 @@ class Command(BaseCommand):
             
         observers = Observer.objects.filter(role__iexact='OBS')
         for observer in observers:
-            for day in EDAY_DAYS:
+            for day in EDAY_DAYS[:-3] + EDAY_DAYS[-2:]:# This is set to remove the date for the senatorial election.
                 if day[0]:
                     report_date = day[0]
                     rc = observer.location
