58cbda76e8c5cf3b441d559d193e089d25060c05
Tim Akinbo <takinbo@timbaobjects.com>
2011-03-23 19:12:31 +0100

    fixed a bug in the creation of ghosts to use index 6 instead of 2

--- psc/management/commands/create_ghosts.py
+++ psc/management/commands/create_ghosts.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         created_observers = 0
         lga_observers = Observer.objects.filter(role="LGA", position=1)
         for lga_observer in lga_observers:
-            new_lga_observer, created = Observer.objects.get_or_create(observer_id="%s2" % lga_observer.observer_id[:-1], \
+            new_lga_observer, created = Observer.objects.get_or_create(observer_id="%s6" % lga_observer.observer_id[:-1], \
                 location_type=lga_observer.location_type, location_id=lga_observer.location_id, 
                 supervisor=lga_observer.supervisor, partner=lga_observer.partner, role=lga_observer.role, 
                 gender=lga_observer.gender, position=2)
--- psc/management/commands/prepopulate_checklists.py
+++ psc/management/commands/prepopulate_checklists.py
@@ -51,20 +51,25 @@ class Command(BaseCommand):
             print "%d Display, Claims and Objection Checklists Prepopulated" % dco_reports_created
 
         elif args[0] == 'eday':
-            print "Prepopulating Eday Checklists..."
+            print "Prepopulating EDay Checklists..."
             observers = Observer.objects.filter(role__iexact='OBS')
             for observer in observers:
                 for day in (EDAY_DAYS[1],) + EDAY_DAYS[-3:]:# This is set to remove the date for the senatorial election.
                     if day[0]:
                         report_date = day[0]
                         rc = observer.location
-                        eday, created = EDAYChecklist.objects.get_or_create(date=report_date, observer=observer, checklist_index='1' if int(observer.observer_id[-1]) % 2 else '2', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
-                        if created:
+                        try:
+                            eday = EDAYChecklist.objects.get(date=report_date, observer=observer, checklist_index='1' if int(observer.observer_id[-1]) % 2 else '2', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
+                        except EDAYChecklist.DoesNotExist:
+                            eday = EDAYChecklist.objects.create(date=report_date, observer=observer, checklist_index='1' if int(observer.observer_id[-1]) % 2 else '2', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
                             eday_reports_created += 1
+                            
                     
                         if int(observer.observer_id[-1]) % 2:
-                            eday_control, control_created = EDAYChecklist.objects.get_or_create(date=report_date, observer=observer, checklist_index='3', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
-                            if control_created:
+                            try:
+                                eday_control = EDAYChecklist.objects.get(date=report_date, observer=observer, checklist_index='3', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
+                            except EDAYChecklist.DoesNotExist:
+                                eday_control = EDAYChecklist.objects.create(date=report_date, observer=observer, checklist_index='3', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
                                 eday_reports_created += 1
         
             lga_supervisors = Observer.objects.filter(role__iexact='LGA')
@@ -73,13 +78,18 @@ class Command(BaseCommand):
                     if day[0]:
                         report_date = day[0]
                         rc = lga_supervisor.location
-                        eday, created = EDAYChecklist.objects.get_or_create(date=report_date, observer=lga_supervisor, checklist_index='1' if int(lga_supervisor.observer_id[-1]) % 2 else '2', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
-                        if created:
+                        try:
+                            eday = EDAYChecklist.objects.get(date=report_date, observer=lga_supervisor, checklist_index='1' if int(lga_supervisor.observer_id[-1]) % 6 else '2', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
+                        except EDAYChecklist.DoesNotExist:
+                            eday = EDAYChecklist.objects.create(date=report_date, observer=lga_supervisor, checklist_index='1' if int(lga_supervisor.observer_id[-1]) % 6 else '2', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
                             eday_reports_created += 1
                     
                     
-                        if int(lga_supervisor.observer_id[-1]) % 2:
-                            eday_control, control_created = EDAYChecklist.objects.get_or_create(date=report_date, observer=lga_supervisor, checklist_index='3', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
-                            if control_created:
+                        if int(lga_supervisor.observer_id[-1]) % 6:
+                            try:
+                                eday_control = EDAYChecklist.objects.get(date=report_date, observer=lga_supervisor, checklist_index='3', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
+                            except EDAYChecklist.DoesNotExist:
+                                eday_control = EDAYChecklist.objects.create(date=report_date, observer=lga_supervisor, checklist_index='3', location_type=ContentType.objects.get_for_model(rc), location_id=rc.id)
                                 eday_reports_created += 1
+                                
             print "%d Election Day Checklists Prepopulated" % eday_reports_created
