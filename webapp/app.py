2993de3dbd197b7b6c7450d71443af4f11e59dc2
Tim Akinbo <takinbo@timbaobjects.com>
2011-07-21 12:10:49 +0100

    1. Inclusion of incident form prefixes in pattern regex 2. Refactoring of code for incident and checklist validation

--- webapp/app.py
+++ webapp/app.py
@@ -11,7 +11,11 @@ from datetime import datetime, timedelta
 
 class App(AppBase):
     def __init__(self, router):
-        pattern_string = r'PSC(?P<observer_id>\d+)(?P<form_type>(' + "|".join(ChecklistForm.objects.all().values_list("prefix", flat=True)) + '))(?P<day>\d{1,2})(?P<location_type>(' + "|".join(LocationType.objects.all().exclude(in_form=False).values_list("code", flat=True)) + '))(?P<location_id>\d+)(!?)((?P<responses>[A-Z\d]{1,})@?((?P<comments>).*))?'
+        form_prefixes = list(ChecklistForm.objects.all().values_list("prefix", flat=True))
+        form_prefixes.extend(list(IncidentForm.objects.all().values_list("prefix", flat=True)))
+        form_prefixes = list(set(form_prefixes)) # make the list unique
+        
+        pattern_string = r'PSC(?P<observer_id>\d+)(?P<form_type>(' + "|".join(form_prefixes) + '))(?P<day>\d{1,2})(?P<location_type>(' + "|".join(LocationType.objects.all().exclude(in_form=False).values_list("code", flat=True)) + '))(?P<location_id>\d+)(!?)((?P<responses>[A-Z\d]{1,})@?((?P<comments>).*))?'
         self.pattern = re.compile(pattern_string, re.I)
         self.range_error_response = 'Invalid response(s) for question(s): "%s"'
         self.checklist_attribute_error_response = 'Invalid responses for the checklist code: "%s"'
@@ -38,14 +42,15 @@ class App(AppBase):
             try:
                 message.observer = Contact.objects.get(observer_id=match.group('observer_id'))
             except Contact.DoesNotExist:
-                return message.respond('Observer ID not found. Please resend with valid PSC. You sent: %s' % message.message_only)
+                return message.respond('Observer ID not found. Please resend with valid Observer ID. You sent: %s' % message.message_only)
             try:
                 message.location = Location.objects.get(type__code=match.group('location_type'), code=match.group('location_id'))
             except Location.DoesNotExist:
+                # TODO: respond with an appropriate error message
                 return message.respond('Unknown Location')
                 
-        # determine date of message
-        # if the day is not specified, the date of the current day should be the value of key 'day'.
+            # determine date of message
+            # if the day is not specified, the date of the current day should be the value of key 'day'.
             if match.group('day'):
                 day = int(match.group('day'))
             else:
@@ -62,39 +67,76 @@ class App(AppBase):
                 else:
                     month -= 1
             message.received_at = datetime(year, month, day)
-
-            responses = self._parse_checklist(match.group('responses'))
+            
+            is_invalid_question = False
+            is_invalid_response = False
+            invalid_questions = []
+            invalid_responses = []
             
             #To determine whether a message is a Checklist or an incident
             if (message.text.find("!") == -1): # Then it is a checklist
                 
                 # Validating the checklist question types
-                checklist_form = ChecklistForm.objects.get(prefix=match.group('form_type'))
-                questions = ChecklistQuestion.objects.filter(form=checklist_form)
-                question_codes = questions.values_list('code', flat=True)
-                for question_code in responses.keys():
-                    if question_code not in question_codes:
+                responses = self._parse_checklist(match.group('responses'))
+                
+                try:
+                    checklist_form = ChecklistForm.objects.get(prefix=match.group('form_type'))
+                    questions = ChecklistQuestion.objects.filter(form=checklist_form)
+                    question_codes = questions.values_list('code', flat=True)
+                    for question_code in responses.keys():
+                        if question_code not in question_codes:
+                            # TODO: use a more appropriate response
+                            is_invalid_question = True
+                            invalid_questions.append(question_code)
+                        else:
+                            question_regex = ChecklistQuestion.objects.get(form=checklist_form, code=question_code).type.validate_regex
+                            if not re.compile(question_regex).match(responses[question_code]):
+                                is_invalid_response = True
+                                invalid_responses.append(question_code)
+                            else:
+                                # the question code and response are valid now save or update
+                                # TODO: persist data
+                                pass
+                            
+                    # TODO: use invalid_questions to construct a better response
+                    if is_invalid_question:   
                         return message.respond('Invalid question')
-                        
-                # Validating the checklist question responses
-                for question_code in responses.keys():
-                    question_regex = ChecklistQuestion.objects.get(form=checklist_form, code=question_code).type.validate_regex
-                    if not re.compile(question_regex).match(responses[question_code]):
+                    elif is_invalid_response:
                         return message.respond('Invalid response for question_code')
+                    else:
+                        return message.respond("Correct Checklist")
                         
-                return message.respond ("Correct Checklist")
-            else:   # It must be an incident.
+                except ChecklistForm.DoesNotExist:
+                    # TODO: checklist form type is invalid
+                    pass
+                    
+            else:
+                #Validating the incident question responses
+                responses = list(match.group('responses'))
                 
-                 #Validating the incident question responses
-                
-                incident_form = IncidentForm.objects.get(prefix=match.group('form_type'))
-                incident_response_codes = IncidentResponse.objects.filter(form=incident_form).values_list('code', flat=True)
-                for incident_response_code in responses.keys():
-                    x = IncidentResponse.objects.get(form=incident_form, code=incident_response_code)
-                    if not re.compile(x).match(responses[incident_response_code]):
+                try:
+                    incident_form = IncidentForm.objects.get(prefix=match.group('form_type'))
+                    incident_response_codes = IncidentResponse.objects.filter(form=incident_form).values_list('code', flat=True)
+                    for incident_response_code in responses:
+                        if not incident_response_code in incident_response_codes:
+                            is_invalid_response = True
+                            invalid_responses.append(incident_response_code)
+                        else:
+                            # response code is valid
+                            # TODO: persist data
+                            pass
+                    
+                    if is_invalid_response:
                         return message.respond('Invalid response code')
+                    else:
+                        return message.respond ("Correct Incident")
                         
-                return message.respond ("Correct Incident")
+                except IncidentForm.DoesNotExist:
+                    # TODO: incident form type is invalid
+                    pass
+            
+            # if all fails, send the default response
+            return self.default(message)
             
     def default(self, message):
        return message.respond('Invalid message:"%s". Please resend!' % message.message_only)
