2072a0b27bb38b08fe82dcf3990f39e4d1061723
Joy Okomowho <jokomowho@timbaobjects.com>
2011-07-29 13:28:41 +0100

    Collection for the models
    Please enter the commit message for your changes. Lines starting

--- zambia/static/collections.js
+++ zambia/static/collections.js
@@ -3,3 +3,23 @@ MessageCollection = PaginatedCollection.extend({
     model: Message,
     baseUrl: '/api/v1/message/',
 });
+
+LocationCollection = Backbone.Collection.extend({
+	model: Location,
+	baseUrl: '/api/v1/location',
+});
+
+IncidentCollection = PaginatedCollection.extend({
+	model: Incident,
+	baseUrl: '/api/v1/incident',
+});
+
+ContactCollection = PaginatedCollection.extend({
+	model: Contact,
+	baseUrl: '/api/v1/contact',
+});
+
+ChecklistCollection = PaginatedCollection.extend({
+	model: Checklist,
+	baseUrl: '/api/v1/checklist',
+});
