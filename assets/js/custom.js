5502944c2a8d6d7701d4337d1de4a0467f95c034
Tim Akinbo <takinbo@timbaobjects.com>
2011-08-11 11:50:27 +0100

    adding javascript source files

---
+++ assets/js/custom.js
@@ -0,0 +1,78 @@
+// application specific
+$(function () {
+    // AJAX throbber for indicating AJAX activity
+    $("#throbber").throbber({
+	    bgopacity: 0, fgcolor: "#AAA", segments: 12,
+		path: function(i, segments) {
+			return "M 40,8 c -4,0 -8,-4 -8,-8 c 0,-4 4,-8 8,-8 l 30,0 c 4,0 8,4 8,8 c 0,4 -4,8 -8,8 l -30,0 z";
+		},
+		opacity: function(i, segments) { return 1-(i%segments)/segments;},
+		speed: 3,
+		show: function() { $(this).fadeIn(100);},
+		hide: function(callback) { $(this).fadeOut(100, callback);}
+	});
+	
+	$(document).ajaxStart(function () { $("#throbber").throbber('enable'); });
+	$(document).ajaxStop(function () { $("#throbber").throbber('disable'); });
+	
+	$("#throbber").throbber('disable');
+	
+	// Search
+	$('form#search').live('submit', function () {
+		search_options = {};
+		
+		$.each($('.search_field'), function (idx, el) {
+			match = /^search_(.*)/.exec($(el).attr('id'));
+			if (match && $(el).val()) {
+				search_options[match[1]] = $(el).val();
+			}
+		});
+		
+		paginated_collection.filtrate(search_options);
+		return false;
+	});
+	
+	// Column Sorting
+	$("a.sortable_column").live('click', function () {
+		match = /^sort_(.*)$/.exec($(this).attr('id'));
+		if (match) {
+			sort_field = match[1];
+			if (paginated_collection.sort_field) {
+				match = /^-?(.*)$/.exec(paginated_collection.sort_field);
+				if (match && match[1] == sort_field) {
+					desc_match = /^-.*$/.exec(paginated_collection.sort_field);
+					if (!desc_match) {
+						// display descending order image
+						$('img.sorted_direction').remove();
+						$(this).after('<img src="/assets/images/sort_desc.png" class="sorted_direction" />');
+
+						// Switch to descending order
+						paginated_collection.sort_by('-'+sort_field);			
+						return false;
+					}
+				}
+			}
+			
+			// display ascending order image
+			$('img.sorted_direction').remove();
+			$(this).after('<img src="/assets/images/sort_asc.png" class="sorted_direction" />');
+			
+			paginated_collection.sort_by(sort_field);
+		}
+		return false;
+	});
+
+    $.widget("custom.catcomplete", $.ui.autocomplete, {
+    	_renderMenu: function( ul, items ) {
+    		var self = this,
+    			currentCategory = "";
+    		$.each( items, function( index, item ) {
+    			if ( item.category != currentCategory ) {
+    				ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+    				currentCategory = item.category;
+    			}
+    			self._renderItem( ul, item );
+    		});
+    	}
+    });
+});
---
+++ assets/js/paginated_collection.js
@@ -0,0 +1,105 @@
+// includes bindings for fetching/fetched
+
+var PaginatedCollection = Backbone.Collection.extend({
+  initialize: function() {
+    _.bindAll(this, 'parse', 'url', 'pageInfo', 'nextPage', 'previousPage', 'firstPage', 'lastPage', 'filtrate', 'sort_by');
+    typeof(options) != 'undefined' || (options = {});
+    typeof(this.limit) != 'undefined' || (this.limit = 20);
+    typeof(this.offset) != 'undefined' || (this.offset = 0);
+    typeof(this.filter_options) != 'undefined' || (this.filter_options = {});
+    typeof(this.sort_field) != 'undefined' || (this.sort_field = '');
+  },
+  fetch: function(options) {
+    typeof(options) != 'undefined' || (options = {});
+    //this.trigger("fetching");
+    var self = this;
+    var success = options.success;
+    options.success = function(resp) {
+      //self.trigger("fetched");
+      if(success) { success(self, resp); }
+    };
+    return Backbone.Collection.prototype.fetch.call(this, options);
+  },
+  parse: function(resp) {
+    this.offset = resp.meta.offset;
+    this.limit = resp.meta.limit;
+    this.total = resp.meta.total_count;
+    return resp.objects;
+  },
+  url: function() {
+      urlparams = {offset: this.offset, limit: this.limit};
+      urlparams = $.extend(urlparams, this.filter_options);
+      if (this.sort_field) {
+          urlparams = $.extend(urlparams, {sort_by: this.sort_field});
+      }
+      return this.baseUrl + '?' + $.param(urlparams);
+  },
+  pageInfo: function() {
+    var info = {
+      total: this.total,
+      offset: this.offset,
+      page: Math.ceil(this.offset / this.limit) || 1,
+      limit: this.limit,
+      pages: Math.ceil(this.total / this.limit),
+      prev: false,
+      next: false
+    };
+
+    var max = Math.min(this.total, this.offset + this.limit);
+
+    if (this.total == this.pages * this.limit) {
+      max = this.total;
+    }
+
+    info.range = [(this.offset + 1), max];
+
+    if (this.offset > 0) {
+      info.prev = (this.offset - this.limit) || 1;
+    }
+
+    if (this.offset + this.limit < info.total) {
+      info.next = this.offset + this.limit;
+    }
+
+    return info;
+  },
+  nextPage: function() {
+    if (!this.pageInfo().next) {
+      return false;
+    }
+    this.offset = this.offset + this.limit;
+    return this.fetch();
+  },
+  previousPage: function() {
+    if (!this.pageInfo().prev) {
+      return false;
+    }
+    this.offset = (this.offset - this.limit) || 0;
+    return this.fetch();
+  },
+  firstPage: function() {
+    if (!this.pageInfo().page > 1) {
+      return false;
+    }
+    this.offset = 0;
+    return this.fetch();
+  },
+  lastPage: function() {
+    if (!(this.pageInfo().page < this.pageInfo().pages)) {
+      return false;
+    }
+    this.offset = ((this.pageInfo().pages - 1) * this.limit) || 0;
+    return this.fetch();
+  },
+  filtrate: function (options) {
+      this.filter_options = options || {};
+      this.offset = 0;
+      return this.fetch();
+  },
+  sort_by: function (field) {
+      this.sort_field = field;
+      this.offset = 0;
+      return this.fetch();
+  }
+
+});
