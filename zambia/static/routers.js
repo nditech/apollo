50501fe48edd3a1c32dbf2ee14d119219b676956
Tim Akinbo <takinbo@timbaobjects.com>
2011-08-17 13:19:20 +0100

    autocomplete for the location textbox

--- zambia/static/routers.js
+++ zambia/static/routers.js
@@ -24,6 +24,12 @@ ZambiaRouter = Backbone.Router.extend({
               checklists_view = new ChecklistCollectionView({collection: coll}).render();
               $('div.full_width_content').append(checklists_view);
               
+              // Autocomplete for location input textbox
+              $( "#search_location__name" ).catcomplete({
+                 source: '/api/v1/location/search/',
+                 position: { my: 'left top', at: 'left bottom', collision: 'none', offset: '0 -3'}
+              });
+              
               $('.date_field').datepicker();
           },
         });
