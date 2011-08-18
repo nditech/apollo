aa67f1b8a46fcb6dae0943027642264bff9af6df
Tim Akinbo <takinbo@timbaobjects.com>
2011-08-18 19:14:34 +0100

    stab at filtering based on section statuses

--- zambia/api.py
+++ zambia/api.py
@@ -1,5 +1,7 @@
 from webapp.api import *
+from webapp.models import *
 from models import *
+from django.db.models import Q
 
 class ChecklistResponseResource(ModelResource):    
     class Meta:
@@ -34,9 +36,62 @@ class ChecklistResource(ModelResource):
             filters = {}
 
         orm_filters = super(ChecklistResource, self).build_filters(filters)
+        
         if orm_filters.has_key('location__id__exact'):
             id = orm_filters.pop('location__id__exact')
             orm_filters['location__id__in'] = Location.objects.get(id=id).get_descendants(True).values_list('id', flat=True)
+        
+        if filters.get('setup_status'):
+            status = filters.get('setup_status')
+            if status == '1': # complete
+                orm_filters.update(dict([('response__%s__isnull' % field, False) for field in \
+                    ['A', 'B', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF', 'CG', 'CH', 'D', 'E', 'EA', \
+                    'EB', 'EC', 'F', 'G', 'H', 'J']]))
+                
+            elif status == '2': # missing
+                orm_filters.update(dict([('response__%s__isnull' % field, True) for field in \
+                    ['A', 'B', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF', 'CG', 'CH', 'D', 'E', 'EA', \
+                    'EB', 'EC', 'F', 'G', 'H', 'J']]))
+                    
+            elif status == '3': # partial
+                query = None
+                for field in ['A', 'B', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF', 'CG', 'CH', 'D', \
+                    'E', 'EA', 'EB', 'EC', 'F', 'G', 'H', 'J']:
+                    if not query:
+                        exec 'query = Q(response__%s__isnull=False)' % field
+                    else:
+                        exec 'query |= Q(response__%s__isnull=False)' % field
+                orm_filters['pk__in'] = Checklist.objects.filter(query).values_list('id', flat=True)
+
+        if filters.get('voting_status'):
+            status = filters.get('voting_status')
+            if status == '1': # complete
+                orm_filters.update(dict([('response__%s__isnull' % field, False) for field in \
+                    ['A', 'B', 'CA', 'CB', 'CC', 'CD', 'CE', 'CF', 'CG', 'CH', 'D', 'E', 'EA', \
+                    'EB', 'EC', 'F', 'G', 'H', 'J']]))
+                
+            elif status == '2': # missing
+                pass
+            elif status == '3': # partial
+                pass
+
+        if filters.get('closing_status'):
+            status = filters.get('closing_status')
+            if status == '1': # complete
+                pass
+            elif status == '2': # missing
+                pass
+            elif status == '3': # partial
+                pass
+
+        if filters.get('counting_status'):
+            status = filters.get('counting_status')
+            if status == '1': # complete
+                pass
+            elif status == '2': # missing
+                pass
+            elif status == '3': # partial
+                pass
 
         return orm_filters
 
