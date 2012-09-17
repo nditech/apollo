b4abd701754ddf064e36ddf63eb0a1b718aeec8c
Tim Akinbo <takinbo@timbaobjects.com>
2012-09-17 17:40:19 +0100

    added support for cases where groups have no fields

--- core/querysets.py
+++ core/querysets.py
@@ -5,15 +5,15 @@ class SubmissionQuerySet(HStoreQuerySet):
     def is_complete(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
         _where = 'data ?& ARRAY[%s]' % (','.join(['%s'] * len(fields)))
-        return self.extra(where=[_where], params=fields)
+        return self.extra(where=[_where], params=fields) if fields else self
 
     def is_missing(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
         _where = 'NOT data ?| ARRAY[%s]' % (','.join(['%s'] * len(fields)))
-        return self.extra(where=[_where], params=fields)
+        return self.extra(where=[_where], params=fields) if fields else self.none()
 
     def is_partial(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
         _where = 'data ?| ARRAY[%(fields)s] AND NOT data ?& ARRAY[%(fields)s]' % \
             {'fields': ','.join(['%s'] * len(fields))}
-        return self.extra(where=[_where], params=fields * 2)
+        return self.extra(where=[_where], params=fields * 2) if fields else self
