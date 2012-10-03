a1734756bc5e25c275cec371c650f89fc6424e65
Oladipo Odumosu <dodumosu@timbaobjects.com>
2012-10-03 13:37:26 +0100

    Tweaked submission views

--- core/views.py
+++ core/views.py
@@ -1,19 +1,43 @@
 from django.conf import settings
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from .models import *
 
 
+COMPLETION_STATUS = (
+    (0, 'Complete'),
+    (1, 'Partial'),
+    (2, 'Empty'),
+)
+
+
+def get_completion_status(submission):
+    groups = submission.form.groups.all()
+    statuses = {}
+    for group in groups:
+        tags = [field.tag for field in group.fields.all()]
+        temp = [tag in submission.data for tag in tags]
+
+        if all(temp):
+            # complete
+            statuses[group.pk] = 0
+        elif any(temp):
+            # partial
+            statuses[group.pk] = 1
+        else:
+            # unfilled
+            statuses[group.pk] = 2
+
+    return statuses
+
+
 def list_submissions(queryset, page_num):
     '''Utility function to return a set of submissions from a queryset'''
     paginator = Paginator(queryset, settings.SUBMISSIONS_PER_PAGE)
-    
+
     try:
         submissions = paginator.page(page_num)
-    except PageNotAnInteger:
-        # return first page if page is not an integer
-        submissions = paginator.page(1)
     except EmptyPage:
         # return last page if page is out of range
         submissions = paginator.page(paginator.num_pages)
@@ -23,3 +47,14 @@ def list_submissions(queryset, page_num):
 
 def submission_table_view(request, page=None):
     '''Function-based tabular view for submissions'''
+    try:
+        page_num = int(page)
+    except TypeError:
+        page_num = 1
+    except ValueError:
+        page_num = 1
+
+    # submissions here is a Page object
+    submissions = list_submissions(Submission.objects.all(), page_num)
+
+    # get checklist completion status
