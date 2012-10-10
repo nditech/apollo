5c9f602b7ce7766140d305fc5195c0aca554dac0
Joy Okomowho <jokomowho@timbaobjects.com>
2012-10-10 11:42:15 +0100

    Linked contact menu on page layout to contact url

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
     url(r'^api/', include(v2_api.urls)),
     url(r'^submissions/(?P<form>\d+)/?', SubmissionListView.as_view(), name='submissions_list'),
     url(r'^submission/(?P<pk>\d+)/?', SubmissionEditView.as_view(), name='submission_edit'),
-    url(r'^contacts/?', ContactListView.as_view(), name='contacts'),
+    url(r'^contacts/?', ContactListView.as_view(), name='contacts_list'),
     url(r'^contact/(?P<pk>\d+)/?', ContactEditView.as_view(), name='contact_edit'),
     url(r'^tpl/(?P<template_name>.+)/?', TemplatePreview.as_view()),
 )
