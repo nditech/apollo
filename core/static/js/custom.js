0de238175cfe71d5e20012a0e901f9c3e101a64c
Joy Okomowho <jokomowho@timbaobjects.com>
2012-10-10 16:41:04 +0100

    integrated prototype visualizations on dashboard

--- core/static/js/custom.js
+++ core/static/js/custom.js
@@ -11,3 +11,169 @@ $(function(){
   		$(this).parent().datepicker('show');
   	});
   });
+
+var chart;
+    
+    $(function () {
+      
+      // Build the chart
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'setup',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+              percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Status',
+                data: [
+                    ['Partial', 50],
+                    ['Missing', 50],
+                    ['Complete', 50]
+                ],
+                color: 'red'
+            }]
+        });
+    });
+
+var chart;
+    
+    $(function () {
+      
+      // Build the chart
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'voting',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+              percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Status',
+                data: [
+                    ['Partial', 20],
+                    ['Missing', 30],
+                    ['Complete', 40]
+                ],
+                color: 'red'
+            }]
+        });
+    });
+var chart;
+    
+    $(function () {
+      
+      // Build the chart
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'counting',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+              percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Status',
+                data: [
+                    ['Partial', 30],
+                    ['Missing', 80],
+                    ['Complete', 30]
+                ],
+                color: 'red'
+            }]
+        });
+    });
+var chart;
+    
+    $(function () {
+      
+      // Build the chart
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'closing',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+              percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Status',
+                data: [
+                    ['Missing', 45.0],
+                    ['Partial', 26.8],
+                    ['Complete', 12.8]
+                ],
+                color: 'red'
+            }]
+        });
+    });
