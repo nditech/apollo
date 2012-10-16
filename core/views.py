c29c5a09b2f336b71e7f97cfa034f5ec2effc738
Oladipo Odumosu <dodumosu@timbaobjects.com>
2012-10-16 15:27:41 +0100

    Refactored export to accomodate hstore fields

--- core/views.py
+++ core/views.py
@@ -1,9 +1,17 @@
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+import re
+
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, ListView, UpdateView
+import tablib
 from .forms import ContactModelForm, generate_submission_form
 from .models import *
 
@@ -146,18 +154,50 @@ def reformat_field_name(field_name):
     return re.sub('_+', ' ', field_name).title()
 
 
-def export(queryset, *fields, **kwargs):
+def export(queryset, *args, **kwargs):
     dataset = tablib.Dataset()
 
-    # no provision yet for HStore fields
-    dataset.headers = map(reformat_field_name, fields)
+    # grab the name of the hstore field if it exists
+    hstore_field = kwargs.pop('hstore_field', None)
 
-    # add the values
-    for row in queryset.order_by('id').values_list(*fields):
+    # also, grab the list of keys for the hstore field as well
+    hstore_keys = kwargs.pop('hstore_keys', None)
+
+    # finally, grab the format specifier
+    format = kwargs.pop('format', 'xls')
+
+    # make sure we snap back to the default if we get something unsupported
+    format = 'xls' if format not in export_formats else format
+
+    # reorder querysets
+    queryset = queryset.order_by('id')
+
+    stripped_args = list(args)
+
+    try:
+        index = stripped_args.index(hstore_field)
+        stripped_args.remove(hstore_field)
+    except ValueError:
+        index = None
+
+    headers = map(reformat_field_name, stripped_args)
+
+    if hstore_keys is not None:
+        headers.extend(map(reformat_field_name, hstore_keys))
+
+    dataset.headers = headers
+
+    for record in queryset.values_list(*args):
+        if index is not None:
+            row = list(record[:index])
+
+            for key in hstore_keys:
+                row.append(record[index].get(key, None))
+
+            row.extend(record[index + 1:])
+        else:
+            row = record
+
         dataset.append(row)
 
-    # get the format from the keyword args
-    format = kwargs.pop('format', 'xls')
-    format = format if format in export_formats else 'xls'
-
-    return StringIO.StringIO(getattr(dataset, format))
+    return StringIO(getattr(dataset, format))
