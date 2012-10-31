cca5505ab5f8a5299ca023f18eafd83815aceea1
Tim Akinbo <takinbo@timbaobjects.com>
2012-10-31 11:21:53 +0100

    explicitly specify table to extra data attribute from

--- core/querysets.py
+++ core/querysets.py
@@ -4,16 +4,16 @@ from django_orm.postgresql.hstore.queryset import HStoreQuerySet
 class SubmissionQuerySet(HStoreQuerySet):
     def is_complete(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
-        _where = 'data ?& ARRAY[%s]' % (','.join(['%s'] * len(fields)))
+        _where = '"core_submission"."data" ?& ARRAY[%s]' % (','.join(['%s'] * len(fields)))
         return self.extra(where=[_where], params=fields) if fields else self
 
     def is_missing(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
-        _where = 'NOT data ?| ARRAY[%s]' % (','.join(['%s'] * len(fields)))
+        _where = 'NOT "core_submission"."data" ?| ARRAY[%s]' % (','.join(['%s'] * len(fields)))
         return self.extra(where=[_where], params=fields) if fields else self.none()
 
     def is_partial(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
-        _where = 'data ?| ARRAY[%(fields)s] AND NOT data ?& ARRAY[%(fields)s]' % \
+        _where = '"core_submission"."data" ?| ARRAY[%(fields)s] AND NOT "core_submission"."data" ?& ARRAY[%(fields)s]' % \
             {'fields': ','.join(['%s'] * len(fields))}
         return self.extra(where=[_where], params=fields * 2) if fields else self
