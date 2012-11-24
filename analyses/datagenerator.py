cb91b2e7bd4243bca42891c4ce041bdbb4293f21
Tim Akinbo <takinbo@timbaobjects.com>
2012-11-24 09:34:37 +0100

    custom control over pickling and compressing the graph before caching

--- analyses/datagenerator.py
+++ analyses/datagenerator.py
@@ -2,8 +2,21 @@ from django.core.cache import cache
 import networkx as nx
 from django_dag.models import Node, Edge
 from core.models import Location
+import pickle
+# zlib compression routines to reduce the size of the generated graphs
+# so they actually be cached.
+try:
+    from zlib import compress, decompress
+except ImportError:
+    compress = decompress = lambda x: x
+
 
 def generate_locations_graph():
+    '''
+    This method generates a graph of all the locations in the database using
+    the graph that has already been stored in the database courtesy of
+    django_dag.
+    '''
     nodes = Node.objects.filter(graph__name='location').values('pk', 'object_id')
     locations = Location.objects.filter(pk__in=[node['object_id'] for node in nodes]).values('pk', 'name', 'type__name')
     DG = nx.DiGraph()
@@ -14,13 +27,24 @@ def generate_locations_graph():
         DG.add_edge(node_from, node_to)
     return DG
 
+
 def get_locations_graph():
-    graph = cache.get('locations_graph')
-    if not graph:
+    buf = cache.get('locations_graph')
+    if not buf:
         graph = generate_locations_graph()
-        cache.set('locations_graph', graph)
+        # pickle and compress the graph
+        buf = compress(pickle.dumps(graph))
+        cache.set('locations_graph', buf)
+    else:
+        graph = pickle.loads(decompress(buf))
     return graph
 
+
 def get_location_ancestors_by_type(graph, location_id, types=['Province']):
+    '''
+    Using the depth-first-search algorithm, retrieve all the ancestry of a location.
+    The subgraph generated is then filtered to only return location types that are
+    specified.
+    '''
     nodes = graph.subgraph(nx.dfs_tree(graph, location_id).nodes()).nodes(data=True)
     return [node[1] for node in nodes if node[1]['type'] in types]
