35c14579f11271d746a8ced3537a56983145e52e
Tim Akinbo <takinbo@timbaobjects.com>
2012-11-24 16:10:20 +0100

    cosmetic changes

--- analyses/datagenerator.py
+++ analyses/datagenerator.py
@@ -3,6 +3,7 @@ import networkx as nx
 from django_dag.models import Node, Edge
 from core.models import Location
 
+
 def generate_locations_graph():
     nodes = Node.objects.filter(graph__name='location').values('pk', 'object_id')
     locations = Location.objects.filter(pk__in=[node['object_id'] for node in nodes]).values('pk', 'name', 'type__name')
@@ -14,12 +15,14 @@ def generate_locations_graph():
         DG.add_edge(node_from, node_to)
     return DG
 
+
 def get_locations_graph():
     graph = cache.get('locations_graph')
     if not graph:
         graph = generate_locations_graph()
         cache.set('locations_graph', graph)
     return graph
+
 
 def get_location_ancestors_by_type(graph, location_id, types=['Province']):
     nodes = graph.subgraph(nx.dfs_tree(graph, location_id).nodes()).nodes(data=True)
