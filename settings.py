85bc9f30ec99212ee4d110a13d071f32705f2eb1
Oladipo Odumosu <dodumosu@timbaobjects.com>
2013-01-24 15:25:01 +0100

    removed specific kannel settings in setting.py

--- settings.py
+++ settings.py
@@ -38,14 +38,6 @@ INSTALLED_BACKENDS = {
     },
     "kannel-smsc": {
         "ENGINE": "rapidsms.backends.kannel",
-        "sendsms_url": "http://127.0.0.1:13013/cgi-bin/sendsms",
-        "sendsms_params": {"smsc": "FAKE",
-                           "from": "123", # not set automatically by SMSC
-                           "username": "rapidsms",
-                           "password": "CHANGE-ME"}, # or set in localsettings.py
-        "coding": 0,
-        "charset": "ascii",
-        "encode_errors": "ignore", # strip out unknown (unicode) characters
     }
 }
 
