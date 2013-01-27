3643a53931fe207cba41801b35c21a1aa5b92b6b
Oladipo Odumosu <dodumosu@timbaobjects.com>
2013-01-27 18:27:33 +0100

    Updated tests to isolate failure point

--- src/apollo/importer/tests.py
+++ src/apollo/importer/tests.py
@@ -10,48 +10,101 @@ from tempfile import TemporaryFile
 from django.test import TestCase
 from tablib import Dataset
 from core.models import *
-from .importer import *
+from .importer import _save_location_type
 
 
 class ImporterTest(TestCase):
+    location_type_data = None
+    location_data = None
+    observer_data = None
+
     def setUp(self):
         root = logging.getLogger('south')
         root.setLevel(logging.INFO)
 
-    def test_excel_location_type_import(self):
-        data = Dataset(headers=['Type', 'Parent'])
-        data.append(['Root', ''])
-        data.append(['Branch1', 'Root'])
-        data.append(['Branch2', 'Branch1'])
-        data.append(['Continent', 'Fake'])  # for sanity check
+        self.location_type_data = Dataset(headers=['Type', 'Parent'])
+        self.location_type_data.append(['Country', ''])
+        self.location_type_data.append(['Region', 'Country'])
+        self.location_type_data.append(['State', 'Region'])
+        # sanity check
+        self.location_type_data.append(['Continent', 'Fake'])
 
-        with TemporaryFile() as temp:
-            temp.write(data.xls)
+        self.location_data = Dataset(headers=['Name', 'Code', 'Location Type'])
+        self.location_data.append(['Lagos', 'LAG', 'State'])
+        self.location_data.append(['South-West', 'SW', 'Region'])
+        # sanity check
+        self.location_data.append(['Antarctica', 'ANTA', 'Continent'])
 
-            # reset stream pointer for reading
-            temp.seek(0)
+    # def test_excel_location_type_import(self):
+    #     with TemporaryFile() as temp:
+    #         temp.write(self.location_type_data.xls)
 
-            errors = import_excel_location_types(temp)
-            logging.info(errors)
+    #         # reset stream pointer for reading
+    #         temp.seek(0)
 
-            self.assertEqual(len(errors), 1)
-            self.assertEqual(len(LocationType.objects.all()), 3)
+    #         errors = import_excel_location_types(temp)
+    #         logging.info(errors)
 
-    def test_csv_location_type_import(self):
-        data = Dataset(headers=['Type', 'Parent'])
-        data.append(['Country', ''])
-        data.append(['Region', 'Country'])
-        data.append(['State', 'Region'])
-        data.append(['Continent', 'Fake'])  # for sanity check
+    #         self.assertEqual(len(errors), 1)
+    #         self.assertEqual(LocationType.objects.count(), 3)
 
-        with TemporaryFile() as temp:
-            temp.write(data.csv)
+    # def test_csv_location_type_import(self):
+    #     with TemporaryFile() as temp:
+    #         temp.write(self.location_type_data.csv)
 
-            # reset stream pointer for reading
-            temp.seek(0)
+    #         # reset stream pointer for reading
+    #         temp.seek(0)
 
-            errors = import_csv_location_types(temp)
-            logging.info(errors)
+    #         errors = import_csv_location_types(temp)
+    #         logging.info(errors)
 
-            self.assertEqual(len(errors), 1)
-            self.assertEqual(len(LocationType.objects.all()), 3)
+    #         self.assertEqual(len(errors), 1)
+    #         self.assertEqual(LocationType.objects.count(), 3)
+
+    # def test_csv_location_import(self):
+    #     with TemporaryFile() as temp1:
+    #         temp1.write(self.location_type_data.csv)
+    #         temp1.seek(0)
+
+    #         import_csv_location_types(temp1)
+
+    #     with TemporaryFile() as temp2:
+    #         temp2.write(self.location_data.csv)
+    #         temp2.seek(0)
+
+    #         errors = import_csv_locations(temp2)
+
+    #         self.assertEqual(len(errors), 1)
+    #         self.assertEqual(Location.objects.count(), 2)
+
+    # def test_excel_location_import(self):
+    #     with TemporaryFile() as temp1:
+    #         temp1.write(self.location_type_data.xls)
+    #         temp1.seek(0)
+
+    #         import_excel_location_types(temp1)
+
+    #     with TemporaryFile() as temp2:
+    #         temp2.write(self.location_data.xls)
+    #         temp2.seek(0)
+
+    #         errors = import_excel_locations(temp2)
+
+    #         self.assertEqual(len(errors), 1)
+    #         self.assertEqual(Location.objects.count(), 2)
+
+    def test1(self):
+        _save_location_type('Country', '')
+        _save_location_type('Zone', 'Country')
+        _save_location_type('State', 'Zone')
+        _save_location_type('LGA', 'State')
+        _save_location_type('Ward', 'LGA')
+        _save_location_type('PU', 'Ward')
+
+        self.assertEqual(LocationType.objects.count(), 6)
+
+    def test2(self):
+        _save_location_type('Country', '')
+        _save_location_type('Region', 'Country')
+
+        self.assertEqual(LocationType.objects.count(), 2)
