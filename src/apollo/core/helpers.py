61c00c8fee8584e4f5e28a27e86ae4852731dc76
Oladipo Odumosu <dodumosu@timbaobjects.com>
2013-01-31 21:47:17 +0100

    Refactored dashboard generator to take a queryset

--- src/apollo/core/helpers.py
+++ src/apollo/core/helpers.py
@@ -1,21 +1,18 @@
-from .models import Form, Location, Submission
+from .models import Form
 
 
-def generate_dashboard_summary(form_id, location_id):
+def generate_dashboard_summary(form_id, submission_queryset):
     try:
         form = Form.objects.get(pk=form_id)
-        location = Location.objects.get(pk=location_id)
     except Form.DoesNotExist:
-        return {}
-    except Location.DoesNotExist:
         return {}
 
     summary = {}
 
     for group in form.groups.all():
-        complete = len(Submission.objects.all().is_within(location).is_complete(group))
-        missing = len(Submission.objects.all().is_within(location).is_missing(group))
-        partial = len(Submission.objects.all().is_within(location).is_partial(group))
+        complete = submission_queryset.filter(observer=None).is_complete(group).count()
+        missing = submission_queryset.filter(observer=None).is_missing(group).count()
+        partial = submission_queryset.filter(observer=None).is_partial(group).count()
 
         group_summary = {'complete': complete, 'missing': missing, 'partial': partial}
 
