2b953013147b4f3b3a3c4cb9a455bee499bd96db
Tim Akinbo <takinbo@timbaobjects.com>
2013-02-01 17:50:59 +0100

    refactored dashboard summary method

--- src/apollo/core/helpers.py
+++ src/apollo/core/helpers.py
@@ -1,20 +1,16 @@
 from .models import Form
 
 
-def generate_dashboard_summary(form_id, submission_queryset):
-    try:
-        form = Form.objects.get(pk=form_id)
-    except Form.DoesNotExist:
-        return {}
-
+def generate_dashboard_summary(submission_queryset, form):
     summary = []
 
+    # filter out and only select checklists to be filled by observers
     for group in form.groups.all():
-        complete = submission_queryset.filter(observer=None).is_complete(group).count()
-        missing = submission_queryset.filter(observer=None).is_missing(group).count()
-        partial = submission_queryset.filter(observer=None).is_partial(group).count()
+        complete = submission_queryset.exclude(observer=None).is_complete(group).count()
+        missing = submission_queryset.exclude(observer=None).is_missing(group).count()
+        partial = submission_queryset.exclude(observer=None).is_partial(group).count()
 
-        group_summary = {group.name.lower(): {'complete': complete, 'missing': missing, 'partial': partial}}
+        group_summary = {group.name: {'complete': complete, 'missing': missing, 'partial': partial}}
 
         summary.append(group_summary)
 
