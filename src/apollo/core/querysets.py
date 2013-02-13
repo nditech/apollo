d20f7d55c07e2ee6ad4eaef56fd73b7901f1af52
Tim Akinbo <takinbo@timbaobjects.com>
2013-02-13 08:41:27 +0100

    refactoring queryset methods

--- src/apollo/core/querysets.py
+++ src/apollo/core/querysets.py
@@ -5,22 +5,25 @@ from djorm_hstore.functions import HstorePeek
 
 class SearchableLocationQuerySet(HStoreQueryset):
     def is_within(self, location):
-        return self.filter(location__pk__in=[loc['id'] for loc in location.nx_descendants(include_self=True)])
+        return self.filter(
+            location__pk__in=[
+                loc['id'] for loc in location.nx_descendants(include_self=True)
+            ])
 
 
 class SubmissionQuerySet(SearchableLocationQuerySet):
     def is_complete(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
         expr = OR(
-                SqlExpression("data", "?&", fields),
-                SqlExpression("master__data", "?&", fields))
+            SqlExpression("data", "?&", fields),
+            SqlExpression("master__data", "?&", fields))
         return self.where(expr) if fields else self
 
     def is_missing(self, group):
         fields = list(group.fields.values_list('tag', flat=True))
         expr = AND(
-                ~SqlExpression("data", "?|", fields),
-                ~SqlExpression("master__data", "?|", fields))
+            ~SqlExpression("data", "?|", fields),
+            ~SqlExpression("master__data", "?|", fields))
         return self.where(expr) if fields else self.none()
 
     def is_partial(self, group):
@@ -28,10 +31,15 @@ class SubmissionQuerySet(SearchableLocationQuerySet):
         # checks that either data is partial or master__data is partial
         # but none of either data or master__data is complete
         expr = AND(
-                OR(
-                    AND(SqlExpression("data", "?|", fields), ~SqlExpression("data", "?&", fields)),
-                    AND(SqlExpression("master__data", "?|", fields), ~SqlExpression("master__data", "?&", fields))),
-                AND(~SqlExpression("data", "?&", fields), ~SqlExpression("master__data", "?&", fields)))
+            OR(
+                AND(SqlExpression("data", "?|", fields),
+                    ~SqlExpression("data", "?&", fields)),
+                AND(SqlExpression("master__data", "?|", fields),
+                    ~SqlExpression("master__data", "?&", fields))
+            ),
+            AND(~SqlExpression("data", "?&", fields),
+                ~SqlExpression("master__data", "?&", fields))
+        )
         return self.where(expr) if fields else self
 
     def data(self, tags):
