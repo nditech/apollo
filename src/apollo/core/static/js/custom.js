9d65668b6c7532180e67008b9bf62ee69f8d33d9
Tim Akinbo <takinbo@timbaobjects.com>
2013-02-15 09:26:11 +0100

    1. CSS correction for the select2 plugin 2. jquery for ajax-based Location and Observer select2 dropdowns

--- src/apollo/core/static/js/custom.js
+++ src/apollo/core/static/js/custom.js
@@ -24,4 +24,53 @@ $(function(){
     minimumInputLength: 1,
     matcher: function(term, text) { return text.toUpperCase().indexOf(term.toUpperCase()) === 0; }
   });
+
+  $('.select2-locations').select2({
+    allowClear: true,
+    minimumInputLength: 1,
+    loadMorePadding: 5,
+    ajax: {
+      url: '/api/v2/locations/',
+      dataType: 'json',
+      quietMillis: 500,
+      data: function (term, page) {
+        return {
+          name__istartswith: term,
+          limit: 20,
+          offset: (page - 1) * 20
+        };
+      },
+      results: function (data, page) {
+        var more = (page * 20) < data.meta.total_count;
+        return {results: data.objects, more: more};
+      }
+    },
+    formatResult: function (location, container, query) { return location.name + ' · <i>' + location.type.name + '</i>'; },
+    formatSelection: function (location, container) { return location.name + ' · <i>' + location.type.name + '</i>'; },
+    escapeMarkup: function(m) { return m; }
+  });
+
+  $('.select2-observers').select2({
+    minimumInputLength: 1,
+    loadMorePadding: 5,
+    ajax: {
+      url: '/api/v2/contacts/',
+      dataType: 'json',
+      quietMillis: 500,
+      data: function (term, page) {
+        return {
+          observer_id__istartswith: term,
+          limit: 20,
+          offset: (page - 1) * 20
+        };
+      },
+      results: function (data, page) {
+        var more = (page * 20) < data.meta.total_count;
+        return {results: data.objects, more: more};
+      }
+    },
+    formatResult: function (observer, container, query) { return observer.observer_id + ' · <i>' + observer.name + '</i>'; },
+    formatSelection: function (observer, container) { return observer.observer_id + ' · <i>' + observer.name + '</i>'; },
+    escapeMarkup: function(m) { return m; }
+  });
 });
