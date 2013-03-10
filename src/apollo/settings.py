55cdc20d66db93fed769531716d3940252cf5ed7
Tim Akinbo <takinbo@timbaobjects.com>
2013-03-10 05:02:47 +0100

    fixed table layout and enabled timezone awareness

--- src/apollo/settings.py
+++ src/apollo/settings.py
@@ -19,6 +19,8 @@ DATABASES = {
 
 TIME_ZONE = 'Africa/Lagos'
 
+USE_TZ = True
+
 # the rapidsms backend configuration is designed to resemble django's
 # database configuration, as a nested dict of (name, configuration).
 #
