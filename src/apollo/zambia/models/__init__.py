5cec480f5bc38a00fdccbf62926e564d49906382
Tim Akinbo <takinbo@timbaobjects.com>
2013-06-09 11:22:09 +0100

    cleanup

--- src/apollo/zambia/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-if getattr(settings, 'ZAMBIA_DEPLOYMENT', 'RRP') == 'GENERAL': # TODO: the default should be changed to RRP
-    from general_models import *
-else:
-    from rrp_models import *
--- src/apollo/zambia/static/collections.js
+++
@@ -1,6 +0,0 @@
-// Backbone.js Collections
-
-ContactCollection = PaginatedCollection.extend({
-	model: Contact,
-	baseUrl: '/api/v1/contacts/',
-});
