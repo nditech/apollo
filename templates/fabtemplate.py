2788b9e8027e32d3ebe872cded1532e9d4192758
Tim Akinbo <takinbo@timbaobjects.com>
2013-09-23 22:31:57 +0100

    updates to the fabric template to support additional deployment instructions

--- templates/fabtemplate.py
+++ templates/fabtemplate.py
@@ -1,13 +1,9 @@
 import os
 import dotenv
-from fabric.api import local, put, cd, abort, run, sudo, env, shell_env
+import dj_database_url
+from fabric.api import local, put, cd, abort, run, sudo, env, shell_env, settings
 from fabric.contrib.files import exists
 from fabric.contrib.console import confirm
-
-dotenv.read_dotenv(os.path.realpath('.env'))
-
-env.hosts = [os.environ.get('FABRIC_HOSTS')]
-env.user = os.environ.get('FABRIC_USER')
 
 STAGING_ROOT = "${staging-root}"
 PRODUCTION_ROOT = "${production-root}"
@@ -53,19 +49,37 @@ def manage(command, server="staging", noprompt=False):
         with cd(app_path(server)):
             run('bin/%s %s' % (SCRIPT_NAME, command))
 
-def provision(server="app"):
+def provision(environment="app", server="staging"):
     # provisions an application server or database server
-    if confirm("Provision %s server?" % (server,)):
-        if server == "app":
+    if local('test -e .env-%s' % (server,), capture=True).succeeded:
+        dotenv.read_dotenv('.env-%s' % (server,))
+        database_config = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    if confirm("Provision %s server?" % (environment,)):
+        if environment == "app":
             sudo('apt-get update')
-            sudo('apt-get upgrade')
-            sudo('apt-get install nginx memcached python-dev build-essential')
-        elif server == "db":
+            sudo('apt-get install -y software-properties-common python-software-properties')
+            sudo('add-apt-repository -y ppa:ubuntugis/ppa')
             sudo('apt-get update')
-            sudo('apt-get upgrade')
-            sudo('apt-get install postgresql-server')
+            sudo('apt-get upgrade -y')
+            sudo('apt-get install -y vim nginx memcached python-dev build-essential git-core libpq-dev postgresql-client rabbitmq-server librabbitmq-dev libxml2-dev libxslt-dev libproj-dev binutils gdal-bin gettext')
+            sudo('openssl genrsa -des3 -out /etc/nginx/%s.key 2048' % (SCRIPT_NAME,))
+            sudo('openssl rsa -in /etc/nginx/%s.key -out /etc/nginx/%s.key' % (SCRIPT_NAME, SCRIPT_NAME))
+            sudo("openssl req -new -key /etc/nginx/%s.key -out /etc/nginx/%s.csr -subj '/CN=%s'" % (SCRIPT_NAME, SCRIPT_NAME, env.host))
+            sudo('openssl x509 -req -days 365 -in /etc/nginx/%s.csr -signkey /etc/nginx/%s.key -out /etc/nginx/%s.crt' % (SCRIPT_NAME, SCRIPT_NAME, SCRIPT_NAME))
+        elif environment == "db":
+            sudo('apt-get update')
+            sudo('apt-get install -y software-properties-common python-software-properties')
+            sudo('add-apt-repository -y ppa:ubuntugis/ppa')
+            sudo('apt-get update')
+            sudo('apt-get upgrade -y')
+            sudo('apt-get install -y vim postgresql postgresql-contrib postgis')
+
+            if database_config:
+                sudo('createdb %s' % (database_config['NAME'],), user='postgres')
+                sudo('psql -c "CREATE EXTENSION postgis;" %s' % (database_config['NAME'],), user='postgres')
+                sudo('psql -c "CREATE EXTENSION hstore;" %s' %(database_config['NAME'],), user='postgres')
         else:
-            abort('Choices available for server are ["app", "db"]')
+            abort('Choices available for environment are ["app", "db"]')
     else:
         abort("Aborting at user request")
 
@@ -93,13 +107,18 @@ def deploy(server="staging", version="HEAD"):
             with cd('%s/%s' % (root_dir, SCRIPT_NAME)):
                 if local('test -e .env-%s' % (server,), capture=True).succeeded:
                     put('.env-%s' % (server,), '.env')
+                else:
+                    run('touch .env')
                 run('./init')
-                run('set -a && source .env 2>/dev/null || bin/buildout -c production.cfg')
-                sudo('ln -sf ${buildout:parts-directory}/nginx/%s.conf /etc/nginx/sites-available/' % (SCRIPT_NAME,))
-                sudo('bin/honcho export -a %s -l ${buildout:directory}/var/log upstart /etc/init' % (SCRIPT_NAME,))
+                run('set -a && source .env 2>/dev/null && bin/buildout -c production.cfg')
+                sudo('ln -sf `pwd`/parts/nginx/%s.conf /etc/nginx/sites-enabled/' % (SCRIPT_NAME,))
+                sudo('bin/honcho export -a %s -l `pwd`/var/log upstart /etc/init' % (SCRIPT_NAME,))
                 run('mkdir -p assets')
+            with cd('%s/%s/eggs/Django-1.4.3-py2.7.egg/django/contrib/gis/db/backends/postgis/' % (root_dir, SCRIPT_NAME)):
+                with settings(warn_only=True):
+                    run('patch -fsr - <../../../../../../../../templates/creation.patch')
             with cd('%s/%s/src/%s' % (root_dir, SCRIPT_NAME, SCRIPT_NAME)):
-                manage('../../bin/%s compilemessages' % (SCRIPT_NAME,), server, True)
+                run('../../bin/%s compilemessages' % (SCRIPT_NAME,), server, True)
 
             manage('collectstatic --noinput -l', server, True)
             manage('syncdb --noinput', server, True)
