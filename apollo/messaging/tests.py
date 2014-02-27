90c4041be0e9cf26a8a4b1083a3bcf0a3c49b34d
Tim Akinbo <takinbo@timbaobjects.com>
2014-02-27 03:22:19 +0000

    APOLLO-32 #time 20m unit tests for sms parsing helper functions #start-progress

--- apollo/messaging/tests.py
+++ apollo/messaging/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from core.utils.test import MongoEngineTestCase
+from django.test import utils
 
-# Create your tests here.
+
+@utils.override_settings(DEBUG=True)
+class MessagingTest(MongoEngineTestCase):
+    def test_parse_text(self):
+        from messaging.utils import parse_text
+        self.assertEqual(
+            parse_text('ABC101AA1BA2'),
+            ('ABC', '101', 'CHECKLIST', 'AA1BA2', None))
+        self.assertEqual(
+            parse_text('ABC101!ABC'),
+            ('ABC', '101', 'INCIDENT', 'ABC', None))
+        self.assertEqual(
+            parse_text('ABC101AA1BA2@with a comment'),
+            ('ABC', '101', 'CHECKLIST', 'AA1BA2', 'with a comment'))
+        self.assertEqual(
+            parse_text('ABC101'),
+            ('ABC', '101', 'CHECKLIST', None, None))
+        self.assertEqual(
+            parse_text('ABC101@with a comment'),
+            ('ABC', '101', 'CHECKLIST', None, 'with a comment'))
+        self.assertEqual(
+            parse_text('ABC'),
+            (None, None, None, None, None))
+        self.assertEqual(
+            parse_text(''),
+            (None, None, None, None, None))
+        self.assertEqual(
+            parse_text('@with a comment'),
+            (None, None, None, None, 'with a comment'))
+
+    def test_parse_responses(self):
+        from messaging.utils import parse_responses
+        self.assertEqual(
+            parse_responses('AA1BA2CA200', 'CHECKLIST'),
+            {'AA': '1', 'BA': '2', 'CA': '200'})
+        self.assertEqual(
+            parse_responses('ABCDEF', 'INCIDENT'),
+            {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1, 'F': 1})
+        self.assertEqual(
+            parse_responses('', 'CHECKLIST'),
+            {})
+        self.assertEqual(
+            parse_responses('', 'INCIDENT'),
+            {})
+        self.assertEqual(
+            parse_responses('AA1BEA', 'CHECKLIST'),
+            {'AA': '1', 'BEA': ''})
+        self.assertEqual(
+            parse_responses('AA1BEA2', 'INCIDENT'),
+            {'A': 1, 'B': 1, 'E': 1})
