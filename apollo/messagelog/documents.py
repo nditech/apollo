91a9aad6296531e5985cacca1d58f6903dbeb1ee
Tim Akinbo <takinbo@timbaobjects.com>
2014-03-14 12:47:16 +0000

    removed django-apollo

--- apollo/messagelog/documents.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import *
-from datetime import datetime
-
-
-class Message(Document):
-    sender = StringField()
-    text = StringField()
-    timestamp = DateTimeField(default=datetime.utcnow())
