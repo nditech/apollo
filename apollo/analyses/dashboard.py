5952c8ad03fe0638f7291bdca7c7e827916d5854
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-03-17 17:19:52 +0000

    Added dashboard aggregation from feature/permissions branch

---
+++ apollo/analyses/dashboard.py
@@ -0,0 +1,106 @@
+from __future__ import unicode_literals
+from collections import OrderedDict
+from logging import getLogger
+from apollo.core.models import Submission
+
+logger = getLogger(__name__)
+
+
+def get_coverage(submission_queryset, group=None, location_type=None):
+    if group is None and location_type is None:
+        return _get_global_coverage(submission_queryset)
+    else:
+        return _get_group_coverage(submission_queryset, group, location_type)
+
+
+def _get_group_coverage(submission_queryset, group, location_type):
+    group_name = group.name
+    # build MongoDB aggregation pipeline
+    pipeline = [
+        {'$match': submission_queryset._query},
+        {'$group': {
+            '_id': {
+                'location': '$location_tree.{}'.format(location_type.name),
+                'completion': '$completion'
+            },
+            'total': {'$sum': 1}
+        }
+        },
+        {'$group': {
+            '_id': {
+                'location': '$_id.location',
+                group_name: '$_id.completion.{}'.format(group_name)
+            },
+            'total': {'$sum': '$total'}
+        }
+        },
+        {'$project': {
+            '_id': 0,
+            'location': '$_id.location',
+            'completion': '$_id.completion',
+            'total': '$total'
+        }
+        }
+    ]
+
+    try:
+        datasrc = Submission._get_collection().aggregate(pipeline)
+    except Exception, e:
+        logger.exception(e)
+        raise e
+
+    # reshape the result
+    result = datasrc.get('result')
+    if not result:
+        return None
+
+    locations = {r.get('location') for r in result}
+    coverage = OrderedDict({l: {} for l in locations})
+    for r in result:
+        l = r.pop('location')
+        coverage[l].update({r['completion']: r['total']})
+
+    return coverage
+
+
+def _get_global_coverage(submission_queryset):
+    # build the MongoDB aggregation pipeline
+    pipeline = [
+        {'$match': submission_queryset._query},
+        {'$group': {
+            '_id': '$completion',
+            'total': {'$sum': 1},
+        }},
+        {'$project': {
+            '_id': 0,
+            'completion': '$_id',
+            'total': '$total'
+        }}
+    ]
+
+    try:
+        datasrc = Submission._get_collection().aggregate(pipeline)
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    result = datasrc.get('result')
+    if not result:
+        return None
+
+    groups = result[0].get('completion').keys()
+
+    # reshape the result
+    coverage = OrderedDict()
+    for group in groups:
+        complete = sum((r.get('total') for r in result if r.get('completion').get(group) == 'Complete'))
+        partial = sum((r.get('total') for r in result if r.get('completion').get(group) == 'Partial'))
+        missing = sum((r.get('total') for r in result if r.get('completion').get(group) == 'Missing'))
+
+        coverage.update({group: {
+            'Complete': complete,
+            'Partial': partial,
+            'Missing': missing
+        }})
+
+    return coverage
