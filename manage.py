9e222886d80293da780f17cd156485031768a531
Tim Akinbo <takinbo@timbaobjects.com>
2014-03-21 13:53:30 +0000

    added a shell command with app, db, models and services context

--- manage.py
+++ manage.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager, Server, Shell
 
+from apollo.core import db
+from apollo import models, services
 from apollo.frontend import create_app
 from apollo.manage import \
     (CreateUserCommand, DeleteUserCommand, ListUsersCommand)
 
-manager = Manager(create_app())
+app = create_app()
+
+
+def _mk_ctx():
+    return dict(app=app, db=db, models=models, services=services)
+
+manager = Manager(app)
 manager.add_command('run', Server())
+manager.add_command('shell', Shell(make_context=_mk_ctx))
 manager.add_command('createuser', CreateUserCommand())
 manager.add_command('deleteuser', DeleteUserCommand())
 manager.add_command('listusers', ListUsersCommand())
