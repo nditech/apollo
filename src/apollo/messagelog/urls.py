f480edcb51c1d5cf696dd81ac025f6efba16c230
Tim Akinbo <takinbo@timbaobjects.com>
2014-03-31 18:43:00 +0000

    cleanup of legacy app

--- src/apollo/messagelog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-from .views import *
-
-urlpatterns = patterns('',
-    url(r'^$', MessageListView.as_view(), name='messages_list'),
-)
--- src/apollo/tabimport/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .forms import FileuploadForm, MatchingForm
-from .views import DataImportView
-
-
-urlpatterns = patterns('',
-    url(r'^$', DataImportView.as_view([FileuploadForm, MatchingForm]), name='tabimport'),
-)
