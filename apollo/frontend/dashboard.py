195e803caf4f39d551992233e4ead3bf534043e8
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-04-02 12:28:40 +0000

    Removed print statement

--- apollo/frontend/dashboard.py
+++ apollo/frontend/dashboard.py
@@ -83,7 +83,6 @@ def index():
 
         data = get_coverage(queryset, group, sub_location_type)
 
-    print 'data is {}'.format(data)
     # load the page context
     context = {
         'args': args,
