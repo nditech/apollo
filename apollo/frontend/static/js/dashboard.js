205cd0eafaa2dd5e2bf23f4ca2f0b1cfe974b04c
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-04-02 07:48:11 +0000

    Tweaked dashboard chart settings

--- apollo/frontend/static/js/dashboard.js
+++ apollo/frontend/static/js/dashboard.js
@@ -2,7 +2,7 @@ $(function () {
 	$('.piechart').each(function() {
 		var data = [];
 		var element = this;
-		var ds = JSON.parse(this.dataset.chart);
+		var ds = JSON.parse(element.dataset.chart);
 		for (prop in ds)
 			if (ds.hasOwnProperty(prop) && prop != 'name')
 				data.push({label: prop, value: ds[prop]});
@@ -11,12 +11,15 @@ $(function () {
 			var chart = nv.models.pieChart()
 				.x(function(d) { return d.label; })
 				.y(function(d) { return d.value; })
-				.width(150)
-				.height(150)
+				.width(400)
+				.height(400)
+				.color(['#4e9a06', '#c00', '#f57900'])
 				.showLabels(false);
 
 			d3.select(element).datum(data)
 				.transition().duration(350)
+				.attr('height', 400)
+				.attr('width', 400)
 				.call(chart);
 
 			return chart;
