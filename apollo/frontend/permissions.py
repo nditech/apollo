22b733779d338a365aac0464569ad42691659405
Tim Akinbo <takinbo@timbaobjects.com>
2014-04-10 09:56:32 +0000

    added more permissions

--- apollo/frontend/permissions.py
+++ apollo/frontend/permissions.py
@@ -2,3 +2,6 @@ from flask.ext.principal import Permission, ActionNeed
 
 view_events = Permission(ActionNeed('view_events'))
 send_messages = Permission(ActionNeed('send_messages'))
+add_submission = Permission(ActionNeed('add_submission'))
+edit_submission = Permission(ActionNeed('edit_submission'))
+export_submissions = Permission(ActionNeed('export_submissions'))
