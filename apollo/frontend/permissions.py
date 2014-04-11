fa5735e99e83cbe763a2a708f018e53e15735db5
Tim Akinbo <takinbo@timbaobjects.com>
2014-04-11 09:47:27 +0000

    the admin role should have all permissions

--- apollo/frontend/permissions.py
+++ apollo/frontend/permissions.py
@@ -1,11 +1,15 @@
-from flask.ext.principal import Permission, ActionNeed
+from flask.ext.principal import Permission, ActionNeed, RoleNeed
 
-view_events = Permission(ActionNeed('view_events'))
-send_messages = Permission(ActionNeed('send_messages'))
-edit_participant = Permission(ActionNeed('edit_participant'))
-export_participants = Permission(ActionNeed('export_participants'))
+view_events = Permission(ActionNeed('view_events'), RoleNeed('admin'))
+send_messages = Permission(ActionNeed('send_messages'), RoleNeed('admin'))
+edit_participant = Permission(
+    ActionNeed('edit_participant'), RoleNeed('admin'))
+export_participants = Permission(
+    ActionNeed('export_participants'), RoleNeed('admin'))
 
 # submissions
-add_submission = Permission(ActionNeed('add_submission'))
-edit_submission = Permission(ActionNeed('edit_submission'))
-export_submissions = Permission(ActionNeed('export_submissions'))
+add_submission = Permission(ActionNeed('add_submission'), RoleNeed('admin'))
+edit_submission = Permission(
+    ActionNeed('edit_submission'), RoleNeed('admin'))
+export_submissions = Permission(
+    ActionNeed('export_submissions'), RoleNeed('admin'))
