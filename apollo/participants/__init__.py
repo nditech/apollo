0cd68be7efad605dc48418917e53771a7cff8464
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-04-13 10:16:35 +0000

    Stubbed out export for participants

--- apollo/participants/__init__.py
+++ apollo/participants/__init__.py
@@ -1,9 +1,41 @@
+from tablib import Dataset
 from ..core import Service
 from .models import Participant, ParticipantRole, ParticipantPartner
 
 
 class ParticipantsService(Service):
     __model__ = Participant
+
+    def export_list(self, queryset):
+        ds = Dataset(
+            headers=[
+                'Participant ID', 'Name', 'Role', 'Partner',
+                'Location ID', 'Supervisor ID', 'Gender', 'Email', 'Phone',
+                'Phone', 'Phone'
+            ]
+        )
+
+        for participant in queryset:
+            # limit to three numbers for export and pad if less than three
+            phone_numbers = [i.number for i in participant.phones][:3]
+            phone_numbers += [''] * (3 - len(phone_numbers))
+
+            record = [
+                participant.participant_id,
+                participant.name,
+                participant.role.name,
+                participant.partner.name if participant.partner else '',
+                participant.location.code if participant.location else '',
+                participant.supervisor.participant_id if participant.supervisor else '',
+                participant.gender,
+                participant.email
+            ]
+
+            record.extend(phone_numbers)
+
+            ds.append(record)
+
+        return ds
 
 
 class ParticipantRolesService(Service):
