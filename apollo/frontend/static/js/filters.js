95f76ce299831a2ff02a3c9a1eb8f77f350ef308
Joy Okomowho <jokomowho@timbaobjects.com>
2014-04-22 18:18:56 +0000

    'clear' button for participants list template now resets the form

--- apollo/frontend/static/js/filters.js
+++ apollo/frontend/static/js/filters.js
@@ -1,9 +1,8 @@
 $(function () {
 	$('#filter_reset').on('click', function() {
-		var form = $(this).parent();
-		form.find(':input').not('button').each(function() { $(this).val(''); })
-		form.submit();
+		var $form = $(this).parents('.form-inline');
+		$form.find(':input').not('button').each(function() { $(this).val(''); })
+		$form.submit();
 	});
-
 	$('select.select2').select2();
 });
