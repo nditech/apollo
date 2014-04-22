ac398f5fb5eee225979a6510b71df2f150aa5c11
Joy Okomowho <jokomowho@timbaobjects.com>
2014-04-22 11:20:33 +0000

    Migrated dashboard charts from Protovis to D3

--- apollo/frontend/static/js/dashboard.js
+++ apollo/frontend/static/js/dashboard.js
@@ -1,26 +1,97 @@
-$(function () {
-	$('.piechart').each(function() {
-		var data = [];
-		var element = this;
-		var ds = JSON.parse(element.dataset.chart);
-		for (prop in ds)
-			if (ds.hasOwnProperty(prop) && prop != 'name')
-				data.push({label: prop, value: ds[prop]});
+  function drawPieChart(el) {
+    var color = d3.scale.ordinal().range(["#FF5200", "#FFE22B", "#3BDF4A"]);
+    var w = 140;
+    var h = 300;
 
-		nv.addGraph(function() {
-			var chart = nv.models.pieChart()
-				.x(function(d) { return d.label + ' (' + d.value + ')'; })
-				.y(function(d) { return d.value; })
-				.valueFormat(d3.format('^d'))
-				.color(['#4e9a06', '#c00', '#f57900'])
-				.margin({top: 40, bottom: 40, left: 15, right: 15})
-				.showLabels(false);
+    var outerRadius = w / 2;
+    var innerRadius = 0;
+    
+    var json = JSON.parse(el.dataset.chart);
+    var data = [json.Missing, json.Partial, json.Complete];
+    var labels = ['Missing', 'Partial', 'Complete'];
+    var total = data.reduce(function (prev, curr, idx, arr) { return prev + curr; });
 
-			d3.select(element).datum(data)
-				.transition().duration(350)
-				.call(chart);
+    function number_format(num) {
+      if (num.toFixed(0) == num || num < 1)
+        return num.toFixed(0);
+      else
+        return num.toFixed(1);
+    }
 
-			return chart;
-		});
-	});
-});
+    var svg = d3.select(el)
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h/2)
+
+    //Draw arc paths
+    svg.selectAll("g.pieArc")
+      .data(d3.layout.pie()(data))
+      .enter()
+      .append("g")
+      .attr("class", "arc")
+      .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")
+      
+      .append("path")
+      .attr("fill", function(d, i) {
+        return color(i);
+      })
+      .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius));
+
+    //Labels
+    svg.selectAll("g.pieLabel")
+      .data(d3.layout.pie()(data))
+      .enter()
+      .append("g")
+      .attr("class", "arc")
+      .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")
+
+    
+      .append("text")
+      .attr("transform", function(d) {
+        return "translate(" + d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius).centroid(d) + ")";
+      })
+      .attr("text-anchor", "middle")
+      .text(function(d) {return (d.value/total * 100).toFixed(0) + '%';})
+      .style('font-size', '10px');
+
+    //Legend
+    var legend = svg.append("g")
+      .attr("width", w)
+      .attr("height", h /4)
+      .selectAll("g")
+      .data(color.domain().slice().reverse())
+      .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("circle")
+      .attr("cx", w/4 - 23)
+      .attr("cy", h/2 + 24)
+      .attr("r", 5)
+      .style("fill", color);
+
+    //Legend Label
+    legend.append("text")
+      .data(labels)
+      .attr("x", w/4 - 12)
+      .attr("y", h/2 + 24)
+      .attr("dy", ".35em")
+      .style('font-family', 'courier')
+      .text(function(d) { return d})
+      .style('font-size', '12.5px');
+
+    //Legend data
+    legend.append("text")
+      .data(data)
+      .attr("x", w /2 + 23)
+      .attr("y", h/2 + 24)
+      .attr("dy", ".35em")
+      .style('font-family', 'courier')
+      .text(function(d) { return '· ' +  d})
+      .style('font-size', '12.5px');
+  }
+
+  $(function () {
+    $('.piechart').each(function(idx, el) {
+      drawPieChart(el);
+    });
+  });
