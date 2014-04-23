3e68a02ad3e8cac33c9a583466b09c3a576a75bb
Tim Akinbo <takinbo@timbaobjects.com>
2014-04-23 13:25:58 +0000

    only load files beginning with 'views_' in search of flask blueprints

--- apollo/helpers.py
+++ apollo/helpers.py
@@ -14,12 +14,13 @@ def register_blueprints(app, package_name, package_path):
     """
     rv = []
     for _, name, _ in pkgutil.iter_modules(package_path):
-        m = importlib.import_module('%s.%s' % (package_name, name))
-        for item in dir(m):
-            item = getattr(m, item)
-            if isinstance(item, Blueprint):
-                app.register_blueprint(item)
-            rv.append(item)
+        if name.startswith('views_'):
+            m = importlib.import_module('%s.%s' % (package_name, name))
+            for item in dir(m):
+                item = getattr(m, item)
+                if isinstance(item, Blueprint):
+                    app.register_blueprint(item)
+                rv.append(item)
     return rv
 
 
