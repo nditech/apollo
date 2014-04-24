eb92008cdad4caebce384f0019e5e0eea2e00f0c
Tim Akinbo <takinbo@timbaobjects.com>
2014-04-24 15:34:43 +0000

    permissions and menu options for editing locations

--- apollo/frontend/views_locations.py
+++ apollo/frontend/views_locations.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from flask import Blueprint, g, redirect, render_template, request, url_for
 from flask.ext.babel import lazy_gettext as _
 from flask.ext.security import login_required
+from flask.ext.menu import register_menu
 from ..models import Location
-from . import route
+from . import route, permissions
 from .forms import generate_location_edit_form
 
 bp = Blueprint('locations', __name__, template_folder='templates',
@@ -13,6 +14,10 @@ bp = Blueprint('locations', __name__, template_folder='templates',
 
 
 @route(bp, '/location/<pk>', methods=['GET', 'POST'])
+@register_menu(
+    bp, 'edit_location', _('Edit Location'),
+    visible_when=lambda: permissions.edit_locations.can())
+@permissions.edit_locations.require(403)
 @login_required
 def location_edit(pk):
     template_name = 'core/location_edit.html'
@@ -34,10 +39,14 @@ def location_edit(pk):
     return render_template(template_name, form=form, page_title=page_title)
 
 
-@route(bp, '/locations/builder')
+@route(bp, '/locationsbuilder')
+@register_menu(
+    bp, 'locations_builder', _('Locations Builder'),
+    visible_when=lambda: permissions.edit_locations.can())
+@permissions.edit_locations.require(403)
 @login_required
 def locations_builder():
     template_name = 'frontend/location_builder.html'
-    page_title = _('Locations Structure Builder')
+    page_title = _('Locations Builder')
 
     return render_template(template_name, page_title=page_title)
