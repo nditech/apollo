ab5889d3825cdf80ada82610eb692ceabe63525e
Tim Akinbo <takinbo@timbaobjects.com>
2014-04-29 19:35:17 +0100

    bug fixes and refactoring of locations import to use a better cache key

--- apollo/locations/tasks.py
+++ apollo/locations/tasks.py
@@ -2,32 +2,46 @@ from hashlib import sha256
 import pandas as pd
 from .. import services
 
+field_transform = {
+    'code': lambda s: str(s),
+    'political_code': lambda s: str(s),
+    'registered_voters': lambda i: int(i)
+}
+
 
 class LocationCache():
     cache = {}
 
-    def _cache_key(self, location_type, location_name):
+    def _cache_key(self, location_code, location_type, location_name):
         # remove smart quotes
-        location_type = location_type.replace(
+        location_code = unicode(location_code).replace(
             u"\u2018", "").replace(
             u"\u2019", "").replace(u"\u201c", "").replace(u"\u201d", "")
-        location_name = location_name.replace(
+        location_type = unicode(location_type).replace(
+            u"\u2018", "").replace(
+            u"\u2019", "").replace(u"\u201c", "").replace(u"\u201d", "")
+        location_name = unicode(location_name).replace(
             u"\u2018", "").replace(
             u"\u2019", "").replace(u"\u201c", "").replace(u"\u201d", "")
         return sha256(
-            '{}:{}'.format(location_type, location_name)).hexdigest()
+            '{}:{}:{}'.format(
+                location_code, location_type, location_name)).hexdigest()
 
-    def get(self, location_type, location_name):
-        cache_key = self._cache_key(location_type, location_name)
+    def get(self, location_code, location_type, location_name):
+        cache_key = self._cache_key(
+            location_code, location_type, location_name)
         return self.cache.get(cache_key, None)
 
-    def has(self, location_type, location_name):
-        cache_key = self._cache_key(location_type, location_name)
+    def has(self, location_code, location_type, location_name):
+        cache_key = self._cache_key(
+            location_code, location_type, location_name)
         return cache_key in self.cache
 
     def set(self, location_obj):
         cache_key = self._cache_key(
-            location_obj.location_type, location_obj.name)
+            location_obj.code or '',
+            location_obj.location_type or '',
+            location_obj.name or '')
         self.cache[cache_key] = location_obj
 
 
@@ -36,8 +50,9 @@ def location_kwargs(item, df_series):
     items_to_pop = []
     for key in mapping_item:
         if mapping_item[key]:
-            if callable(mapping_item[key]):
-                mapping_item[key] = mapping_item[key](df_series)
+            if key in field_transform:
+                mapping_item[key] = field_transform.get(key)(
+                    df_series.get(mapping_item[key]))
             else:
                 mapping_item[key] = df_series.get(mapping_item[key])
         else:
@@ -53,19 +68,26 @@ def update_locations(fh, mapping, event):
 
     for idx in df.index:
         for lt in mapping.keys():
-            if not cache.has(lt, df.ix[idx][mapping[lt]['name']]):
+            if not cache.has(
+                df.ix[idx][mapping[lt]['code']] if mapping[lt]['code'] else '',
+                lt or '',
+                df.ix[idx][mapping[lt]['name']] if mapping[lt]['name'] else ''
+            ):
                 kwargs = location_kwargs(mapping[lt], df.ix[idx])
                 kwargs['location_type'] = lt
                 kwargs['deployment'] = event.deployment
                 loc = services.locations.get_or_create(**kwargs)
                 cache.set(loc)
         for lt in mapping.keys():
-            loc = cache.get(lt, df.ix[idx][mapping[lt]['name']])
+            loc = cache.get(
+                df.ix[idx][mapping[lt]['code']] if mapping[lt]['code'] else '',
+                lt or '',
+                df.ix[idx][mapping[lt]['name']] if mapping[lt]['name'] else '')
             ancestor_types = services.location_types.get(name=lt).ancestors_ref
             anc = filter(
-                lambda a: a is not None,
-                [cache.get(lt.name, df.ix[idx][mapping[lt.name]['name']])
-                 for lt in ancestor_types])
-            loc.ancestors_ref = anc
-            loc.events.append(event)
-            loc.save()
+                lambda a: a, [cache.get(
+                    df.ix[idx][mapping[lt.name]['code']] if mapping[lt.name]['code'] else '',
+                    lt.name or '',
+                    df.ix[idx][mapping[lt.name]['name']] if mapping[lt.name]['name'] else '')
+                    for lt in ancestor_types])
+            loc.update(add_to_set__events=event, add_to_set__ancestors_ref=anc)
