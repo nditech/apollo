d29230662bcffef786876706a74c10fa230e83c6
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-05-02 15:17:38 +0000

    Added more comments to help maintainers

--- apollo/frontend/views_admin.py
+++ apollo/frontend/views_admin.py
@@ -18,18 +18,28 @@ class MyView(BaseView):
 
 
 class EventAdminView(ModelView):
+    # disallow event creation
     can_create = False
+
+    # what fields to be displayed in the list view
     column_list = ('name', 'start_date', 'end_date')
+
+    # what fields filtering is allowed by
     column_filters = ('name', 'start_date', 'end_date')
+
+    # rules for form editing. in this case, only the listed fields
+    # and the header for the field set
     form_rules = [
         rules.FieldSet(('name', 'start_date', 'end_date'), _('Event'))
     ]
 
     def get_query(self):
+        '''Returns the queryset of the objects to list.'''
         user = current_user._get_current_object()
         return models.Event.objects(deployment=user.deployment)
 
     def is_accessible(self):
+        '''For checking if the admin view is accessible.'''
         role = models.Role.objects.get(name='admin')
         return current_user.is_authenticated() and current_user.has_role(role)
 
