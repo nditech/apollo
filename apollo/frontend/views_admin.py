1c789b8821c968db840fc8dd503268277f8b0b31
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-05-02 15:11:16 +0000

    Added editing of events to admin view

--- apollo/frontend/views_admin.py
+++ apollo/frontend/views_admin.py
@@ -1,7 +1,10 @@
 from flask.ext.admin import BaseView, expose
+from flask.ext.admin.contrib.mongoengine import ModelView
+from flask.ext.admin.form import rules
+from flask.ext.babel import lazy_gettext as _
 from flask.ext.security import current_user
 from ..core import admin
-from ..models import Role
+from .. import models
 
 
 class MyView(BaseView):
@@ -10,8 +13,25 @@ class MyView(BaseView):
         return self.render('admin/index.html')
 
     def is_accessible(self):
-        role = Role.objects.get(name='admin')
+        role = models.Role.objects.get(name='admin')
         return current_user.is_authenticated() and current_user.has_role(role)
 
 
+class EventAdminView(ModelView):
+    can_create = False
+    column_list = ('name', 'start_date', 'end_date')
+    column_filters = ('name', 'start_date', 'end_date')
+    form_rules = [
+        rules.FieldSet(('name', 'start_date', 'end_date'), _('Event'))
+    ]
+
+    def get_query(self):
+        user = current_user._get_current_object()
+        return models.Event.objects(deployment=user.deployment)
+
+    def is_accessible(self):
+        role = models.Role.objects.get(name='admin')
+        return current_user.is_authenticated() and current_user.has_role(role)
+
 admin.add_view(MyView(name='Hello'))
+admin.add_view(EventAdminView(models.Event))
