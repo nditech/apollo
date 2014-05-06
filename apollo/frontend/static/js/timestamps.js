f477709c7beef98aaf31b94a33c1b0369894a5e9
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-05-06 10:02:53 +0000

    Added JavaScript code for rendering UNIX timestamps into local time

--- apollo/frontend/static/js/timestamps.js
+++ apollo/frontend/static/js/timestamps.js
@@ -4,3 +4,10 @@ $(function () {
 		this.innerText = timestamp.fromNow();
 	});
 });
+
+$(function() {
+	$('.timestamp').each(function(index) {
+		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		this.innerText = timestamp.format('llll');
+	});
+});
