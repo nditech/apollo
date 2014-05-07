c58580f7552952e2231dc38ff9740fb38ebea00d
Tim Akinbo <takinbo@timbaobjects.com>
2014-05-07 16:38:01 +0100

    i18n refactoring and improvements to momentjs

--- apollo/frontend/static/js/timestamps.js
+++ apollo/frontend/static/js/timestamps.js
@@ -3,23 +3,14 @@ $(function () {
 		var timestamp = moment.unix(Number.parseInt(this.innerText));
 		this.innerText = timestamp.fromNow();
 	});
-});
-
-$(function() {
 	$('.timestamp').each(function(index) {
 		var timestamp = moment.unix(Number.parseInt(this.innerText));
 		this.innerText = timestamp.format('llll');
 	});
-});
-
-$(function() {
 	$('.timestamp-date').each(function(index) {
 		var timestamp = moment.unix(Number.parseInt(this.innerText));
 		this.innerText = timestamp.format('ll');
 	});
-});
-
-$(function() {
 	$('.timestamp-time').each(function(index) {
 		var timestamp = moment.unix(Number.parseInt(this.innerText));
 		this.innerText = timestamp.format('LT');
