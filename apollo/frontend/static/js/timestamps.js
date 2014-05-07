b5d798cb891b46ed9df8a91b7682cca0845f256e
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-05-07 11:26:33 +0000

    Converted message date and time rendering to use moment.js in message log

--- apollo/frontend/static/js/timestamps.js
+++ apollo/frontend/static/js/timestamps.js
@@ -11,3 +11,18 @@ $(function() {
 		this.innerText = timestamp.format('llll');
 	});
 });
+
+$(function() {
+	$('.timestamp-date').each(function(index) {
+		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		this.innerText = timestamp.format('ll');
+	});
+});
+
+$(function() {
+	$('.timestamp-time').each(function(index) {
+		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		this.innerText = timestamp.format('LT');
+	});
+});
+
