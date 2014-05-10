37afbaff2f6a06952d0cc245bc742549cd759aaf
Tim Akinbo <takinbo@timbaobjects.com>
2014-05-10 18:01:27 +0000

    fix for assigning a sane default time before momentjs kicks in to replace with a localized version

--- apollo/frontend/static/js/timestamps.js
+++ apollo/frontend/static/js/timestamps.js
@@ -1,18 +1,18 @@
 $(function () {
 	$('.tonow').each(function (index) {
-		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		var timestamp = moment.unix(Number.parseInt($(this).data('timestamp')));
 		this.innerText = timestamp.fromNow();
 	});
 	$('.timestamp').each(function(index) {
-		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		var timestamp = moment.unix(Number.parseInt($(this).data('timestamp')));
 		this.innerText = timestamp.format('llll');
 	});
 	$('.timestamp-date').each(function(index) {
-		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		var timestamp = moment.unix(Number.parseInt($(this).data('timestamp')));
 		this.innerText = timestamp.format('ll');
 	});
 	$('.timestamp-time').each(function(index) {
-		var timestamp = moment.unix(Number.parseInt(this.innerText));
+		var timestamp = moment.unix(Number.parseInt($(this).data('timestamp')));
 		this.innerText = timestamp.format('LT');
 	});
 });
