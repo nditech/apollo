6a9b848a5514139ef83cba283bd8e1c5b0afd82b
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-06-21 12:59:33 +0000

    Defaulted completion rating

--- apollo/participants/models.py
+++ apollo/participants/models.py
@@ -93,7 +93,7 @@ class Participant(db.DynamicDocument):
     event = db.ReferenceField(Event)
     deployment = db.ReferenceField(Deployment)
 
-    completion_rating = db.FloatField()
+    completion_rating = db.FloatField(default=100)
 
     meta = {
         'indexes': [
