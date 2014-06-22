dae0466cc9f2b760b4ea703c3a18e2193ab100a3
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-06-22 23:48:50 +0000

    Removed unused import

--- apollo/frontend/views_odk.py
+++ apollo/frontend/views_odk.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from hashlib import md5
 import json
 from flask import Blueprint, g, make_response, render_template, request
 from lxml import etree
