879a09443f6bc69bc72203afe47ad0c22b3ed5a3
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-10-20 15:34:47 +0100

    SMS chart now shows both incoming and outgoing messages (not cumulatively summed)

--- apollo/frontend/views_messages.py
+++ apollo/frontend/views_messages.py
@@ -52,22 +52,29 @@ def message_list():
 
 
 def message_time_series(message_queryset):
-    subset = message_queryset(direction='IN')
-    df = pd.DataFrame(list(subset.only('received').as_pymongo()))
+    df = pd.DataFrame(
+        list(message_queryset.only('received', 'direction').as_pymongo())
+    )
 
     # set a marker for each message
     df['marker'] = 1
 
     # set the index to the message timestamp, resample to hourly
     # and sum all markers in each hour, filling NaNs with 0
-    df = df.set_index('received').resample('1Min', how='sum').fillna(0)
-
-    # do a cumulative sum of each row in the dataframe
-    d = df.cumsum()
+    df = df.set_index('received')
+    incoming = df[df.direction == 'IN']
+    outgoing = df[df.direction == 'OUT']
+    incoming = incoming.resample('1Min', how='sum').fillna(0)
+    outgoing = outgoing.resample('1Min', how='sum').fillna(0)
 
     # return as UNIX timestamp, value pairs
-    data = [(calendar.timegm(i[0].utctimetuple()) * 1000, int(i[1]))
-            for i in sorted(d.to_dict()['marker'].items())
-            ]
+    data = {
+        'incoming': [(calendar.timegm(i[0].utctimetuple()) * 1000, int(i[1]))
+                     for i in sorted(incoming.to_dict()['marker'].items())
+                    ],
+        'outgoing': [(calendar.timegm(i[0].utctimetuple()) * 1000, int(i[1]))
+                     for i in sorted(outgoing.to_dict()['marker'].items())
+                    ]
+    }
 
     return data
