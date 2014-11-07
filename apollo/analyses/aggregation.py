9024b6ab1aa99eddacf45086586ec708eaca5b43
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-11-07 15:51:47 +0100

    Field stat generation output of numeric fields now matches that using Pandas

--- apollo/analyses/aggregation.py
+++ apollo/analyses/aggregation.py
@@ -16,7 +16,26 @@ def generate_numeric_field_stats(queryset, tag):
 
     collection = queryset._collection
 
-    return collection.aggregate(pipeline)
+    result = collection.aggregate(pipeline).get('result')
+    data = {
+        'type': 'numeric',
+        'missing': 0,
+        'reported': 0,
+        'mean': 0,
+        'percent_missing': 0,
+        'percent_reported': 0
+    }
+
+    for r in result:
+        if r['_id'] == 'missing':
+            data['missing'] = r['count']
+            data['percent_missing'] = float(r['count']) / queryset.count()
+        if r['_id'] == 'reported':
+            data['reported'] = r['count']
+            data['mean'] = r['mean']
+            data['percent_reported'] = float(r['count']) / queryset.count()
+
+    return data
 
 
 def generate_single_choice_field_stats(queryset, tag):
@@ -36,7 +55,22 @@ def generate_single_choice_field_stats(queryset, tag):
 
     collection = queryset._collection
 
-    return collection.aggregate(pipeline)
+    result = collection.aggregate(pipeline).get('result')
+    form = queryset[0].form
+    field = form.get_field_by_tag(tag)
+    data = {
+        'labels': field.options.keys(),  # need to change this so it's sorted
+        'missing': 0,
+        'reported': 0,
+        'total': queryset.count(),
+        'percent_missing': 0,
+        'percent_reported': 0,
+        'type': 'single-choice'
+    }
+
+    
+
+    return data
 
 
 def generate_multi_choice_field_stats(queryset, tag):
