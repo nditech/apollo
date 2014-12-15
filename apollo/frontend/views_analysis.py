ba9a01607a91f7c5f34952a6403951ef5bfe1119
Tim Akinbo <takinbo@timbaobjects.com>
2014-12-15 02:45:31 +0100

    margin of error addition

--- apollo/frontend/views_analysis.py
+++ apollo/frontend/views_analysis.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 from collections import OrderedDict
 from functools import partial
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, current_app
 from flask.ext.babel import lazy_gettext as _
 from flask.ext.menu import register_menu
 from flask.ext.security import login_required
@@ -13,7 +13,12 @@ from mongoengine import Q
 from ..submissions.models import FLAG_STATUSES
 from ..services import forms, locations, location_types, submissions
 from . import route, permissions, filters
-from .helpers import analysis_breadcrumb_data, analysis_navigation_data
+from .helpers import (
+    analysis_breadcrumb_data,
+    analysis_navigation_data
+)
+import math
+import pandas as pd
 
 
 def get_analysis_menu():
@@ -177,6 +182,8 @@ def _voting_results(form_pk, location_pk=None):
 
     overall_summation = dataset.groupby(
         location.location_type).sum().ix[location.name]
+    valid_dataframe = dataset[dataset.reported==True].query(u'%s==u"%s"' % (
+        location.location_type, location.name))
     valid_summation = dataset[dataset.reported==True].groupby(
         location.location_type).sum().ix[location.name]
     reporting = overall_summation[['missing', 'reported']]
@@ -205,6 +212,19 @@ def _voting_results(form_pk, location_pk=None):
         valid_summation[result_field_labels + [rejected_votes_field]].sum(
             axis=1)
     }
+    if form.calculate_moe and current_app.config.get('ENABLE_MOE'):
+        data_analyses['overall']['all_valid_votes_moe_95'] = _margin_of_error(
+            valid_dataframe, result_field_labels,
+            result_field_labels + [rejected_votes_field])
+        data_analyses['overall']['all_valid_votes_moe_99'] = _margin_of_error(
+            valid_dataframe, result_field_labels,
+            result_field_labels + [rejected_votes_field], 258.0)
+        data_analyses['overall']['total_rejected_moe_95'] = _margin_of_error(
+            valid_dataframe, rejected_votes_field,
+            result_field_labels + [rejected_votes_field])
+        data_analyses['overall']['total_rejected_moe_99'] = _margin_of_error(
+            valid_dataframe, rejected_votes_field,
+            result_field_labels + [rejected_votes_field], 258.0)
 
     for result_field_label in result_field_labels:
         data_analyses['overall'][u'{}_cnt'.format(result_field_label)] = \
@@ -212,6 +232,15 @@ def _voting_results(form_pk, location_pk=None):
         data_analyses['overall'][u'{}_pct'.format(result_field_label)] = \
             valid_summation.get(result_field_label, 0) / float(
                 data_analyses['overall']['all_valid_votes'])  # all_votes?
+        if form.calculate_moe and current_app.config.get('ENABLE_MOE'):
+            data_analyses['overall'][u'{}_moe_95'.format(
+                result_field_label)] = _margin_of_error(
+                valid_dataframe, result_field_label,
+                result_field_labels + [rejected_votes_field])
+            data_analyses['overall'][u'{}_moe_99'.format(
+                result_field_label)] = _margin_of_error(
+                valid_dataframe, result_field_label,
+                result_field_labels + [rejected_votes_field], 258.0)
 
     # grouped summaries
     for location_type in location_tree.keys():
@@ -226,6 +255,8 @@ def _voting_results(form_pk, location_pk=None):
             for sublocation in location_tree[location_type]:
                 _overall = grouped_summation.ix[sublocation.name]
                 _valid = grouped_valid_summation.ix[sublocation.name]
+                _valid_dataframe = dataset[dataset.reported==True].query(
+                    u'%s==u"%s"' % (location_type, sublocation.name))
 
                 _reporting = _overall[['missing', 'reported']]
                 _reporting['reported_pct'] = _reporting['reported'] / (
@@ -256,6 +287,26 @@ def _voting_results(form_pk, location_pk=None):
                         axis=1)
                 }
 
+                if form.calculate_moe and current_app.config.get('ENABLE_MOE'):
+                    _sublocation_report['all_valid_votes_moe_95'] = \
+                        _margin_of_error(
+                            _valid_dataframe, result_field_labels,
+                            result_field_labels + [rejected_votes_field])
+                    _sublocation_report['all_valid_votes_moe_99'] = \
+                        _margin_of_error(
+                            _valid_dataframe, result_field_labels,
+                            result_field_labels + [rejected_votes_field],
+                            258.0)
+                    _sublocation_report['total_rejected_moe_95'] = \
+                        _margin_of_error(
+                            _valid_dataframe, rejected_votes_field,
+                            result_field_labels + [rejected_votes_field])
+                    _sublocation_report['total_rejected_moe_99'] = \
+                        _margin_of_error(
+                            _valid_dataframe, rejected_votes_field,
+                            result_field_labels + [rejected_votes_field],
+                            258.0)
+
                 for result_field_label in result_field_labels:
                     _sublocation_report[u'{}_cnt'.format(
                         result_field_label)] = _valid.get(
@@ -264,6 +315,20 @@ def _voting_results(form_pk, location_pk=None):
                         result_field_label)] = _valid.get(
                         result_field_label, 0) / float(
                         _sublocation_report['all_valid_votes'])
+
+                    if (
+                        form.calculate_moe and current_app.config.get(
+                            'ENABLE_MOE')
+                    ):
+                        _sublocation_report[u'{}_moe_95'.format(
+                            result_field_label)] = _margin_of_error(
+                            _valid_dataframe, result_field_label,
+                            result_field_labels + [rejected_votes_field])
+                        _sublocation_report[u'{}_moe_99'.format(
+                            result_field_label)] = _margin_of_error(
+                            _valid_dataframe, result_field_label,
+                            result_field_labels + [rejected_votes_field],
+                            258.0)
                 data_analyses['grouped'][location_type].append(
                     _sublocation_report)
         except IndexError:
@@ -352,3 +417,44 @@ def results_analysis(form_id):
 @login_required
 def results_analysis_with_location(form_id, location_id):
     return _voting_results(form_id, location_id)
+
+
+def _proportion(dataframe, numerator, denominator):
+    m = dataframe.ix[:, denominator].sum(axis=1)
+    mbar = m.sum(axis=0) / m.size
+    return dataframe.ix[:, numerator].sum(axis=0).sum(axis=0) / (mbar * m.size)
+
+
+def _variance(dataframe, numerator, denominator):
+    p = _proportion(dataframe, numerator, denominator)
+    psquared = p * p
+
+    m = dataframe.ix[:, denominator].sum(axis=1)
+    msquared = m * m
+
+    a = dataframe.ix[:, numerator].sum(axis=1)
+    asquared = a * a
+
+    mbar = m.sum(axis=0) / m.size
+    mbarsquared = mbar * mbar
+
+    f = m.sum(axis=0) / current_app.config.get('BIG_N')
+    k = m.size
+    am = a * m
+    sigma_asquared = asquared.sum(axis=0)
+    sigma_msquared = msquared.sum(axis=0)
+    sigma_am = am.sum(axis=0)
+
+    return (
+        (1 - f) / (k * mbarsquared)) * ((sigma_asquared -
+                                        2 * p * sigma_am +
+                                        psquared *
+                                        sigma_msquared) / (k - 1))
+
+
+def _margin_of_error(dataframe, numerator, denominator, cv=196.0):
+    moe = round(math.sqrt(abs(_variance(
+        dataframe, numerator, denominator)) * cv), 2)
+    if pd.np.isnan(moe) or pd.np.isinf(moe):
+        moe = 0
+    return moe
