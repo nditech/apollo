9260cf77e39aa34b7857d16f3039dadb83474cd1
Tim Akinbo <takinbo@timbaobjects.com>
2015-08-25 13:11:11 +0100

    updates to fabfile to build images from the tag version

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,15 @@ from fabric.api import sudo, run, local, put, cd
 PROJECT_NAME = 'apollo'
 PROJECT_DIR = 'projects'
 
+def get_current_branch():
+    current_branch = local("git branch --no-color | grep '^\* ' | grep -v 'no branch' | sed 's/^* //g'", capture=True)
+    return current_branch
+
+def change_branch(branch='master'):
+    return local('git checkout %s' % branch)
+
+def get_latest_version():
+    return local("git tag -l | tail -n 1 | sed -e 's/^v//g'", capture=True)
 
 def make_archive(version="HEAD"):
     filename = "%s-%s.tar.gz" % (PROJECT_NAME, version)
@@ -25,9 +34,10 @@ def push(docker_index, image_version):
         docker_index, PROJECT_NAME))
 
 
-def build(
-    image_version, docker_index='docker.timbaobjects.com', version="HEAD"
-):
+def build(docker_index='docker.timbaobjects.com', version="HEAD"):
+    current_branch = get_current_branch()
+    image_version = get_latest_version()
+    change_branch('master')
     archive = make_archive(version)
     run('mkdir -p %s' % PROJECT_DIR)  # create target directory
     put(archive, PROJECT_DIR)
@@ -38,3 +48,5 @@ def build(
         sudo('docker build -t %s .' % PROJECT_NAME)
     tag(docker_index, image_version)
     push(docker_index, image_version)
+    change_branch(current_branch)
+
