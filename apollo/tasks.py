c3b6f563cb582de2a5e81e1723aced046666ec76
Oladipo Odumosu <dodumosu@timbaobjects.com>
2015-09-10 12:24:41 +0100

    Added checklist creation task to the global task list

--- apollo/tasks.py
+++ apollo/tasks.py
@@ -2,6 +2,7 @@ from .formsframework.tasks import update_submissions
 from .messaging.tasks import send_messages, send_email
 from .participants.tasks import import_participants
 from .locations.tasks import import_locations
+from .submissions.tasks import init_submissions
 from .factory import create_celery_app
 
 celery = create_celery_app()
