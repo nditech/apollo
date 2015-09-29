c96d2883de552b1ac25bb86b38fb14682da79d2f
James Muranga <jmured@gmail.com>
2015-09-29 12:27:06 +0700

    Added digest auth to allow login from participants while submitting forms from odk collect

--- apollo/frontend/views_odk.py
+++ apollo/frontend/views_odk.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 import json
+
 from flask import Blueprint, g, make_response, render_template, request
+from flask_httpauth import HTTPDigestAuth
 from lxml import etree
 from mongoengine import signals
 from slugify import slugify
-from apollo import services
+
+from apollo import services, csrf
 from apollo.frontend import route
 from apollo.frontend.helpers import DictDiffer
+from apollo.participants import Participant
 
 DEFAULT_CONTENT_LENGTH = 1000000
 DEFAULT_CONTENT_TYPE = 'text/xml; charset=utf-8'
@@ -38,6 +42,15 @@ def open_rosa_default_response(**kwargs):
 
 bp = Blueprint('xforms', __name__, template_folder='templates')
 
+participant_auth = HTTPDigestAuth()
+
+@participant_auth.get_password
+def get_pw(p_id):
+    try:
+        participant = Participant.objects.get(participant_id=p_id)
+        return participant.password
+    except Participant.DoesNotExist:
+        return None
 
 @route(bp, '/xforms/formList')
 def get_form_download_list():
@@ -77,8 +90,9 @@ def get_form(form_pk):
 
     return response
 
-
+@csrf.exempt
 @route(bp, '/xforms/submission', methods=['HEAD', 'POST'])
+@participant_auth.login_required
 def submission():
     if request.method == 'HEAD':
         response = open_rosa_default_response(status_code=204)
@@ -91,25 +105,22 @@ def submission():
         document = etree.parse(source_file, parser)
 
         form_pk = document.xpath('//data/form_id')[0].text
-        deviceID = document.xpath('//data/device_id')[0].text
         form = services.forms.get(id=form_pk)
 
-        participant = services.participants.get(device_id=deviceID)
+        participant = Participant.objects.get(participant_id=participant_auth.username())
 
         if not form or not participant:
             return open_rosa_default_response(status_code=404)
     except (IndexError, etree.LxmlError):
         return open_rosa_default_response(status_code=400)
 
-    # always overwrite the most recent submission
     submission = services.submissions.find(
         contributor=participant,
         form=form,
         submission_type='O'
     ).order_by('-created').first()
 
-    if not submission and form.form_type == 'INCIDENT':
-        # create a new incident
+    if not submission:
         submission = services.submissions.new(
             contributor=participant,
             created=datetime.utcnow(),
@@ -119,13 +130,11 @@ def submission():
             location=participant.location,
             submission_type='O',
         )
-    elif form.form_type != 'INCIDENT':
-        return open_rosa_default_response(status_code=405)
 
     for tag in form.tags:
         path_spec = '//data/{}'.format(tag)
         element = document.xpath(path_spec)[0]
-        if element.text:
+        if element.text and getattr(submission, tag, None):
             setattr(submission, tag, int(element.text))
 
     with signals.post_save.connected_to(
@@ -159,9 +168,9 @@ def update_submission_version(sender, document, **kwargs):
         if not diff.added() and not diff.removed() and not diff.changed():
             return
 
-    # use participant device ID as identity
+    # use participant ID as identity
     channel = 'WEB'
-    identity = document.contributor.device_id
+    identity = participant_auth.username()
 
     services.submission_versions.create(
         submission=document,
