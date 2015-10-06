94503aa660bf3493a90c8c3d670e4d4c7cfd600e
Oladipo Odumosu <dodumosu@timbaobjects.com>
2015-10-06 13:20:30 +0100

    Readded Celery tasks

--- apollo/tasks.py
+++ apollo/tasks.py
@@ -1,3 +1,9 @@
 from apollo.factory import create_celery_app
 
 celery = create_celery_app()
+
+from apollo.formsframework.tasks import update_submissions
+from apollo.messaging.tasks import send_messages, send_email
+from apollo.participants.tasks import import_participants
+from apollo.locations.tasks import import_locations
+from apollo.submissions.tasks import update_submissions
