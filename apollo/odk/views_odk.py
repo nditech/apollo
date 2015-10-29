c99c822e7d5cfb6d207d2af04f5a7b8c192ecab6
Oladipo Odumosu <dodumosu@timbaobjects.com>
2015-10-29 12:00:58 +0100

    use compiled xpath expression instead

--- apollo/odk/views_odk.py
+++ apollo/odk/views_odk.py
@@ -136,11 +136,11 @@ def submission():
         # no existing submission for that form and participant
         return open_rosa_default_response(status_code=404)
 
+    tag_finder = etree.XPath(u'//data/*[local-name() = $tag]')
     for tag in form.tags:
         field = form.get_field_by_tag(tag)
-        path_spec = '//data/{}'.format(tag)
         try:
-            element = document.xpath(path_spec)[0]
+            element = tag_finder(document, tag=tag)[0]
         except IndexError:
             # normally shouldn't happen, but the whole file
             # might not have been uploaded.
