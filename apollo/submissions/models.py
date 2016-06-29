8e69a186181887c9c340153db12ac69cff3ff6f0
Oladipo Odumosu <dodumosu@timbaobjects.com>
2016-06-29 13:05:16 +0100

    Bugfix: use the cls argument to prefix the VERIFICATION_STATUSES constant.

--- apollo/submissions/models.py
+++ apollo/submissions/models.py
@@ -272,7 +272,7 @@ class Submission(db.DynamicDocument):
                 form=form, contributor=participant, location=location,
                 created=event.start_date, deployment=deployment,
                 event=event, submission_type='O',
-                verification_status=VERIFICATION_STATUSES[0][0])
+                verification_status=cls.VERIFICATION_STATUSES[0][0])
             # force creation of master
             submission.master
 
