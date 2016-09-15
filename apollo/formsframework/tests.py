71178423f79c40543e28da24572cfde5759237e1
Tim Akinbo <takinbo@timbaobjects.com>
2016-09-15 12:46:37 +0100

    test coverage for expression parsing

--- apollo/formsframework/tests.py
+++ apollo/formsframework/tests.py
@@ -6,7 +6,7 @@ from apollo import create_app
 from apollo.messaging.utils import parse_responses
 from apollo.formsframework.forms import build_questionnaire
 from apollo.formsframework.models import Form, FormField, FormGroup
-from apollo.formsframework.parser import Comparator
+from apollo.formsframework.parser import Comparator, grammar_factory
 
 
 ENV_PATH = os.path.join(
@@ -115,3 +115,43 @@ class ComparatorTest(TestCase):
         self.assertFalse(comparator.eval('!= True'))
         self.assertTrue(comparator.eval('>= True'))
         self.assertTrue(comparator.eval('<= True'))
+
+
+class GrammarTest(TestCase):
+    def create_app(self):
+        read_env(ENV_PATH)
+        return create_app()
+
+    def setUp(self):
+        class AttributeHolder(object):
+            def set(self, attr, value):
+                setattr(self, attr, value)
+
+        self.env = AttributeHolder()
+
+    def test_operands(self):
+        self.env.set('AA', 5)
+        self.env.set('AB', 3)
+        self.env.set('AC', 2)
+
+        evaluator = grammar_factory(self.env)
+
+        self.assertEqual(evaluator('AA').expr(), 5)
+        self.assertEqual(evaluator('AB').expr(), 3)
+        self.assertEqual(evaluator('15').expr(), 15)
+
+    def test_operations(self):
+        self.env.set('AA', 5)
+        self.env.set('AB', 3)
+        self.env.set('AC', 2)
+
+        evaluator = grammar_factory(self.env)
+
+        self.assertEqual(evaluator('AA + AB').expr(), 8)
+        self.assertEqual(evaluator('AA - AB').expr(), 2)
+        self.assertEqual(evaluator('AA * AB').expr(), 15)
+        self.assertEqual(evaluator('AB / AC').expr(), 1.5)
+        self.assertEqual(evaluator('AC ^ AB').expr(), 8)
+        self.assertEqual(evaluator('AA + 10').expr(), 15)
+        self.assertEqual(evaluator('AA + 5 ^ 3').expr(), 130)
+        self.assertEqual(evaluator('(AA + 5) ^ 3').expr(), 1000)
