d5e2330f38ac52b5eca73e0e641338b5e4a5cb20
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-01-11 12:12:56 +0100

    Add the extra information to the extra parameter, not the context

--- apollo/sentry_ext.py
+++ apollo/sentry_ext.py
@@ -6,11 +6,15 @@ from apollo import settings
 
 
 class ApolloRavenClient(Client):
-	def capture(self, *args, **kwargs):
-		self.context.merge({
-			u'event': getattr(g, u'event', None),
-			u'apollo_version': getattr(settings, u'APOLLO_VERSION',
-				u'Unspecified'),
-		})
+    def capture(self, event_type, data=None, date=None, time_spent=None,
+            extra=None, stack=None, tags=None, **kwargs):
 
-		super(ApolloRavenClient, self).capture(self, *args, **kwargs)
+        extra = extra or {}
+        extra.update({
+            u'event': str(getattr(g, u'event', None)),
+            u'apollo_version': getattr(settings, u'APOLLO_VERSION',
+                u'Unspecified'),
+        })
+
+        return super(ApolloRavenClient, self).capture(event_type, data,
+            date, time_spent, extra, stack, tags, **kwargs)
