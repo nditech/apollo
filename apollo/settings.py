f70b13a593dd424841e3cdca6a4e43faaeb092cd
Oladipo Odumosu <dodumosu@timbaobjects.com>
2018-01-24 10:52:02 +0100

    Migrated settings to prettyconf, added basic SQLAlchemy settings

--- apollo/settings.py
+++ apollo/settings.py
@@ -1,49 +1,46 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 from urllib.parse import urlparse
-import ast
 import numpy
 import os
 import string
 
-from apollo.utils import read_env
+from prettyconf import config
 
-env_path = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-read_env(env_path)
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+
+config.starting_path = PROJECT_ROOT
+
 
 try:
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = config('SECRET_KEY')
 except KeyError:
     raise KeyError('A SECRET_KEY value must be defined in the environment.')
 
-DEBUG = ast.literal_eval(
-    os.environ.get('DEBUG', 'False'))
-PAGE_SIZE = ast.literal_eval(
-    os.environ.get('PAGE_SIZE', '25'))
+DEBUG = config('DEBUG', cast=config.boolean, default=False)
+PAGE_SIZE = config('PAGE_SIZE', cast=int, default=25)
 
 MONGODB_SETTINGS = {
-    'DB': os.environ.get('MONGO_DATABASE_NAME', 'apollo').strip(),
+    'DB': config('MONGO_DATABASE_NAME', default='apollo').strip(),
     'HOST': urlparse(
-        os.environ.get('MONGODB_PORT', 'mongodb://localhost')).netloc
+        config('MONGODB_PORT', default='mongodb://localhost')).netloc
 }
 
 # default to UTC for prior deployments
-TIMEZONE = os.environ.get('TIMEZONE', 'UTC')
+TIMEZONE = config('TIMEZONE', default='UTC')
 
-SSL_REQUIRED = ast.literal_eval(
-    os.environ.get('SSL_REQUIRED', 'True'))
-ENABLE_MOE = ast.literal_eval(
-    os.environ.get('ENABLE_MOE', 'False'))
-X_FRAME_OPTIONS = os.environ.get('X_FRAME_OPTIONS', 'DENY')
+SSL_REQUIRED = config('SSL_REQUIRED', cast=config.boolean, default=True)
+ENABLE_MOE = config('ENABLE_MOE', cast=config.boolean, default=False)
+X_FRAME_OPTIONS = config('X_FRAME_OPTIONS', default='DENY')
 
 # This setting informs the application server of the number of
 # upstream proxies to account for. Useful to determining true IP address
 # for the request and preventing spoofing. Because this application is
 # designed to be run behind a proper webserver, the default is set to 1
-UPSTREAM_PROXY_COUNT = os.environ.get('UPSTREAM_PROXY_COUNT', 1)
+UPSTREAM_PROXY_COUNT = config('UPSTREAM_PROXY_COUNT', cast=int, default=1)
 
-SENTRY_DSN = os.environ.get('SENTRY_DSN')
+SENTRY_DSN = config('SENTRY_DSN', default=None)
 SENTRY_USER_ATTRS = ['email']
 
 SECURITY_PASSWORD_HASH = 'pbkdf2_sha256'
@@ -51,30 +48,30 @@ SECURITY_PASSWORD_SALT = SECRET_KEY
 SECURITY_URL_PREFIX = '/accounts'
 SECURITY_POST_LOGOUT_VIEW = '/accounts/login'
 SECURITY_LOGIN_USER_TEMPLATE = 'frontend/login_user.html'
-SECURITY_EMAIL_SENDER = os.environ.get('SECURITY_EMAIL_SENDER')
-SECURITY_SEND_REGISTER_EMAIL = ast.literal_eval(
-    os.environ.get('SECURITY_SEND_REGISTER_EMAIL', 'False'))
+SECURITY_EMAIL_SENDER = config('SECURITY_EMAIL_SENDER', default=None)
+SECURITY_SEND_REGISTER_EMAIL = config(
+    'SECURITY_SEND_REGISTER_EMAIL', cast=config.boolean, default=False)
 SECURITY_RECOVERABLE = True
 SECURITY_TRACKABLE = True
 SESSION_COOKIE_SECURE = True if SSL_REQUIRED else False
 PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
 
-MAIL_SERVER = os.environ.get('MAIL_SERVER')
-MAIL_PORT = os.environ.get('MAIL_PORT')
-MAIL_USE_TLS = ast.literal_eval(os.environ.get('MAIL_USE_TLS', 'False'))
-MAIL_USE_SSL = ast.literal_eval(os.environ.get('MAIL_USE_SSL', 'False'))
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_SERVER = config('MAIL_SERVER', default=None)
+MAIL_PORT = config('MAIL_PORT', default=None)
+MAIL_USE_TLS = config('MAIL_USE_TLS', cast=config.boolean, default=False)
+MAIL_USE_SSL = config('MAIL_USE_SSL', cast=config.boolean, default=False)
+MAIL_USERNAME = config('MAIL_USERNAME', default=None)
+MAIL_PASSWORD = config('MAIL_PASSWORD', default=None)
 DEFAULT_EMAIL_SENDER = SECURITY_EMAIL_SENDER
 
 CELERY_BROKER_URL = 'redis://{host}/{database}'.format(
     host=urlparse(
-        os.environ.get('REDIS_PORT', 'redis://localhost')).netloc,
-    database=os.environ.get('REDIS_DATABASE', '0'))
+        config('REDIS_PORT', default='redis://localhost')).netloc,
+    database=config('REDIS_DATABASE', default='0'))
 CELERY_RESULT_BACKEND = 'redis://{host}/{database}'.format(
     host=urlparse(
-        os.environ.get('REDIS_PORT', 'redis://localhost')).netloc,
-    database=os.environ.get('REDIS_DATABASE', '0'))
+        config('REDIS_PORT', default='redis://localhost')).netloc,
+    database=config('REDIS_DATABASE', default='0'))
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
@@ -82,8 +79,8 @@ CELERY_ACCEPT_CONTENT = ['json']
 CACHE_TYPE = 'simple' if DEBUG else 'redis'
 CACHE_REDIS_URL = 'redis://{host}/{database}'.format(
     host=urlparse(
-        os.environ.get('REDIS_PORT', 'redis://localhost')).netloc,
-    database=os.environ.get('REDIS_DATABASE', '0'))
+        config('REDIS_PORT', default='redis://localhost')).netloc,
+    database=config('REDIS_DATABASE', default='0'))
 
 LANGUAGES = {
     'en': 'English',
@@ -95,7 +92,7 @@ LANGUAGES = {
     'ru': 'Русский',
     'ro': 'Română',
 }
-BABEL_DEFAULT_LOCALE = os.environ.get('BABEL_DEFAULT_LOCALE', 'en')
+BABEL_DEFAULT_LOCALE = config('BABEL_DEFAULT_LOCALE', default='en')
 
 ALLOWED_PUNCTUATIONS = '!'
 CHARACTER_TRANSLATIONS = (
@@ -110,19 +107,22 @@ PUNCTUATIONS = [s for s in string.punctuation if s not in ALLOWED_PUNCTUATIONS] 
 TRANS_TABLE = dict((ord(char_from), ord(char_to))
                    for char_from, char_to in
                    CHARACTER_TRANSLATIONS)
-MESSAGING_OUTGOING_GATEWAY = ast.literal_eval(
-    os.environ.get('MESSAGING_OUTGOING_GATEWAY', '''{
+MESSAGING_OUTGOING_GATEWAY = config(
+    'MESSAGING_OUTGOING_GATEWAY',
+    cast=config.eval,
+    default='''{
         'type': 'kannel',
         'gateway_url': 'http://localhost:13013/cgi-bin/sendsms',
         'username': 'foo',
         'password': 'bar'
-    }'''))
-MESSAGING_CC = ast.literal_eval(os.environ.get('MESSAGING_CC', '[]'))
-MESSAGING_SECRET = os.environ.get('MESSAGING_SECRET')
+    }''')
+MESSAGING_CC = config('MESSAGING_CC', cast=config.tuple, default=tuple())
+MESSAGING_SECRET = config('MESSAGING_SECRET', default=None)
 
-APPLICATIONS = ast.literal_eval(os.environ.get(
+APPLICATIONS = config(
     'APPLICATIONS',
-    '''("apollo.frontend",
+    cast=config.eval,
+    default='''("apollo.frontend",
         "apollo.locations",
         "apollo.participants",
         "apollo.formsframework",
@@ -130,11 +130,17 @@ APPLICATIONS = ast.literal_eval(os.environ.get(
         "apollo.messaging",
         "apollo.process_analysis",
         "apollo.result_analysis",
-        "apollo.odk")'''))
+        "apollo.odk")''')
 
-BIG_N = ast.literal_eval(os.environ.get('BIG_N', '0')) or numpy.inf
-GOOGLE_ANALYTICS_KEY = os.environ.get('GOOGLE_ANALYTICS_KEY')
+BIG_N = config('BIG_N', cast=int, default=0) or numpy.inf
+GOOGLE_ANALYTICS_KEY = config('GOOGLE_ANALYTICS_KEY', default=None)
 
-TRANSLATE_CHARS = ast.literal_eval(os.environ.get('TRANSLATE_CHARS', 'True'))
-TRANSLITERATE_INPUT = ast.literal_eval(os.environ.get('TRANSLITERATE_INPUT', 'False'))
-TRANSLITERATE_OUTPUT = ast.literal_eval(os.environ.get('TRANSLITERATE_OUTPUT', 'False'))
+TRANSLATE_CHARS = config('TRANSLATE_CHARS', cast=config.boolean, default=True)
+TRANSLITERATE_INPUT = config('TRANSLITERATE_INPUT', cast=config.boolean, default=False)
+TRANSLITERATE_OUTPUT = config('TRANSLITERATE_OUTPUT', cast=config.boolean, default=False)
+
+
+# SQLAlchemy settings
+SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='sqlite:///:memory:')
+SQLALCHEMY_ECHO = DEBUG
+SQLALCHEMY_TRACK_MODIFICATIONS = False
