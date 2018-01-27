1ab8f2c3e4cfaf99c0502a2dec9455e64afd2881
Oladipo Odumosu <dodumosu@timbaobjects.com>
2018-01-27 10:49:43 +0100

    Fixed bad identifier name, changed primary key from a UUID field

--- apollo/dal/models.py
+++ apollo/dal/models.py
@@ -21,19 +21,19 @@ class CRUDMixin(object):
         return commit and self.save() or self
 
     def save(self, commit=True):
-        db.session.add(self)
+        db2.session.add(self)
         if commit:
-            db.session.commit()
+            db2.session.commit()
 
         return self
 
     def delete(self, commit=True):
-        db.session.delete(self)
-        return commit and db.session.commit()
+        db2.session.delete(self)
+        return commit and db2.session.commit()
 
 
 class BaseModel(CRUDMixin, db2.Model):
     '''Base model class'''
     __abstract__ = True
 
-    id = db2.Column(UUIDType, default=uuid4, primary_key=True)
+    uuid = db2.Column(UUIDType, default=uuid4)
