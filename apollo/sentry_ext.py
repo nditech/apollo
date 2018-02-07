2b3dac0a7afb7f5b4eaf86fccbd5f6422c633923
Oladipo Odumosu <dodumosu@timbaobjects.com>
2018-02-07 23:44:02 +0100

    Fixed more issues with sentry client

--- apollo/sentry_ext.py
+++ apollo/sentry_ext.py
@@ -8,24 +8,24 @@ from apollo import settings
 
 class ApolloRavenClient(Client):
     def capture(self, event_type, data=None, date=None, time_spent=None,
-            extra=None, stack=None, tags=None, **kwargs):
+                extra=None, stack=None, tags=None, **kwargs):
 
         extra = extra or {}
+        if current_app:
+            with current_app.app_context():
+                if not current_user.is_anonymous:
+                    deployment = current_user.deployment
+                    event = current_user.event
+                else:
+                    deployment = None
+                    event = None
 
-        with current_app.app_context():
-            if not current_user.is_anonymous:
-                deployment = current_user.deployment
-                event = current_user.event
-            else:
-                deployment = None
-                event = None
+                extra.update({
+                    'deployment': getattr(deployment, 'name', 'Unspecified'),
+                    'event': getattr(event, 'name', 'Unspecified'),
+                    'apollo_version': getattr(settings, 'APOLLO_VERSION',
+                                              'Unspecified'),
+                })
 
-            extra.update({
-                'deployment': getattr(deployment, 'name', 'Unspecified'),
-                'event': getattr(event, 'name', 'Unspecified'),
-                'apollo_version': getattr(settings, 'APOLLO_VERSION',
-                    'Unspecified'),
-            })
-
-        return super(ApolloRavenClient, self).capture(event_type, data,
-            date, time_spent, extra, stack, tags, **kwargs)
+        return super(ApolloRavenClient, self).capture(
+            event_type, data, date, time_spent, extra, stack, tags, **kwargs)
