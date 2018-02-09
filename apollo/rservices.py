974bef8d5853598a59e8849401e455a19b3e0b50
Oladipo Odumosu <dodumosu@timbaobjects.com>
2018-02-09 15:52:41 +0100

    Added more service classes

--- apollo/rservices.py
+++ apollo/rservices.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from apollo.deployments.services import EventService
+from apollo.deployments.services import (
+    EventService, FormSetService, LocationSetService, ParticipantSetService)
 from apollo.formsframework.services import FormService
 from apollo.locations.services import (
     LocationService, LocationTypeService, SampleService)
@@ -10,10 +11,13 @@ from apollo.users.services import UserService, UserUploadService
 
 events = EventService()
 forms = FormService()
+form_sets = FormSetService()
 locations = LocationService()
+location_sets = LocationSetService()
 location_types = LocationTypeService()
 messages = MessageService()
 participants = ParticipantService()
+participant_sets = ParticipantSetService()
 samples = SampleService()
 submissions = SubmissionService()
 users = UserService()
