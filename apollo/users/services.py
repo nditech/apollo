ba51024d34fff3b180b1acc2d43cecbbaa799b84
Oladipo Odumosu <dodumosu@timbaobjects.com>
2018-02-14 13:01:42 +0100

    Updated user service

--- apollo/users/services.py
+++ apollo/users/services.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from apollo.dal.service import Service
+from apollo.frontend import permissions
 from apollo.users import models
 
 
@@ -15,25 +16,21 @@ class UserService(Service):
         # if this is an admin user, set all role
         # and user permissions
         if instance.is_admin():
-            return {
-                'user': '__all__',
-                'role': '__all__'
-            }
+            return [permissions.RoleNeed('admin')]
 
-        perm_cache = {}
+        perm_cache = []
         # load up role permissions
         role_perms = models.RolePermission.query.with_entities(
             models.RolePermission.perm_name).join(models.Role.users).filter(
             models.Role.users.contains(instance),
             models.RolePermission.deployment == deployment).all()
+        perm_cache.extend([permissions.ActionNeed(n[0]) for n in role_perms])
 
         # load up user perms
         user_perms = models.UserPermission.query.with_entities(
             models.UserResourcePermission.perm_name).filter_by(
                 user_id=instance.id, deployment_id=deployment.id).all()
-
-        perm_cache['role_perms'] = role_perms
-        perm_cache['user_perms'] = user_perms
+        perm_cache.extend([permissions.ActionNeed(n[0]) for n in user_perms])
 
         return perm_cache
 
