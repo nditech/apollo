e238e7f9e9afb2bc00269ce074c1925b62377f88
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-02-17 08:40:03 +0000

    Updated tests

--- apollo/formsframework/tests.py
+++ apollo/formsframework/tests.py
@@ -1,97 +1,67 @@
 # -*- coding: utf-8 -*-
-import os
-import warnings
 from flask_testing import TestCase
-from werkzeug.datastructures import MultiDict
+# from werkzeug.datastructures import MultiDict
 from apollo import create_app
-from apollo.messaging.utils import parse_responses
-from apollo.formsframework.forms import build_questionnaire
-from apollo.formsframework.models import Form, FormField, FormGroup
+# from apollo.messaging.utils import parse_responses
+# from apollo.formsframework.forms import build_questionnaire
+# from apollo.formsframework.models import Form, FormField, FormGroup
 from apollo.formsframework.parser import Comparator, grammar_factory
 
 
-ENV_PATH = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.env')
+# class QuestionnaireTest(TestCase):
+#     def create_app(self):
+#         return create_app()
 
+#     def setUp(self):
+#         aa = FormField(name='AA', description='AA')
+#         ab = FormField(name='AB', description='AB',
+#                        allows_multiple_values=True,
+#                        options={'1': 'One', '2': 'Two'})
 
-def read_env(env_path=None):
-    if env_path is None:
-        env_path = os.path.join(os.path.dirname(__file__), '.env')
-    if not os.path.exists(env_path):
-        warnings.warn('No environment file found. Skipping load.')
-        return
+#         grp1 = FormGroup(name='First', slug='First')
+#         grp1.fields.append(aa)
+#         grp1.fields.append(ab)
 
-    for k, v in parse_env(env_path):
-        os.environ.setdefault(k, v)
+#         self.checklist_form = Form(name='TCF', form_type='CHECKLIST')
+#         self.checklist_form.groups.append(grp1)
 
+#         a = FormField(name='A', description='A', represents_boolean=True)
+#         b = FormField(name='B', description='B', represents_boolean=True)
 
-def parse_env(env_path):
-    with open(env_path) as f:
-        for line in f:
-            line = line.strip()
-            if not line or line.startswith('#') or '=' not in line:
-                continue
-            k, v = line.split('=', 1)
-            v = v.strip('"').strip("'")
-            yield k, v
+#         grp2 = FormGroup(name='Other', slug='Other')
+#         grp2.fields.append(a)
+#         grp2.fields.append(b)
 
+#         self.incident_form = Form(name='TIF', form_type='INCIDENT')
+#         self.incident_form.groups.append(grp2)
 
-class QuestionnaireTest(TestCase):
-    def create_app(self):
-        read_env(ENV_PATH)
-        return create_app()
+#     def test_checklist_parsing(self):
+#         sample_text = 'AA2AB12'
+#         q = build_questionnaire(
+#             self.checklist_form,
+#             MultiDict(parse_responses(sample_text, self.checklist_form)[0]))
+#         flag = q.validate()
+#         data = q.data
 
-    def setUp(self):
-        aa = FormField(name='AA', description='AA')
-        ab = FormField(name='AB', description='AB',
-                       allows_multiple_values=True,
-                       options={'1': 'One', '2': 'Two'})
+#         self.assertEqual(data['AA'], 2)
+#         self.assertEqual(data['AB'], [1, 2])
+#         # invalid due to missing data
+#         self.assertFalse(flag)
 
-        grp1 = FormGroup(name='First', slug='First')
-        grp1.fields.append(aa)
-        grp1.fields.append(ab)
+#     def test_incident_parsing(self):
+#         sample_text = 'AB'
+#         responses = parse_responses(sample_text, self.incident_form)[0]
+#         q = build_questionnaire(self.incident_form, MultiDict(responses))
 
-        self.checklist_form = Form(name='TCF', form_type='CHECKLIST')
-        self.checklist_form.groups.append(grp1)
-
-        a = FormField(name='A', description='A', represents_boolean=True)
-        b = FormField(name='B', description='B', represents_boolean=True)
-
-        grp2 = FormGroup(name='Other', slug='Other')
-        grp2.fields.append(a)
-        grp2.fields.append(b)
-
-        self.incident_form = Form(name='TIF', form_type='INCIDENT')
-        self.incident_form.groups.append(grp2)
-
-    def test_checklist_parsing(self):
-        sample_text = 'AA2AB12'
-        q = build_questionnaire(
-            self.checklist_form,
-            MultiDict(parse_responses(sample_text, self.checklist_form)[0]))
-        flag = q.validate()
-        data = q.data
-
-        self.assertEqual(data['AA'], 2)
-        self.assertEqual(data['AB'], [1, 2])
-        # invalid due to missing data
-        self.assertFalse(flag)
-
-    def test_incident_parsing(self):
-        sample_text = 'AB'
-        responses = parse_responses(sample_text, self.incident_form)[0]
-        q = build_questionnaire(self.incident_form, MultiDict(responses))
-
-        flag = q.validate()
-        data = q.data
-        self.assertEqual(data['A'], 1)
-        self.assertEqual(data['B'], 1)
-        self.assertFalse(flag)
+#         flag = q.validate()
+#         data = q.data
+#         self.assertEqual(data['A'], 1)
+#         self.assertEqual(data['B'], 1)
+#         self.assertFalse(flag)
 
 
 class ComparatorTest(TestCase):
     def create_app(self):
-        read_env(ENV_PATH)
         return create_app()
 
     def test_numeric_comparisons(self):
@@ -120,7 +90,6 @@ class ComparatorTest(TestCase):
 
 class GrammarTest(TestCase):
     def create_app(self):
-        read_env(ENV_PATH)
         return create_app()
 
     def setUp(self):
