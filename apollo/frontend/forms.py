ec8798273e606c2f28d10af4665727eace3a48ed
Oladipo Odumosu <dodumosu@timbaobjects.com>
2018-02-21 14:58:38 +0000

    Updated file upload form

--- apollo/frontend/forms.py
+++ apollo/frontend/forms.py
@@ -380,14 +380,6 @@ def generate_submission_edit_form_class(form):
 
 def file_upload_form(*args, **kwargs):
     class FileUploadForm(WTSecureForm):
-        event = SelectField(
-            _('Event'),
-            choices=_make_choices(
-                events.find().scalar('id', 'name'),
-                _('Select Event')
-            ),
-            validators=[validators.input_required()]
-        )
         spreadsheet = FileField(
             _('Data File'),
         )
