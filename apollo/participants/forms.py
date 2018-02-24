70112b07abedf8f1c4821e2f0e32fc8fe21b03c4
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-02-24 09:38:08 +0000

    Minor cleanup

--- apollo/participants/forms.py
+++ apollo/participants/forms.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask_babelex import lazy_gettext as _
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileRequired
 from wtforms import FileField, SelectField
 from wtforms.validators import InputRequired
 
 from apollo import models, services
 from apollo.frontend.forms import _make_choices
-
-SPREADSHEET_FORMATS = ['csv', 'ods', 'tsv', 'xls', 'xlsx']
 
 
 def participant_upload_form_factory(deployment):
