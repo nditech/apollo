5e02688ae455a1c688451997560ab12cae335425
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-02-27 06:23:38 +0000

    Most form views are now set-aware

--- apollo/formsframework/views_forms.py
+++ apollo/formsframework/views_forms.py
@@ -2,7 +2,8 @@
 
 from apollo.frontend import route, permissions
 from flask import (
-    Blueprint, current_app, flash, g, redirect, render_template, request, url_for)
+    Blueprint, current_app, flash, g, redirect, render_template, request,
+    url_for)
 from flask_babelex import lazy_gettext as _
 from flask_menu import register_menu
 from flask_security import login_required
@@ -42,19 +43,13 @@ def form_set_list():
     return render_template(template_name, **context)
 
 
-
-@route(bp, '/forms/init', methods=['POST'])
+@route(bp, '/<int:form_set_id>/forms/init', methods=['POST'])
 @permissions.edit_forms.require(403)
 @login_required
-def checklist_init():
+def checklist_init(form_set_id):
     flash_message = ''
     flash_category = ''
-    form = make_checklist_init_form()
-
-    try:
-        str_func = str
-    except NameError:
-        str_func = str
+    form = make_checklist_init_form(g.event)
 
     if form.validate_on_submit():
         flash_category = 'checklist_init_success'
@@ -70,22 +65,22 @@ def checklist_init():
         flash_category = 'checklist_init_failure'
         flash_message = _('Checklists were not created')
 
-    flash(str_func(flash_message), flash_category)
+    flash(str(flash_message), flash_category)
 
-    return redirect(url_for('.list_forms'))
+    return redirect(url_for('.list_forms', form_set_id=form_set_id))
 
 
-@route(bp, '/formbuilder/<pk>', methods=['GET', 'POST'])
+@route(bp, '/formbuilder/<int:id>', methods=['GET', 'POST'])
 @permissions.edit_forms.require(403)
 @login_required
-def form_builder(pk):
+def form_builder(id):
     page_title = _('Form Builder')
     template_name = 'frontend/formbuilder.html'
-    form = services.forms.get_or_404(pk=pk)
+    form = services.forms.fget_or_404(id=id)
 
     ctx = dict(
         page_title=page_title,
-        form=form.name
+        form=form
     )
 
     if request.method == 'GET':
@@ -96,15 +91,15 @@ def form_builder(pk):
 
         if data:
             FormBuilderSerializer.deserialize(form, data)
-            update_submissions.delay(str(form.pk))
+            # update_submissions.delay(str(form.pk))
 
         return ''
 
 
-@route(bp, '/forms/new', methods=['GET', 'POST'])
+@route(bp, '/<int:form_set_id>/forms/new', methods=['GET', 'POST'])
 @permissions.edit_forms.require(403)
 @login_required
-def new_form():
+def new_form(form_set_id):
     page_title = _('Create form')
     template_name = 'frontend/form_edit.html'
 
@@ -113,27 +108,29 @@ def new_form():
     if not web_form.validate_on_submit():
         context = {
             'page_title': page_title,
-            'form': web_form
+            'form': web_form,
+            'form_set_id': form_set_id
         }
 
         return render_template(template_name, **context)
 
     deployment = g.get('deployment')
-    event = g.get('event')
 
     # hack because of nasty bug with forms service
-    form = services.forms.__model__(deployment=deployment, events=[event])
+    form = services.forms.__model__(
+        deployment_id=deployment.id,
+        form_set_id=form_set_id)
     web_form.populate_obj(form)
     form.save()
 
-    return redirect(url_for('.list_forms'))
+    return redirect(url_for('.list_forms', form_set_id=form_set_id))
 
 
-@route(bp, '/forms/<pk>', methods=['GET', 'POST'])
+@route(bp, '/<int:form_set_id>/forms/<int:form_id>', methods=['GET', 'POST'])
 @permissions.edit_forms.require(403)
 @login_required
-def edit_form(pk):
-    form = services.forms.get_or_404(pk=pk)
+def edit_form(form_set_id, form_id):
+    form = services.forms.get_or_404(id=form_id, form_set_id=form_set_id)
     page_title = _('Edit %(name)s', name=form.name)
     template_name = 'frontend/form_edit.html'
 
@@ -142,7 +139,8 @@ def edit_form(pk):
     if not web_form.validate_on_submit():
         context = {
             'page_title': page_title,
-            'form': web_form
+            'form': web_form,
+            'form_set_id': form_set_id
         }
 
         return render_template(template_name, **context)
@@ -150,22 +148,23 @@ def edit_form(pk):
     web_form.populate_obj(form)
     form.save()
 
-    return redirect(url_for('.list_forms'))
+    return redirect(url_for('.list_forms', form_set_id=form_set_id))
 
 
-@route(bp, '/forms')
+@route(bp, '/<int:form_set_id>/forms')
 @register_menu(
     bp, 'user.forms', _('Forms'),
     visible_when=lambda: permissions.edit_forms.can())
 @permissions.edit_forms.require(403)
 @login_required
-def list_forms():
+def list_forms(form_set_id):
     page_title = _('Forms')
     template_name = 'frontend/form_list.html'
-    forms = services.forms.find()
-    checklist_init_form = make_checklist_init_form()
+    forms = services.forms.find(form_set_id=form_set_id)
+    checklist_init_form = make_checklist_init_form(g.event)
 
     context = {
+        'form_set_id': form_set_id,
         'forms': forms,
         'page_title': page_title,
         'init_form': checklist_init_form
@@ -174,11 +173,11 @@ def list_forms():
     return render_template(template_name, **context)
 
 
-@route(bp, '/forms/<pk>/qa', methods=['GET', 'POST'])
+@route(bp, '/<int:form_set_id>/forms/<int:form_id>/qa', methods=['GET', 'POST'])
 @permissions.edit_forms.require(403)
 @login_required
-def quality_assurance(pk):
-    form = services.forms.get_or_404(pk=pk)
+def quality_assurance(form_set_id, form_id):
+    form = services.forms.get_or_404(id=form_id, form_set_id=form_set_id)
     page_title = _('Quality Assurance — %(name)s', name=form.name)
     template_name = 'frontend/quality_assurance.html'
 
@@ -194,7 +193,7 @@ def quality_assurance(pk):
                         'rvalue': rhs
                     })
             form.save()
-            return redirect(url_for('.list_forms'))
+            return redirect(url_for('.list_forms', form_set_id=form_set_id))
         except ValueError:
             pass
 
