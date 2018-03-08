513fb2aca4b061d50eec77afff34138cc24992c4
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-03-08 09:41:19 +0000

    Added verification status attribute

--- apollo/submissions/models.py
+++ apollo/submissions/models.py
@@ -87,6 +87,9 @@ class Submission(BaseModel):
     updated = db.Column(db.DateTime, onupdate=current_timestamp)
     sender_verified = db.Column(db.Boolean, default=True)
     quarantine_status = db.Column(ChoiceType(QUARANTINE_STATUSES), default='')
+    verification_status = db.Column(
+        ChoiceType(VERIFICATION_STATUSES),
+        default=VERIFICATION_STATUSES[0][0])
     incident_description = db.Column(db.String)
     incident_status = db.Column(ChoiceType(INCIDENT_STATUSES))
     overridden_fields = db.Column(ARRAY(db.String))
