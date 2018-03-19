afc984658155897d958f965ac07a4502997207d5
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-03-19 11:01:24 +0000

    Added form class for importing admin divisions

--- apollo/locations/forms.py
+++ apollo/locations/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask_babelex import lazy_gettext as _
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
 from wtforms.fields import StringField
 from wtforms.validators import InputRequired
 from wtforms.widgets import HiddenInput
@@ -8,6 +9,11 @@ from wtforms.widgets import HiddenInput
 
 class SampleForm(FlaskForm):
     '''Sample creation form'''
-    name = StringField(_('Name'), [InputRequired()])
+    name = StringField(_('Name'), validators=[InputRequired()])
     location_data = StringField(
         _('Locations'), widget=HiddenInput())
+
+
+class AdminDivisionImportForm(FlaskForm):
+    '''Form for processing admin division imports'''
+    import_file = FileField(_('Import file'), validators=[InputRequired()])
