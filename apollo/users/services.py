c079f8b45927d9b1ca38c11cfbbb88f131cb4592
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-03-20 22:19:45 +0000

    Updated user service

--- apollo/users/services.py
+++ apollo/users/services.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
+from apollo.dal.models import Permission
 from apollo.dal.service import Service
 from apollo.frontend import permissions
-from apollo.users import models
+from apollo.users.models import Role, User, UserUpload
 
 
 class UserService(Service):
-    __model__ = models.User
+    __model__ = User
 
     def get_permissions_cache(self, instance):
         if not self._isinstance(instance):
             return None
-
-        deployment = instance.deployment
 
         # if this is an admin user, set all role
         # and user permissions
@@ -20,20 +19,17 @@ class UserService(Service):
 
         perm_cache = []
         # load up role permissions
-        role_perms = models.RolePermission.query.with_entities(
-            models.RolePermission.perm_name).join(models.Role.users).filter(
-            models.Role.users.contains(instance),
-            models.RolePermission.deployment == deployment).all()
+        role_perms = Permission.query.with_entities(Permission.name).join(
+            Permission.roles).filter(Role.users.contains(instance)).all()
         perm_cache.extend([permissions.ActionNeed(n[0]) for n in role_perms])
 
         # load up user perms
-        user_perms = models.UserPermission.query.with_entities(
-            models.UserResourcePermission.perm_name).filter_by(
-                user_id=instance.id, deployment_id=deployment.id).all()
+        user_perms = Permission.query.with_entities(Permission.name).filter(
+            Permission.users.contains(instance)).all()
         perm_cache.extend([permissions.ActionNeed(n[0]) for n in user_perms])
 
         return perm_cache
 
 
 class UserUploadService(Service):
-    __model__ = models.UserUpload
+    __model__ = UserUpload
