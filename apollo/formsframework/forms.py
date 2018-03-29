01e7a5372426f228915b135b3ab828ad63218740
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-03-29 22:08:04 +0000

    Message processing form uses the SQLA models

--- apollo/formsframework/forms.py
+++ apollo/formsframework/forms.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from functools import partial
-import json
 import re
 
 from flask import g
@@ -12,7 +11,7 @@ from wtforms import (
     Form, BooleanField, IntegerField, SelectField, StringField, validators,
     widgets)
 
-from .. import services, models
+from .. import models, services, utils
 from ..frontend.helpers import DictDiffer
 from ..participants.utils import update_participant_completion_rating
 from .custom_fields import IntegerSplitterField
@@ -37,20 +36,22 @@ def update_submission_version(submission):
             models.SubmissionVersion.timestamp.desc()).first()
 
     if previous:
-        prev_data = json.loads(previous.data)
-        diff = DictDiffer(version_data, prev_data)
+        diff = DictDiffer(version_data, previous.data)
 
         # don't do anything if the data wasn't changed
         if not diff.added() and not diff.removed() and not diff.changed():
             return
 
     channel = 'SMS'
+    identity = g.phone
 
     services.submission_versions.create(
-        submission=submission,
-        data=json.dumps(version_data),
+        submission_id=submission.id,
+        data=version_data,
+        timestamp=datetime.utcnow(),
         channel=channel,
-        identity=g.get('phone', '')
+        identity=identity,
+        deployment_id=submission.deployment_id
     )
 
 
@@ -60,21 +61,26 @@ def filter_participants(form, participant_id):
     event = getattr(g, 'event', services.events.default())
     current_events = services.events.overlapping_events(event)
     form_events = models.Event.query.filter_by(form_set=form.form_set)
-    participant_sets = current_events.intersect(form_events).join(
-        models.Event.participant_set).with_entities(models.ParticipantSet)
+    query = current_events.intersect(form_events).join(
+        models.Participant,
+        models.Participant.participant_set_id == models.Event.participant_set_id    # noqa
+    ).with_entities(models.Participant)
 
-    participant = models.Participant.objects(
-            event__in=events, participant_id=participant_id).first()
+    participant = query.filter_by(participant_id=participant_id).first()
 
     return participant
 
 
-def filter_form(form_pk):
+def filter_form(form_id):
     event = getattr(g, 'event', services.events.default())
     current_events = services.events.overlapping_events(event)
+    query = current_events.join(
+        models.Form,
+        models.Event.form_set_id == models.Form.form_set_id
+    ).with_entities(models.Form)
 
-    if form_pk:
-        form = models.Form.objects(events__in=events, pk=form_pk).first()
+    if form_id:
+        form = query.filter_by(id=form_id).first()
 
         return form
 
@@ -105,99 +111,127 @@ class BaseQuestionnaireForm(Form):
 
         return success
 
-    def save(self):
+    def save(self, commit=True):
         ignored_fields = ['form', 'participant', 'sender', 'comment']
         submission = None
+        current_events = services.events.overlapping_events(
+            g.event)
+        current_event_ids = current_events.with_entities(models.Event.id)
+        update_params = {}
 
         # also ignore fields that have errors so as not to save them
         ignored_fields.extend(list(self.errors.keys()))
         try:
+            form = self.data.get('form')
             participant = self.data.get('participant')
 
-            if self.data.get('form').form_type == 'CHECKLIST':
+            if form.form_type == 'CHECKLIST':
                 # when searching for the submission, take into cognisance
                 # that the submission may be in one of several concurrent
                 # events
-                submission = models.Submission.objects(
-                    contributor=participant,
-                    form=self.data.get('form'), submission_type='O',
-                    event__in=services.events.overlapping_events(g.event),
-                    deployment=self.data.get('form').deployment).first()
-                if self.data.get('comment') and submission:
+                submission = models.Submission.query.filter(
+                    models.Submission.participant == participant,
+                    models.Submission.form == form,
+                    models.Submission.submission_type == 'O',
+                    models.Submission.event_id.in_(current_event_ids)
+                ).first()
+
+                if self.data.get('comment') and submission and commit:
                     services.submission_comments.create_comment(
                         submission, self.data.get('comment'))
             else:
                 # the submission event is determined by taking the intersection
-                # of form events and concurrent events and taking an arbitrary
-                # element from that set; basic idea being that we want to
-                # find what is common between the form's events and the current
-                # events
-                submission_event = participant.event
+                # of form events, participant events and concurrent events
+                # and taking the last event ordered by descending end date
+                event = current_events.filter_by(
+                    form_set_id=form.form_set_id,
+                    participant_set_id=participant.participant_set_id
+                ).order_by(models.Event.end.desc()).first()
+
                 submission = models.Submission(
-                    form=self.data.get('form'),
-                    contributor=self.data.get('participant'),
-                    location=self.data.get('participant').location,
-                    created=datetime.utcnow(),
-                    deployment=submission_event.deployment,
-                    event=submission_event)
+                    form=form,
+                    participant=participant,
+                    location=participant.location,
+                    created=utils.current_timestamp(),
+                    event=event,
+                    deployment_id=event.deployment_id)
                 if self.data.get('comment'):
-                    submission.description = self.data.get('comment')
+                    submission.incident_description = self.data.get('comment')
 
             if submission:
+                data = submission.data.copy() if submission.data else {}
                 form_fields = [f for f in list(self.data.keys()) if f not in ignored_fields]
                 change_detected = False
                 for form_field in form_fields:
-                    if isinstance(self.data.get(form_field), int):
-                        change_detected = True
-                        if (
-                            getattr(submission, form_field, None) !=
-                            self.data.get(form_field)
-                        ):
-                            setattr(
-                                submission, form_field,
-                                self.data.get(form_field))
+                    field_data = self.data.get(form_field)
 
-                    if (
-                        isinstance(self.data.get(form_field), list) and
-                            self.data.get(form_field)
-                    ):
+                    if isinstance(field_data, int):
                         change_detected = True
-                        original_value = getattr(submission, form_field, None)
+                        if data.get(form_field) != field_data:
+                            data[form_field] = field_data
+
+                    if isinstance(field_data, list) and field_data:
+                        change_detected = True
+                        original_value = data.get(form_field)
                         if isinstance(original_value, list):
                             original_value = sorted(original_value)
-                        if (original_value != self.data.get(form_field)):
-                            setattr(submission, form_field,
-                                    self.data.get(form_field))
+                        if (original_value != field_data):
+                            data[form_field] = field_data
 
                 if change_detected:
-                    g.phone = self.data.get('sender')
+                    phone_num = ugly_phone.sub('', self.data.get('sender'))
+                    g.phone = phone_num
 
                     # confirm if phone number is known and verified
                     participant = self.data.get('participant')
 
                     # retrieve the first phone contact that matches
-                    phone_contact = next(filter(
-                        lambda phone: ugly_phone.sub('', g.phone) ==
-                        phone.number,
-                        participant.phones), False)
+                    phone_contact = services.participant_phones.lookup(
+                        phone_num, participant)
+
                     if phone_contact:
                         submission.sender_verified = phone_contact.verified
-                        phone_contact.last_seen = datetime.utcnow()
-                        participant.save()
-                    else:
-                        submission.sender_verified = False
-                        phone_contact = models.PhoneContact(
-                            number=g.phone,
-                            verified=False, last_seen=datetime.utcnow())
-                        participant.update(add_to_set__phones=phone_contact)
+                        phone_contact.last_seen = utils.current_timestamp()
 
-                    submission.save()
-                    update_submission_version(submission)
+                        if commit:
+                            phone_contact.save()
+                    else:
+                        if submission.id is None:
+                            submission.sender_verified = False
+                        else:
+                            update_params['sender_verified'] = False
+
+                        phone = services.phones.get_by_number(phone_num)
+                        if not phone:
+                            phone = models.Phone(number=phone_num)
+
+                        phone_contact = models.ParticipantPhone(
+                            phone=phone, participant_id=participant.id,
+                            verified=False)
+
+                        if commit:
+                            phone.save()
+                            phone_contact.save()
+
+                    if commit:
+                        submission.data = data
+                        if submission.id is None:
+                            # for a fresh submission, everything will get saved
+                            submission.save()
+                        else:
+                            # for an existing submission, we need an update,
+                            # otherwise the JSONB field won't get persisted
+                            update_params['data'] = data
+                            services.submissions.find(
+                                id=submission.id
+                            ).update(update_params, synchronize_session=False)
+
+                        update_submission_version(submission)
 
                     # update completion rating for participant
-                    if submission.form.form_type == 'CHECKLIST':
-                        update_participant_completion_rating(participant)
-        except models.Submission.DoesNotExist:
+                    # if submission.form.form_type == 'CHECKLIST':
+                    #     update_participant_completion_rating(participant)
+        except Exception:
             pass
 
         return submission
@@ -220,15 +254,15 @@ def build_questionnaire(form, data=None):
                 choices = [(v, k) for k, v in field.get('options').items()]
 
                 if field['type'] == 'multiselect':
-                    fields[field['name']] = IntegerSplitterField(
-                        field['name'],
+                    fields[field['tag']] = IntegerSplitterField(
+                        field['tag'],
                         choices=choices,
                         description=field['description'],
                         validators=[validators.optional()],
                     )
                 else:
-                    fields[field['name']] = SelectField(
-                        field['name'],
+                    fields[field['tag']] = SelectField(
+                        field['tag'],
                         choices=choices,
                         coerce=int,
                         description=field['description'],
@@ -243,12 +277,12 @@ def build_questionnaire(form, data=None):
                 else:
                     field_validators = [
                         validators.optional(),
-                        validators.NumberRange(min=field['min_value'],
-                                               max=field['max_value'])
+                        validators.NumberRange(min=field.get('min', 0),
+                                               max=field.get('max', 9999))
                     ]
 
-                fields[field['name']] = IntegerField(
-                    field['name'],
+                fields[field['tag']] = IntegerField(
+                    field['tag'],
                     description=field['description'],
                     validators=field_validators)
 
