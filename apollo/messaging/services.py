48a1bc91517211ba5d7763bf8124d4a3fa02de54
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-03-29 17:38:39 +0000

    Updated message service to allow message logging

--- apollo/messaging/services.py
+++ apollo/messaging/services.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from apollo.dal.service import Service
 from apollo.messaging.models import Message
 
 
 class MessageService(Service):
     __model__ = Message
+
+    def log_message(self, event, direction, text, recipient='', sender='',
+                    timestamp=None):
+        if timestamp:
+            try:
+                msg_time = datetime.utcfromtimestamp(timestamp)
+            except ValueError:
+                msg_time = datetime.utcnow()
+        else:
+            msg_time = datetime.utcnow()
+
+        return self.create(
+            direction=direction, recipient=recipient, sender=sender, text=text,
+            deployment_id=event.deployment_id, event=event, received=msg_time)
