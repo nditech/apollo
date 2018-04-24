406ebc466c19f7c4790b161d0c8768e252e5c684
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-04-24 15:44:05 +0100

    Bugfix: column overwrite for extra fields

--- apollo/participants/forms.py
+++ apollo/participants/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask_babelex import lazy_gettext as _
 from flask_wtf import FlaskForm
+from slugify import slugify_unicode
 from wtforms import FileField, SelectField
 
 from apollo.frontend.forms import _make_choices
@@ -16,7 +17,8 @@ def make_import_mapping_form(import_file, participant_set):
     map_choices = _make_choices(participant_set.get_import_fields().items())
 
     data_frame = load_source_file(import_file)
-    for index, column in enumerate(data_frame.columns):
-        attributes[str(index)] = SelectField(column, choices=map_choices)
+    for column in data_frame.columns:
+        attributes[slugify_unicode(column)] = SelectField(
+            column, choices=map_choices)
 
     return type('ParticipantImportMapForm', (FlaskForm,), attributes)
