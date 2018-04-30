1f221e88f9b70cfd2e8c61972750d5605f0e89e4
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-04-30 12:16:43 +0100

    Added utility function to remove the BOM from an uploaded file

--- apollo/utils.py
+++ apollo/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import codecs
 from datetime import datetime
 import os
 from uuid import UUID
@@ -37,3 +38,22 @@ def validate_uuid(uuid_string):
         return True
     except ValueError:
         return False
+
+
+def remove_bom_in_place(path):
+    buffer_size = 4096
+    bom_length = len(codecs.BOM_UTF8)
+
+    with open(path, 'r+b') as fp:
+        chunk = fp.read(buffer_size)
+        if chunk.startswith(codecs.BOM_UTF8):
+            i = 0
+            chunk = chunk[bom_length:]
+            while chunk:
+                fp.seek(i)
+                fp.write(chunk)
+                i += len(chunk)
+                fp.seek(bom_length, os.SEEK_CUR)
+                chunk = fp.read(buffer_size)
+            fp.seek(-bom_length, os.SEEK_CUR)
+            fp.truncate()
