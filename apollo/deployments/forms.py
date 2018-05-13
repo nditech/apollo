6d8b56ae18aee8a9252890729593d0808f3a17c2
Dipo Odumosu <dodumosu@timbaobjects.com>
2018-05-13 09:45:43 +0100

    Restrict event choices in selection to those user has access to

--- apollo/deployments/forms.py
+++ apollo/deployments/forms.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import g
 from flask_babelex import lazy_gettext as _
+from flask_security import current_user
 from flask_wtf import FlaskForm
 from wtforms import SelectField, fields, validators
 
+from apollo import services
 from apollo.deployments.models import Event
 from apollo.formsframework.models import FormSet
 from apollo.locations.models import LocationSet
@@ -11,9 +13,26 @@ from apollo.participants.models import ParticipantSet
 
 
 def generate_event_selection_form(*args, **kwargs):
-    choices = [
-        (e.id, e.name) for e in Event.query.with_entities(
-            Event.id, Event.name).order_by(Event.start.desc())]
+    # restrict choices to events they have permission to access
+    if current_user.is_anonymous:
+        choices = []
+    else:
+        user = current_user._get_current_object()
+        if user.is_admin():
+            query = Event.query.filter_by(
+                deployment_id=user.deployment_id)
+        else:
+            perm_cache = services.users.get_permissions_cache(user)
+            allowed_event_ids = [
+                n.value for n in perm_cache
+                if n.method == 'access_resource' and n.type == 'event']
+            query = Event.query.filter(
+                Event.resource_id.in_(allowed_event_ids),
+                Event.deployment_id == user.deployment_id)
+
+        choices = [
+            (e.id, e.name) for e in query.with_entities(
+                Event.id, Event.name).order_by(Event.start.desc())]
 
     class EventSelectionForm(FlaskForm):
         event = SelectField(
