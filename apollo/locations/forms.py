bedac31833c46b1a243c27ef0f31efab4e548167
Odumosu Dipo <ironkyo@gmail.com>
2018-06-14 13:30:40 +0100

    feat: Add validation function for location mapping
    See #36

--- apollo/locations/forms.py
+++ apollo/locations/forms.py
@@ -22,8 +22,31 @@ class AdminDivisionImportForm(FlaskForm):
     import_file = FileField(_('Import file'), validators=[InputRequired()])
 
 
+def _validate_mappings(form):
+    rv = FlaskForm.validate(form)
+
+    errors = []
+    location_set = form.location_set
+    mapped_values = form.data.values()
+
+    for location_type in location_set.location_types:
+        str_id = str(location_type.id)
+        lt_values = [v for v in mapped_values if v.startswith(str_id)]
+
+        if lt_values:
+            if '{}_code'.format(location_type.id) not in lt_values:
+                errors.append(
+                    _('Properties for %(loc_type_name)s were mapped, but no code was specified',
+                      loc_type_name=location_type.name))
+                rv = False
+
+    form.errors['__validate__'] = errors
+
+    return rv
+
+
 def make_import_mapping_form(import_file, location_set):
-    attributes = {}
+    attributes = {'location_set': location_set, 'validate': _validate_mappings}
     map_choices = _make_choices(location_set.get_import_fields().items())
 
     data_frame = load_source_file(import_file)
