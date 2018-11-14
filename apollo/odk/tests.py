545226e0a115e5e55fa6818eb8e78d7acf85be61
Odumosu Dipo <ironkyo@gmail.com>
2018-11-14 11:25:30 +0100

    fix: made source PEP8 compliant

--- apollo/odk/tests.py
+++ apollo/odk/tests.py
@@ -29,8 +29,10 @@ class ODKTest(TestCase):
         time_zone = gettz(settings.TIMEZONE)
         current_timestamp = datetime.now(time_zone)
 
-        start = current_timestamp.replace(hour=0, minute=0, second=0, microsecond=0)
-        end = current_timestamp.replace(hour=23, minute=59, second=59, microsecond=999999)
+        start = current_timestamp.replace(hour=0, minute=0, second=0,
+                                          microsecond=0)
+        end = current_timestamp.replace(hour=23, minute=59, second=59,
+                                        microsecond=999999)
 
         start2 = start + timedelta(days=1)
         end2 = end + timedelta(days=1)
@@ -38,15 +40,20 @@ class ODKTest(TestCase):
         self._form_set_1 = models.FormSet(name='Form Set 1', deployment=self._deployment)
         self._form_set_2 = models.FormSet(name='Form Set 2', deployment=self._deployment)
 
-        self._location_set_1 = models.LocationSet(name='Location Set 1', deployment=self._deployment)
-        self._location_set_2 = models.LocationSet(name='Location Set 2', deployment=self._deployment)
+        self._location_set_1 = models.LocationSet(name='Location Set 1',
+                                                  deployment=self._deployment)
+        self._location_set_2 = models.LocationSet(name='Location Set 2',
+                                                  deployment=self._deployment)
 
-        # self._participant_set_1 = models.ParticipantSet(name='Participant Set 1', deployment=self._deployment)
-        # self._participant_set_2 = models.ParticipantSet(name='Participant Set 2', deployment=self._deployment)
-
-        self._form1 = models.Form(name='Form 1', prefix='XA', deployment=self._deployment, form_type='CHECKLIST', form_set=self._form_set_1)
-        self._form2 = models.Form(name='Form 2', prefix='XB', deployment=self._deployment, form_type='CHECKLIST', form_set=self._form_set_1)
-        self._form3 = models.Form(name='Form 3', prefix='XC', deployment=self._deployment, form_type='CHECKLIST', form_set=self._form_set_2)
+        self._form1 = models.Form(name='Form 1', prefix='XA',
+                                  deployment=self._deployment, form_type='CHECKLIST',
+                                  form_set=self._form_set_1)
+        self._form2 = models.Form(name='Form 2', prefix='XB',
+                                  deployment=self._deployment, form_type='CHECKLIST',
+                                  form_set=self._form_set_1)
+        self._form3 = models.Form(name='Form 3', prefix='XC',
+                                  deployment=self._deployment, form_type='CHECKLIST',
+                                  form_set=self._form_set_2)
 
         self._event1 = models.Event(name='Event 1', deployment=self._deployment,
                                     start=start, end=end, form_set=self._form_set_1)
@@ -62,8 +69,6 @@ class ODKTest(TestCase):
             self._form1,
             self._form2,
             self._form3,
-            # self._participant_set_1,
-            # self._participant_set_2,
             self._event1,
             self._event2,
             self._event3,
