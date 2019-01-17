7a785d03e24f84af525cee7ecc6644dd65cdf46d
Odumosu Dipo <ironkyo@gmail.com>
2019-01-17 11:58:45 +0100

    feat: add locale selection for location imports

--- apollo/locations/forms.py
+++ apollo/locations/forms.py
@@ -2,10 +2,10 @@
 from flask_babelex import lazy_gettext as _
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from wtforms.fields import SelectField, StringField
+from wtforms.fields import SelectField
 from wtforms.validators import InputRequired
-from wtforms.widgets import HiddenInput
 
+from apollo.deployments.utils import get_deployment_locales
 from apollo.frontend.forms import _make_choices
 from apollo.helpers import load_source_file
 
@@ -42,8 +42,13 @@ def make_import_mapping_form(import_file, location_set):
     attributes = {'location_set': location_set, 'validate': _validate_mappings}
     map_choices = _make_choices(location_set.get_import_fields().items())
 
+    locale_choices = get_deployment_locales(location_set.deployment_id)
+
     data_frame = load_source_file(import_file)
     for index, column in enumerate(data_frame.columns):
         attributes[str(index)] = SelectField(column, choices=map_choices)
+
+    attributes['locale'] = SelectField(
+        _('Language'), choices=locale_choices, validators=[InputRequired()])
 
     return type('LocationImportMapForm', (FlaskForm,), attributes)
