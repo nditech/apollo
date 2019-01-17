c87b38d131108d4f45d3e373a8135c4bf819cd81
Odumosu Dipo <ironkyo@gmail.com>
2019-01-17 12:02:21 +0100

    feat: make location exporter translation aware

--- apollo/locations/services.py
+++ apollo/locations/services.py
@@ -4,6 +4,7 @@ from io import StringIO
 
 from apollo import constants
 from apollo.dal.service import Service
+from apollo.deployments.utils import get_deployment_locales
 from apollo.locations.models import Location, LocationSet, LocationType, Sample
 
 
@@ -17,16 +18,24 @@ class LocationService(Service):
     def export_list(self, query):
         headers = []
         location_set_id = query.first().location_set_id
+        deployment_id = query.first().deployment_id
+
+        locales = get_deployment_locales(deployment_id)
+
         location_types = LocationTypeService().find(
             location_set_id=location_set_id
         ).join()
 
         for location_type in location_types:
-            location_name = location_type.name.upper()
-            headers.append('{}_N'.format(location_name))
-            headers.append('{}_ID'.format(location_name))
+            location_type_name = location_type.name.upper()
+            type_locale_headers = [
+                f'{location_type_name}_N_{locale.upper()}'
+                for locale in locales
+            ]
+            headers.extend(type_locale_headers)
+            headers.append('{}_ID'.format(location_type_name))
             if location_type.has_registered_voters:
-                headers.append('{}_RV'.format(location_name))
+                headers.append('{}_RV'.format(location_type_name))
 
         output = StringIO()
         output.write(constants.BOM_UTF8_STR)
@@ -40,13 +49,16 @@ class LocationService(Service):
             record = []
             ancestors = location.ancestors()
             for ancestor in ancestors:
-                record.append(ancestor.name)
+                for locale in locales:
+                    record.append(ancestor.translations[locale].name)
+
                 record.append(ancestor.code)
 
                 if ancestor.location_type.has_registered_voters:
                     record.append(ancestor.registered_voters)
 
-            record.append(location.name)
+            for locale in locales:
+                record.append(location.translations[locale].name)
             record.append(location.code)
 
             if location.location_type.has_registered_voters:
