b85af3b7635d99224927872e5b57e1b3588438c3
Odumosu Dipo <ironkyo@gmail.com>
2019-01-21 15:07:36 +0100

    feat: readact Excel I/O from models module
    also add location data type and update xml export and serializer

--- apollo/formsframework/models.py
+++ apollo/formsframework/models.py
@@ -2,6 +2,7 @@
 import logging
 from operator import itemgetter
 import re
+from uuid import uuid4
 
 from flask_babelex import lazy_gettext as _
 from lxml import etree
@@ -10,7 +11,6 @@ from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy_utils import ChoiceType
 from slugify import slugify_unicode
 from unidecode import unidecode
-from xlwt import Workbook
 
 from apollo.core import db
 from apollo.dal.models import BaseModel, Resource
@@ -18,25 +18,30 @@ from apollo.dal.models import BaseModel, Resource
 NSMAP = {
     None: 'http://www.w3.org/2002/xforms',
     'h': 'http://www.w3.org/1999/xhtml',
-    'ev': 'http://www.w3.org/2001/xml-events',
     'xsd': 'http://www.w3.org/2001/XMLSchema',
-    'jr': 'http://openrosa.org/javarosa'
+    'jr': 'http://openrosa.org/javarosa',
+    'odk': 'http://www.opendatakit.org/xforms',
+    'orx': 'http://openrosa.org/xforms',
 }
 
 HTML_E = ElementMaker(namespace=NSMAP['h'], nsmap=NSMAP)
-EVT_E = ElementMaker(namespace=NSMAP['ev'], nsmap=NSMAP)
 SCHEMA_E = ElementMaker(namespace=NSMAP['xsd'], nsmap=NSMAP)
 ROSA_E = ElementMaker(namespace=NSMAP['jr'], nsmap=NSMAP)
 
+
 FIELD_TYPES = (
     'boolean', 'comment', 'integer', 'select', 'multiselect',
-    'category', 'string'
+    'category', 'string', 'location'
 )
 
 logger = logging.getLogger(__name__)
 
 gt_constraint_regex = re.compile('(?:.*\.\s*\>={0,1}\s*)(\d+)')
 lt_constraint_regex = re.compile('(?:.*\.\s*\<={0,1}\s*)(\d+)')
+
+
+def _make_version_identifer():
+    return uuid4().hex
 
 
 class FormSet(BaseModel):
@@ -72,7 +77,7 @@ class Form(Resource):
     require_exclamation = db.Column(db.Boolean, default=True)
     track_data_conflicts = db.Column(db.Boolean, default=True, nullable=False)
     data = db.Column(JSONB)
-    version_identifier = db.Column(db.String)
+    version_identifier = db.Column(db.String, default=_make_version_identifer)
     form_set_id = db.Column(
         db.Integer, db.ForeignKey('form_set.id'), nullable=False)
     resource_id = db.Column(
@@ -141,168 +146,19 @@ class Form(Resource):
 
         return tags
 
-    def to_excel(self):
-        book = Workbook()
-        survey_sheet = book.add_sheet('survey')
-        choices_sheet = book.add_sheet('choices')
-        analysis_sheet = book.add_sheet('analysis')
-        metadata_sheet = book.add_sheet('metadata')
-
-        survey_header = ['type', 'name', 'label', 'constraints']
-        choices_header = ['list name', 'name', 'label']
-        analysis_header = ['name', 'analysis']
-        metadata_header = ['name', 'prefix', 'form_type',
-                           'require_exclamation', 'calculate_moe',
-                           'accredited_voters_tag', 'invalid_votes_tag',
-                           'registered_voters_tag', 'blank_votes_tag',
-                           'quality_checks_enabled']
-
-        # HEADERS
-        for col, value in enumerate(survey_header):
-            survey_sheet.write(0, col, value)
-
-        for col, value in enumerate(choices_header):
-            choices_sheet.write(0, col, value)
-
-        for col, value in enumerate(analysis_header):
-            analysis_sheet.write(0, col, value)
-
-        for col, value in enumerate(metadata_header):
-            metadata_sheet.write(0, col, value)
-
-        # fill out form metadata
-        metadata_sheet.write(1, 0, self.name)
-        metadata_sheet.write(1, 1, self.prefix)
-        metadata_sheet.write(1, 2, self.form_type.code)
-        metadata_sheet.write(1, 3, 1 if self.require_exclamation else 0)
-        metadata_sheet.write(1, 4, 1 if self.calculate_moe else 0)
-        metadata_sheet.write(1, 5, self.accredited_voters_tag)
-        metadata_sheet.write(1, 6, self.invalid_votes_tag)
-        metadata_sheet.write(1, 7, self.registered_voters_tag)
-        metadata_sheet.write(1, 8, self.blank_votes_tag)
-        metadata_sheet.write(1, 9, 1 if self.quality_checks_enabled else 0)
-
-        current_survey_row = 1
-        current_choices_row = 1
-        current_analysis_row = 1
-        groups = self.data.get('groups')
-        if groups and isinstance(groups, list):
-            boolean_written = False
-            current_group = None
-            for group in groups:
-                if not group:
-                    continue
-
-                if current_group:
-                    current_group = group
-                    survey_sheet.write(current_survey_row, 0, 'end group')
-                    current_survey_row += 1
-                survey_sheet.write(current_survey_row, 0, 'begin group')
-                survey_sheet.write(
-                    current_survey_row, 1, slugify_unicode(group['name']))
-                survey_sheet.write(current_survey_row, 2, group['name'])
-                current_survey_row += 1
-                current_group = group
-
-                fields = group.get('fields')
-                if fields and isinstance(fields, list):
-                    for field in fields:
-                        # output the type
-                        if field['type'] == 'integer':
-                            survey_sheet.write(
-                                current_survey_row, 0, 'integer')
-                            survey_sheet.write(
-                                current_survey_row, 3,
-                                '. >= {} and . <= {}'.format(
-                                    field.get('min', 0),
-                                    field.get('max', 9999)))
-                        elif field['type'] == 'boolean':
-                            survey_sheet.write(
-                                current_survey_row, 0, 'select_one boolean')
-
-                            # write out boolean choices if they haven't been
-                            # written before
-                            if not boolean_written:
-                                choices_sheet.write(
-                                    current_choices_row, 0, 'boolean')
-                                choices_sheet.write(current_choices_row, 1, 0)
-                                choices_sheet.write(
-                                    current_choices_row, 2, 'False')
-                                current_choices_row += 1
-                                choices_sheet.write(
-                                    current_choices_row, 0, 'boolean')
-                                choices_sheet.write(current_choices_row, 1, 1)
-                                choices_sheet.write(
-                                    current_choices_row, 2, 'True')
-                                boolean_written = True
-
-                        elif field['type'] in ('comment', 'string'):
-                            survey_sheet.write(current_survey_row, 0, 'text')
-                        else:
-                            # for questions with choices, write them to the
-                            # choices sheet
-                            option_list_name = '{}_options'.format(
-                                field['tag'])
-                            options = field.get('options')
-                            for description, value in options.items():
-                                choices_sheet.write(
-                                    current_choices_row, 0, option_list_name)
-                                choices_sheet.write(
-                                    current_choices_row, 1, value)
-                                choices_sheet.write(
-                                    current_choices_row, 2, description)
-                                current_choices_row += 1
-
-                            if field['type'] == 'select':
-                                survey_sheet.write(
-                                    current_survey_row, 0,
-                                    'select_one {}'.format(option_list_name))
-                            else:
-                                survey_sheet.write(
-                                    current_survey_row, 0,
-                                    'select_multiple {}'.format(
-                                        option_list_name))
-
-                        # output the name and description
-                        survey_sheet.write(current_survey_row, 1, field['tag'])
-                        survey_sheet.write(
-                            current_survey_row, 2, field['description'])
-                        current_survey_row += 1
-
-                        # also output the analysis
-                        analysis_sheet.write(
-                            current_analysis_row, 0, field['tag'])
-                        analysis_sheet.write(
-                            current_analysis_row, 1, field['analysis_type'])
-                        current_analysis_row += 1
-
-            if current_group:
-                survey_sheet.write(current_survey_row, 0, 'end group')
-
-        return book
-
     def to_xml(self):
         root = HTML_E.html()
         head = HTML_E.head(HTML_E.title(self.name))
-        data = E.data(id='-1')
+        data = E.data(id=self.uuid.hex)
         model = E.model(E.instance(data))
 
         body = HTML_E.body()
-        model.append(E.bind(nodeset='/data/form_id', readonly='true()'))
-        model.append(E.bind(nodeset='/data/version_id', readonly='true()'))
-
-        form_id = etree.Element('form_id')
-        form_id.text = str(self.id)
-
-        version_id = etree.Element('version_id')
-        version_id.text = self.version_identifier
-
-        data.append(form_id)
-        data.append(version_id)
 
         data.append(E.device_id())
         data.append(E.subscriber_id())
         data.append(E.phone_number())
+        data.attrib['{{{}}}version'.format(NSMAP['orx'])] = \
+            self.version_identifier
 
         device_id_bind = E.bind(nodeset='/data/device_id')
         device_id_bind.attrib['{{{}}}preload'.format(NSMAP['jr'])] = \
@@ -337,6 +193,10 @@ class Form(Resource):
                             ref=field['tag']
                         )
                     model.append(E.bind(nodeset=path, type='select1'))
+                elif field_type == 'location':
+                    field_element = E.input(
+                        E.label(field['description']), ref=field['tag'])
+                    model.append(E.bind(nodeset=path, type='geopoint'))
                 elif field_type in ('comment', 'string'):
                     field_element = E.input(
                             E.label(field['description']),
@@ -354,10 +214,10 @@ class Form(Resource):
                             field.get('min', 0),
                             field.get('max', 9999)
                         )))
-                elif field_type in ('select', 'multiselect'):
+                elif field_type in ('select', 'multiselect', 'category'):
                     sorted_options = sorted(field.get('options').items(),
                                             key=itemgetter(1))
-                    if field_type == 'select':
+                    if field_type == 'select' or field_type == 'category':
                         element_factory = E.select1
                         model.append(E.bind(nodeset=path, type='select1'))
                     else:
@@ -406,7 +266,7 @@ class FormBuilderSerializer(object):
             data['component'] = 'textInput'
             data['min'] = field.get('min', 0)
             data['max'] = field.get('max', 9999)
-        elif field_type in ('select', 'multiselect'):
+        elif field_type in ('select', 'multiselect', 'category'):
             sorted_options = sorted(field['options'].items(),
                                     key=itemgetter(1))
             data['options'] = [opt[0] for opt in sorted_options]
