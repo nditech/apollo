d1d4e676157f439f383da3270561542259f26e1c
Odumosu Dipo <ironkyo@gmail.com>
2019-01-24 14:26:55 +0100

    chore: rename parameter

--- apollo/testutils/fixtures.py
+++ apollo/testutils/fixtures.py
@@ -11,8 +11,8 @@ from apollo.core import db
 logger = logging.getLogger(__name__)
 
 
-def load_sql_fixture(fixture_path):
-    source_file = pathlib.Path(fixture_path)
+def load_sql_fixture(fixture):
+    source_file = pathlib.Path(fixture)
     if source_file.exists() and source_file.is_file():
         query_text = text(source_file.read_text())
         try:
