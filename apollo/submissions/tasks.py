d6c36cf0c5ec88fb1be1e7e7d525737a82381a21
Odumosu Dipo <ironkyo@gmail.com>
2019-02-21 22:19:49 +0100

    fix: supply deployment and phone number for background update of submissions

--- apollo/submissions/tasks.py
+++ apollo/submissions/tasks.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from functools import partial
+
 from mongoengine import signals
 
 from apollo import models
@@ -27,15 +29,17 @@ def init_submissions(deployment_pk, event_pk, form_pk, role_pk,
 
 
 @celery.task
-def update_submission(submission_pk):
+def update_submission(submission_pk, sender):
     try:
         submission = models.Submission.objects.get(pk=submission_pk)
     except models.Submission.DoesNotExist:
         return
 
+    post_save_handler = partial(update_submission_version, sender)
+
     with signals.post_save.connected_to(
-            update_submission_version,
-            models.Submission):
+            post_save_handler,
+            sender=models.Submission):
         # save with precomputation enabled
         submission.save()
 
