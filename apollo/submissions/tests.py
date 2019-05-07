f50d8b307790f7519f2c304cbdff9401102d5cfd
dodumosu <ironkyo@gmail.com>
2019-05-07 13:22:02 +0100

    chore: add tests for aggregation (#299)

--- apollo/submissions/tests.py
+++ apollo/submissions/tests.py
@@ -1,6 +1,13 @@
+from unittest import TestCase
+
 import numpy as np
 import pandas as pd
-from unittest import TestCase
+
+from apollo.submissions.aggregation import (
+    _multiselect_field_processor,
+    _numeric_field_processor,
+    _select_field_processor,
+)
 from apollo.submissions.incidents import incidents_csv
 
 
@@ -19,3 +26,35 @@ class IncidentsTest(TestCase):
             incidents_csv(self.df, 'location', 'AA'),
             [{'LOC': 'A', 'TOT': 1, 'A': 0}, {'LOC': 'B', 'TOT': 5, 'A': 0},
              {'LOC': 'C', 'TOT': 4, 'A': 0}, {'LOC': 'D', 'TOT': 0, 'A': 0}])
+
+
+class AggregationTest(TestCase):
+    def test_numeric_field_aggregation(self):
+        column = pd.Series([1, 3, 5, 2, None, 7, 4])
+        result = _numeric_field_processor(column)
+        self.assertEqual(result, 22)
+
+    def test_select_field_aggregation(self):
+        options = [1, 2, 3, 4, 5]
+        column = pd.Series([5, 5, 5, 1, 2, None, 2, 4, 5, None])
+
+        result = _select_field_processor(options, column)
+        self.assertEqual(result, [1, 2, 0, 1, 4])
+
+    def test_multiselect_field_aggregation(self):
+        options = [1, 2, 3, 4, 5]
+        column = pd.Series([
+            [1, 5],
+            [1, 2, 3, 4, 5],
+            None,
+            [2, 3],
+            [3, 4, 5],
+            [1, 2, 3, 4],
+            None,
+            None,
+            [1, 2, 3, 4, 5],
+            None
+        ])
+
+        result = _multiselect_field_processor(options, column)
+        self.assertEqual(result, [4, 4, 5, 4, 4])
