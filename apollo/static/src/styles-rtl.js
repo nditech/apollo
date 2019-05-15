8ba17ffb81a1adc062207b92ee2925c31a83f82d
Tim Akinbo <takinbo@timbaobjects.com>
2019-05-15 09:19:52 +0000

    feat: bootstrap4 styling for the select2 plugin

--- apollo/static/src/styles-rtl.js
+++ apollo/static/src/styles-rtl.js
@@ -1,3 +1,4 @@
 import './scss/app-rtl.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'select2/dist/css/select2.css';
+import '@ttskch/select2-bootstrap4-theme/src/layout.scss';
