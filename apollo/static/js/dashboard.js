3da80d88509917ab822ce643843bea84a47a6e97
Tim Akinbo <takinbo@timbaobjects.com>
2019-05-17 17:04:19 +0000

    fix: change 'Off-line' label to 'No Signal'
    only display pie slice label is value is more than 5%

--- apollo/static/js/dashboard.js
+++ apollo/static/js/dashboard.js
@@ -6,8 +6,8 @@
     
     var json = JSON.parse(el.dataset.chart);
     var data = [json.Missing + (json.Conflict || 0), json.Partial, json.Complete, json.Offline];
-    var color = d3.scale.ordinal().range(["#ca0020", "#f4a582", "#0571b0", "#f7f7f7"]); // Conflict color #E83992
-    var labels = ['Missing', 'Partial', 'Complete', 'Off-line']; // Conflict label
+    var color = d3.scale.ordinal().range(["#ca0020", "#f4a582", "#0571b0", "#aaaaaa"]); // Conflict color #E83992
+    var labels = ['Missing', 'Partial', 'Complete', 'No Signal']; // Conflict label
     var total = data.reduce(function (prev, curr, idx, arr) { return prev + curr; });
 
     function number_format(num) {
@@ -44,13 +44,12 @@
       .attr("class", "arc")
       .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")
 
-    
       .append("text")
       .attr("transform", function(d) {
         return "translate(" + d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius).centroid(d) + ")";
       })
       .attr("text-anchor", "middle")
-      .text(function(d) {return (d.value/total * 100).toFixed(0) + '%';})
+      .text(function(d) {return d.value / total >= 0.05 ? (d.value/total * 100).toFixed(0) + '%' : '';})
       .style('font-size', '10px');
 
     //Legend
@@ -74,19 +73,17 @@
       .attr("x", w/4 - 16)
       .attr("y", h/2 + 24)
       .attr("dy", ".29em")
-      .style('font-family', 'courier')
-      .text(function(d) { return d})
-      .style('font-size', '12.5px');
+      .attr('class', 'text-monospace')
+      .text(function(d) { return d});
 
     //Legend data
     legend.append("text")
       .data(data)
-      .attr("x", w /2 + 15)
+      .attr("x", w /2 + 30)
       .attr("y", h/2 + 24)
       .attr("dy", ".29em")
-      .style('font-family', 'courier')
-      .text(function(d) { return '· ' +  d})
-      .style('font-size', '12.5px');
+      .attr("class", "text-monospace")
+      .text(function(d) { return '· ' +  d});
   }
 
   $(function () {
