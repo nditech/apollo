bc91907353651b2e3cf69a92a13da5ab2047839b
Odumosu Dipo <ironkyo@gmail.com>
2019-05-25 12:04:13 +0100

    fix: streamline query for dashboard
    this commit uses the ?| PostgreSQL operator for containment,
    rather than the ? operator with an OR chain used before.

--- apollo/frontend/dashboard.py
+++ apollo/frontend/dashboard.py
@@ -64,23 +64,28 @@ def _get_group_coverage(query, form, group, location_type):
         return coverage_list
 
     # get conflict submissions first
-    conflict_query_params = [
-        Submission.conflicts.has_key(tag)   # noqa
-        for tag in group_tags
-    ]
-
     conflict_query = query.filter(
-        or_(*conflict_query_params),
+        Submission.conflicts != None,
+        Submission.conflicts.has_any(array(group_tags)),
         Submission.unreachable != True)  # noqa
 
     missing_query = query.filter(
         ~Submission.data.has_any(array(group_tags)),
+        or_(
+            Submission.conflicts == None,
+            ~Submission.conflicts.has_any(array(group_tags))),
         Submission.unreachable != True)  # noqa
 
     complete_query = query.filter(
+        or_(
+            Submission.conflicts == None,
+            ~Submission.conflicts.has_any(array(group_tags))),
         Submission.data.has_all(array(group_tags)))
 
     partial_query = query.filter(
+        or_(
+            Submission.conflicts == None,
+            ~Submission.conflicts.has_any(array(group_tags))),
         ~Submission.data.has_all(array(group_tags)),
         Submission.data.has_any(array(group_tags)),
         Submission.unreachable != True)  # noqa
@@ -165,27 +170,28 @@ def _get_global_coverage(query, form):
     for group in groups:
         group_tags = form.get_group_tags(group['name'])
 
-        # get conflict submissions first
-        conflict_query_params = [
-            Submission.conflicts.has_key(tag)   # noqa
-            for tag in group_tags
-        ]
-
         conflict_query = query.filter(
-            or_(*conflict_query_params),
+            Submission.conflicts != None,
+            Submission.conflicts.has_any(array(group_tags)),
             Submission.unreachable != True)  # noqa
 
         missing_query = query.filter(
-            ~Submission.conflicts.has_any(array(group_tags)),
+            or_(
+                ~Submission.conflicts.has_any(array(group_tags)),
+                Submission.conflicts == None),
             ~Submission.data.has_any(array(group_tags)),
             Submission.unreachable != True)  # noqa
 
         complete_query = query.filter(
-            ~Submission.conflicts.has_any(array(group_tags)),
+            or_(
+                Submission.conflicts == None,
+                ~Submission.conflicts.has_any(array(group_tags))),
             Submission.data.has_all(array(group_tags)))
 
         partial_query = query.filter(
-            ~Submission.conflicts.has_any(array(group_tags)),
+            or_(
+                Submission.conflicts == None,
+                ~Submission.conflicts.has_any(array(group_tags))),
             ~Submission.data.has_all(array(group_tags)),
             Submission.data.has_any(array(group_tags)),
             Submission.unreachable != True)  # noqa
@@ -195,7 +201,6 @@ def _get_global_coverage(query, form):
                 Submission.unreachable == True,  # noqa
                 not_(
                     and_(
-                        ~Submission.conflicts.has_any(array(group_tags)),
                         Submission.data.has_all(array(group_tags)),
                         Submission.unreachable == True
                     )
