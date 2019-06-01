7553ff1cffb2aba71a274a11caac72bd5d1c7a44
Odumosu Dipo <ironkyo@gmail.com>
2019-06-01 17:28:03 +0100

    fix: workaround for default locale
    see: #341

--- apollo/dal/models.py
+++ apollo/dal/models.py
@@ -10,6 +10,7 @@ from uuid import uuid4
 
 from flask import g
 from flask_babelex import get_locale
+from sqlalchemy import Table
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy_utils import TranslationHybrid
@@ -28,10 +29,13 @@ def get_default_locale(obj, attr):
     except RuntimeError:
         raise
 
-    if locale in getattr(obj, attr).keys():
-        return locale
-    else:
-        return sorted(getattr(obj, attr).keys())[0]
+    if isinstance(obj, (db.Model, Table)):
+        if locale in getattr(obj, attr).keys():
+            return locale
+        else:
+            return sorted(getattr(obj, attr).keys())[0]
+
+    return locale
 
 
 translation_hybrid = TranslationHybrid(
