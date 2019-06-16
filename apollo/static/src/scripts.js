764bfdf330dca6a9467aa7f48b8df282bae4af86
Tim Akinbo <takinbo@timbaobjects.com>
2019-06-16 13:13:43 +0000

    ui: added bs-custom-file-input for file input animation

--- apollo/static/src/scripts.js
+++ apollo/static/src/scripts.js
@@ -3,6 +3,11 @@
 import $ from 'jquery';
 import './js/app.js';
 import 'select2/dist/js/select2.js';
+import bsCustomFileInput from 'bs-custom-file-input';
+
+$(document).ready(function () {
+    bsCustomFileInput.init()
+});
 
 window.jQuery = $;
 window.$ = $;
