1f3d7c428dbca81868d6ea79323ef1596b45eb9c
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2019-06-17 08:01:07 +0000

    user interface fixes and improvements (#377)
    * ui: added bs-custom-file-input for file input animation
    
    * ui: reset error state on file upload widget
    
    * fix: removed participants performance
    
    * ui: removed modal for editing participants
    
    * fix: edit participant
    
    * fix: make text input monospace

--- apollo/participants/views_participants.py
+++ apollo/participants/views_participants.py
@@ -4,7 +4,7 @@ import logging
 import os
 import re
 
-from flask import (abort, Blueprint, current_app, flash, g, redirect,
+from flask import (abort, Blueprint, current_app, g, redirect,
                    render_template, request, Response, url_for,
                    stream_with_context)
 from flask_babelex import lazy_gettext as _
@@ -16,8 +16,7 @@ from slugify import slugify_unicode
 from apollo import models, services, utils
 from apollo.core import sentry, uploads
 from apollo.frontend import helpers, permissions, route
-from apollo.frontend.forms import (
-    DummyForm, generate_participant_edit_form)
+from apollo.frontend.forms import generate_participant_edit_form
 from apollo.messaging.tasks import send_messages
 from apollo.participants import api, filters, forms, tasks
 
@@ -25,7 +24,6 @@ from .models import Participant, ParticipantSet, ParticipantRole
 from .models import ParticipantPartner, Phone, ParticipantPhone
 from .models import ParticipantTranslations
 from ..locations.models import Location
-from ..messaging.models import Message
 from ..submissions.models import Submission
 from ..users.models import UserUpload
 
@@ -300,133 +298,6 @@ def participant_list(participant_set_id=0):
     )
 
 
-@route(bp, '/participants/set/<int:participant_set_id>/performance',
-       endpoint="participant_performance_list_with_set", methods=['GET'])
-@route(bp, '/participants/performance',
-       endpoint='participant_performance_list', methods=['GET'])
-@login_required
-@permissions.view_participants.require(403)
-def participant_performance_list(participant_set_id=0):
-    if participant_set_id:
-        participant_set = ParticipantSet.query.filter(
-            ParticipantSet.id == participant_set_id).first_or_404()
-        template_name = 'frontend/participant_performance_list_with_set.html'
-    else:
-        participant_set = g.event.participant_set or abort(404)
-        template_name = 'frontend/participant_performance_list.html'
-
-    page_title = _('Participants Performance')
-
-    sortable_columns = {
-        'id': 'participant_id',
-        'name': 'name',
-        'gen': 'gender'
-    }
-
-    # extra_fields = g.deployment.participant_extra_fields or []
-    extra_fields = []
-    location = None
-    if request.args.get('location'):
-        location = Location.query.filter(
-            Location.id == request.args.get('location')).first()
-
-    for field in extra_fields:
-        sortable_columns.update({field.name: field.name})
-
-    queryset = Participant.query.select_from(
-        Participant, ParticipantTranslations).filter()
-    sample_participant = queryset.first()
-    if sample_participant:
-        location_set_id = sample_participant.location.location_set_id
-    else:
-        location_set_id = None
-
-    filter_class = filters.participant_filterset(
-        participant_set.id, location_set_id)
-    queryset_filter = filter_class(queryset, request.args)
-
-    form = DummyForm(request.form)
-
-    if request.form.get('action') == 'send_message':
-        message = request.form.get('message', '')
-        recipients = [participant.phone if participant.phone else ''
-                      for participant in queryset_filter.qs]
-        recipients.extend(current_app.config.get('MESSAGING_CC'))
-
-        if message and recipients and permissions.send_messages.can():
-            send_messages.delay(str(g.event.pk), message, recipients)
-            return 'OK'
-        else:
-            abort(400)
-
-    if request.args.get('export') and permissions.export_participants.can():
-        # Export requested
-        dataset = services.participants.export_performance_list(
-            queryset_filter.qs)
-        basename = slugify_unicode('%s participants performance %s' % (
-            g.event.name.lower(),
-            datetime.utcnow().strftime('%Y %m %d %H%M%S')))
-        content_disposition = 'attachment; filename=%s.csv' % basename
-        return Response(
-            dataset, headers={'Content-Disposition': content_disposition},
-            mimetype="text/csv"
-        )
-    else:
-        # request.args is immutable, so the .pop() call will fail on it.
-        # using .copy() returns a mutable version of it.
-        args = request.args.to_dict(flat=False)
-        page = int(args.pop('page', [1])[0])
-        if participant_set_id:
-            args['participant_set_id'] = participant_set_id
-
-        sort_by = sortable_columns.get(
-            args.pop('sort_by', ''), 'participant_id')
-        subset = queryset_filter.qs.order_by(sort_by).distinct(sort_by)
-
-        # load form context
-        context = dict(
-            args=args,
-            extra_fields=extra_fields,
-            filter_form=queryset_filter.form,
-            form=form,
-            location=location,
-            location_set_id=location_set_id,
-            page_title=page_title,
-            participant_set=participant_set,
-            participant_set_id=participant_set.id,
-            location_types=helpers.displayable_location_types(
-                is_administrative=True, location_set_id=location_set_id),
-            participants=subset.paginate(
-                page=page, per_page=current_app.config.get('PAGE_SIZE'))
-        )
-
-        return render_template(
-            template_name,
-            **context
-        )
-
-
-@route(bp, '/participant/performance/<pk>')
-@login_required
-@permissions.view_participants.require(403)
-def participant_performance_detail(pk):
-    participant = Participant.query.get_or_404(pk)
-    page_title = _('Participant Performance')
-    template_name = 'frontend/participant_performance_detail.html'
-    messages = Message.query.filter(
-        Message.participant == participant,
-        Message.direction == 'IN'
-    ).order_by('received')
-
-    context = {
-        'participant': participant,
-        'messages': messages,
-        'page_title': page_title
-    }
-
-    return render_template(template_name, **context)
-
-
 @route(bp, '/participant/phone/verify', methods=['POST'])
 @login_required
 @permissions.edit_participant.require(403)
@@ -463,15 +334,13 @@ def toggle_phone_verification():
 @permissions.edit_participant.require(403)
 def participant_edit(id, participant_set_id=0):
     participant = Participant.query.get_or_404(id)
-    page_title = _(
-        'Edit Participant · %(participant_id)s',
-        participant_id=participant.participant_id
-    )
+    breadcrumbs = [
+        _('Participants'),
+        _('Edit Participant'),
+        participant.participant_id
+    ]
 
-    if request.is_xhr:
-        template_name = 'frontend/participant_edit_modal.html'
-    else:
-        template_name = 'frontend/participant_edit.html'
+    template_name = 'frontend/participant_edit.html'
 
     if request.method == 'GET':
         form = generate_participant_edit_form(participant)
@@ -492,13 +361,11 @@ def participant_edit(id, participant_set_id=0):
                 participant.role_id = ParticipantRole.query.get_or_404(
                     form.role.data).id
             if form.supervisor.data:
-                participant.supervisor_id = Participant.query.filter(
-                    Participant.id == form.supervisor.data).first().id
+                participant.supervisor = form.supervisor.data
             else:
-                participant.supervisor_id = None
+                participant.supervisor = None
             if form.location.data:
-                participant.location_id = Location.query.get_or_404(
-                    form.location.data).id
+                participant.location = form.location.data
             if form.partner.data:
                 participant.partner_id = ParticipantPartner.query.get_or_404(
                     form.partner.data).id
@@ -553,7 +420,7 @@ def participant_edit(id, participant_set_id=0):
                 return redirect(url_for('participants.participant_list'))
 
     return render_template(
-        template_name, form=form, page_title=page_title,
+        template_name, form=form, breadcrumbs=breadcrumbs,
         participant_set_id=participant_set_id,
         participant=participant)
 
--- apollo/static/src/js/custom.js
+++ apollo/static/src/js/custom.js
@@ -43,6 +43,52 @@ window.LocationOptions = {
     escapeMarkup: function (m) { return m; }
 };
 
+window.ParticipantOptions = {
+    width: 'null',
+    theme: 'bootstrap4',
+    minimumInputLength: 1,
+    allowClear: true,
+    placeholder: {
+        id: '__None',
+        text: 'Participant'
+    },
+    ajax: {
+        url: '/api/participants/',
+        dataType: 'json',
+        delay: 500,
+        data: function (params) {
+            return {
+                q: params.term,
+                limit: 20,
+                offset: (params.page - 1) * 20 || 0
+            };
+        },
+        processResults: function (data, params) {
+            return {
+                results: data.objects,
+                pagination: {
+                    more: (params.page * 20) < data.meta.total
+                }
+            };
+        }
+    },
+    templateResult: function (participant) {
+        if (participant.participant_id) {
+            return safe_string(participant.participant_id) + ' · ' + safe_string(participant.name);
+        } else {
+            return safe_string(participant.text);
+        }
+    },
+    templateSelection: function (participant) {
+        if (participant.participant_id) {
+            return safe_string(participant.participant_id) + ' · ' + safe_string(participant.name);
+        } else {
+            return safe_string(participant.text);
+        }
+    },
+    escapeMarkup: function (m) { return m; }
+};
+
 window.safe_string = function (v) {
     return $('<textarea />').text(v).html();
 }
