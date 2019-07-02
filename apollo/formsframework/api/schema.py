47199fddaf6daf3ba64fe510ebd60354c373f2be
Odumosu Dipo <ironkyo@gmail.com>
2019-07-02 21:20:58 +0100

    feat: modify form schema

--- apollo/formsframework/api/schema.py
+++ apollo/formsframework/api/schema.py
@@ -6,12 +6,17 @@ from apollo.formsframework.models import Form
 
 
 class FormSchema(BaseModelSchema):
+    events = ma.fields.Method('get_event_ids')
     form_type = ma.fields.Method('get_form_type')
 
     class Meta:
         model = Form
         fields = (
-            'id', 'name', 'prefix', 'form_type', 'track_data_conflicts')
+            'id', 'name', 'prefix', 'form_type', 'track_data_conflicts',
+            'events')
+
+    def get_event_ids(self, obj):
+        return [ev.id for ev in obj.events]
 
     def get_form_type(self, obj):
-        return str(obj.form_type.value)
+        return str(obj.form_type.code)
