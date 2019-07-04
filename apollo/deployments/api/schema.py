ac6e4bbcb34484a6e480bb7e17f9a6dd4948a107
Odumosu Dipo <ironkyo@gmail.com>
2019-07-04 14:47:12 +0100

    fix: replace str fields with datetime fields

--- apollo/deployments/api/schema.py
+++ apollo/deployments/api/schema.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
-from marshmallow import fields
-
 from apollo.api.schema import BaseModelSchema
 from apollo.deployments.models import Event
 
 
 class EventSchema(BaseModelSchema):
-    start = fields.Method(method_name='get_event_start')
-    end = fields.Method(method_name='get_event_end')
-
     class Meta:
         model = Event
         fields = ('id', 'name', 'start', 'end')
-
-    def get_event_start(self, obj):
-        return obj.start.isoformat()
-
-    def get_event_end(self, obj):
-        return obj.end.isoformat()
