cd2267276c153c2491927634321cf3ca2bdf94aa
Odumosu Dipo <ironkyo@gmail.com>
2019-07-05 13:47:07 +0100

    feat: add submission API endpoints

---
+++ apollo/submissions/api/schema.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import marshmallow as ma
+
+from apollo.api.schema import BaseModelSchema
+from apollo.submissions.models import Submission
+
+
+class SubmissionSchema(BaseModelSchema):
+    incident_status = ma.fields.Method('get_incident_status')
+    submission_type = ma.fields.Method('get_submission_type')
+
+    class Meta:
+        model = Submission
+        fields = ('id', 'form_id', 'participant_id', 'location_id', 'data',
+                  'submission_type', 'created', 'updated',
+                  'incident_description', 'incident_status',
+                  'last_phone_number')
+
+    def get_incident_status(self, obj):
+        return obj.incident_status.code
+
+    def get_submission_type(self, obj):
+        return obj.submission_type.code
---
+++ apollo/submissions/api/views.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from flask import g
+from flask_apispec import MethodResource, marshal_with, use_kwargs
+from webargs import fields
+
+from apollo.api.common import BaseListResource
+from apollo.submissions.api.schema import SubmissionSchema
+from apollo.submissions.models import Submission
+
+
+@marshal_with(SubmissionSchema)
+class SubmissionItemResource(MethodResource):
+    def get(self, submission_id):
+        deployment = getattr(g, 'deployment', None)
+        event = getattr(g, 'event', None)
+        deployment_id = deployment.id if deployment else None
+        event_id = event.id if event else None
+
+        submission = Submission.query.filter_by(
+            deployment_id=deployment_id, event_id=event_id,
+            id=submission_id
+        ).one()
+
+        return submission
+
+
+@use_kwargs({'event_id': fields.Int(), 'form_id': fields.Int(),
+            'submission_type': fields.Str()})
+class SubmissionListResource(BaseListResource):
+    schema = SubmissionSchema()
+
+    def get_items(self, **kwargs):
+        deployment = getattr(g, 'deployment', None)
+        deployment_id = deployment.id if deployment else None
+
+        form_id = kwargs.get('form_id')
+
+        event_id = kwargs.get('event_id')
+        if not event_id:
+            event = getattr(g, 'event', None)
+            event_id = event.id if event else None
+
+        query = Submission.query.filter_by(
+            deployment_id=deployment_id,
+            event_id=event_id,
+            form_id=form_id
+        )
+
+        submission_type = kwargs.get('submission_type')
+        if submission_type:
+            query = query.filter_by(submission_type=submission_type)
+
+        return query
