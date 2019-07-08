04f9e63cba160a5cfe32b4b19940e00472377d5c
Tim Akinbo <takinbo@timbaobjects.com>
2019-07-08 02:58:42 +0000

    installed leaflet

---
+++ apollo/static/src/leaflet.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export * from 'leaflet/dist/leaflet.js';
+import 'leaflet/dist/leaflet.css';
+
+import layers2x from 'leaflet/dist/images/layers-2x.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
