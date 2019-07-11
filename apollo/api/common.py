89ea7489910742ae3f70e7feb79e19c95857c48d
Odumosu Dipo <ironkyo@gmail.com>
2019-07-11 07:08:20 +0100

    fix: remove last vestige of flask-restful

--- apollo/api/common.py
+++ apollo/api/common.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import jsonify
 from flask_apispec import MethodResource, use_kwargs
-from flask_restful import reqparse
 from webargs import fields
 
 from apollo.api.decorators import login_or_api_key_required
 from apollo.settings import API_PAGE_SIZE
-
-parser = reqparse.RequestParser()
-parser.add_argument('limit', type=int)
-parser.add_argument('offset', type=int)
 
 
 class BaseListResource(MethodResource):
