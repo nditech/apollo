fb07f8cb2f551dc0f8d39bb513aaa617a31f7cb0
Odumosu Dipo <ironkyo@gmail.com>
2019-07-14 14:18:35 +0100

    fix: stringify multi-valued responses

--- apollo/odk/utils.py
+++ apollo/odk/utils.py
@@ -9,7 +9,7 @@ def make_message_text(form, participant, data):
 
         field = form.get_field_by_tag(tag)
         if field.get('type') == 'multiselect':
-            value_rep = ''.join(sorted(field_value))
+            value_rep = ''.join(str(i) for i in sorted(field_value))
         else:
             value_rep = field_value
 
