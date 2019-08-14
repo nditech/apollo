a24149f519bc3404650de58196eca3c6570a5d77
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2019-08-14 07:22:02 +0000

    responsive fixes (#479)
    * fix navbar segment ordering
    
    * fixed margins on the layout template
    
    * responsive fixes for messages
    
    * responsive fixes for submissions list
    
    * make the dashboard toolbar visible on mobile
    
    * various responsive fixes
    
    * bugfix: for saving gps coordinates for locations
    
    * responsive fix for editing locations

--- apollo/locations/views_locations.py
+++ apollo/locations/views_locations.py
@@ -122,8 +122,9 @@ def location_edit(view, id):
 
         if form.validate():
             location.name = form.name.data
-            location.geom = 'SRID=4326; POINT({longitude:f} {latitude:f})'.format(  # noqa
-                longitude=form.lon.data, latitude=form.lat.data)
+            if form.lon.data and form.lat.data:
+                location.geom = 'SRID=4326; POINT({longitude:f} {latitude:f})'.format(  # noqa
+                    longitude=form.lon.data, latitude=form.lat.data)
 
             # note: if only .name is changed, SQLA does not register
             # the object as being dirty or needing an update so we
