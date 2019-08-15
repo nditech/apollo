8fb9fff84357ae5eabf4833e3385fe8335604312
Tim Akinbo <takinbo@timbaobjects.com>
2019-08-15 19:07:36 +0000

    refactor charts to take parameters and support i18n

--- apollo/static/js/dashboard.js
+++ apollo/static/js/dashboard.js
@@ -1,15 +1,12 @@
-  function drawPieChart(el) {
+function drawPieChart(el, dataMap, labels, labelsMap, colors, label_colors) {
     var w = 160;
     var h = 320;
     var outerRadius = w / 2;
     var innerRadius = 0;
 
-    var json = JSON.parse(el.dataset.chart);
-    var data = [json.Missing + (json.Conflict || 0), json.Partial, json.Complete, json.Offline];
-    var color = d3.scaleOrdinal().range(["#dc3545", "#ffc107", "#007bff", "#aaaaaa"]); // Conflict color #E83992
-    var labels_color = d3.scaleOrdinal().range(["#ffffff", "#000000", "#ffffff", "#000000"]);
-    var labels = ['Missing', 'Partial', 'Complete', 'No Signal']; // Conflict label
-    var total = data.reduce(function (prev, curr, idx, arr) { return prev + curr; });
+    var color = d3.scaleOrdinal().range(colors);
+    var labels_color = d3.scaleOrdinal().range(label_colors);
+    var total = dataMap.values().reduce(function (prev, curr, idx, arr) { return prev + curr; });
 
     var svg = d3.select(el)
       .append("svg")
@@ -19,7 +16,7 @@
 
     //Draw arc paths
     svg.selectAll("g.pieArc")
-      .data(d3.pie()(data))
+      .data(d3.pie()(dataMap.values()))
       .enter()
       .append("g")
       .attr("class", "arc")
@@ -33,7 +30,7 @@
 
     //Labels
     svg.selectAll("g.pieLabel")
-      .data(d3.pie()(data))
+      .data(d3.pie()(dataMap.values()))
       .enter()
       .append("g")
       .attr("class", "arc")
@@ -80,16 +77,16 @@
       .attr("y", h/2 + 32)
       .attr("dy", ".29em")
       .attr('class', 'text-monospace')
-      .text(function(d) { return d});
+      .text(function(d) { return labelsMap.get(d)});
 
     //Legend data
     legend.append("text")
-      .data(data)
+      .data(labels)
       .attr("x", window.rtl ? w - 105 : 105)
       .attr("y", h/2 + 32)
       .attr("dy", ".29em")
       .attr("class", "text-monospace")
-      .text(function(d) { return '· ' +  d});
+      .text(function(d) { return '· ' +  dataMap.get(d)});
 
     var totals = svg.append("g")
       .attr("width", w)
@@ -102,7 +99,7 @@
     totals.append("text")
       .data(["Total"])
       .attr("x", window.rtl ? w - 25 : 25)
-      .attr("y", h/2 + 32 + (data.length + 1) * 16)
+      .attr("y", h/2 + 32 + (dataMap.values().length + 1) * 16)
       .attr("dy", ".29em")
       .attr('class', 'text-monospace font-weight-bold')
       .text(function (d) { return d });
@@ -110,14 +107,122 @@
     totals.append("text")
       .data([total])
       .attr("x", window.rtl ? w - 105 : 105)
-      .attr("y", h / 2 + 32 + (data.length + 1) * 16)
+      .attr("y", h / 2 + 32 + (dataMap.values().length + 1) * 16)
       .attr("dy", ".29em")
       .attr('class', 'text-monospace font-weight-bold')
       .text(function (d) { return '· ' + d });
-  }
+}
 
-  $(function () {
-    $('.chart').each(function(idx, el) {
-      drawPieChart(el);
-    });
-  });
+function drawBarChart(el, dataMap, labels, labelsMap, colors) {
+    var margin = { top: 10, right: 40, bottom: 165, left: 0 };
+    var width = 160 - margin.left - margin.right;
+    var height = 320 - margin.top - margin.bottom;
+    var barMargin = 3;
+
+    var color = d3.scaleOrdinal().range(colors);
+    var color_rtl = d3.scaleOrdinal().range(colors.slice(0).reverse());
+    var labels_rtl = labels.slice(0).reverse();
+
+    var total = dataMap.values().reduce(function (prev, curr, idx, arr) { return prev + curr; });
+
+    var y = d3.scaleLinear().range([height, 0]).domain([0, total]);
+    var x = d3.scaleBand().rangeRound([0, width]).align(.1);
+
+    if (window.rtl) {
+        var yAxis = d3.axisLeft().scale(y).tickValues([0, total]).tickFormat(d3.format('~s'));
+    } else {
+        var yAxis = d3.axisRight().scale(y).tickValues([0, total]).tickFormat(d3.format('~s'));
+    }
+
+    var chart = d3.select(el)
+        .append('svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr("direction", (window.rtl ? "rtl" : "ltr"))
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var bar = chart.selectAll('g')
+        .data(window.rtl ? labels_rtl : labels)
+        .enter().append('g')
+        .attr('transform', function (d) { return 'translate(' + x.domain(window.rtl ? labels_rtl : labels)(d) + ', 0)'; });
+
+    bar.append('rect')
+        .attr('y', function (d) { return y(dataMap.get(d)); })
+        .attr('height', function (d) { return height - y(dataMap.get(d)); })
+        .attr('transform', 'translate(' + (window.rtl ? margin.right + margin.left : margin.left) + ',' + margin.top + ')')
+        .attr('width', x.domain(window.rtl ? labels_rtl : labels)(window.rtl ? labels_rtl[1] : labels[1]) - barMargin)
+        .attr('fill', window.rtl ? color_rtl.domain(labels_rtl) : color.domain(labels));
+
+    bar.append('text')
+        .attr('x', (x.domain(window.rtl ? labels_rtl : labels)(window.rtl ? labels_rtl[1] : labels[1]) - barMargin) / 2)
+        .attr('y', function (d) { return y(dataMap.get(d)) + 4; })
+        .attr('dy', "-.85em")
+        .attr('fill', '#222')
+        .style('font', '8px sans-serif')
+        .attr('text-anchor', 'middle')
+        .attr('transform', 'translate(' + (window.rtl ? margin.right + margin.left : margin.left) + ',' + margin.top + ')')
+        .text(function (d) { return dataMap.get(d); });
+
+    chart.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(' + (window.rtl ? margin.right : (width + margin.left)) + ',' + margin.top + ')')
+        .call(yAxis);
+
+    // Legend
+    var legend = chart.append('g')
+        .attr('transform', 'translate(0, 40)')
+        .attr('width', width)
+        .attr('height', height)
+        .selectAll('g')
+        .data(window.rtl ? color_rtl.range() : color.range())
+        .enter().append('g')
+        .attr('transform', function (d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append('circle')
+        .attr('cx', window.rtl ? width + margin.right + margin.left - 5 : 5)
+        .attr('cy', height)
+        .attr('r', 5)
+        .style('fill', color.domain(labels));
+
+    // Legend Label
+    legend.append('text')
+        .data(labels)
+        .attr('x', window.rtl ? width + margin.right + margin.left - 20 : 20)
+        .attr('y', height)
+        .attr('dy', '.29em')
+        .attr('class', 'text-monospace')
+        .text(function (d) { return labelsMap.get(d); });
+
+    // Legend data
+    legend.append("text")
+        .data(labels)
+        .attr("x", window.rtl ? width + margin.right + margin.left - 100 : 100)
+        .attr("y", height)
+        .attr("dy", ".29em")
+        .attr("class", "text-monospace")
+        .text(function (d) { return '· ' + dataMap.get(d) });
+
+    var totals = chart.append("g")
+        .attr("width", width)
+        .attr("height", height)
+        .selectAll("g")
+        .data(color.range())
+        .enter().append("g")
+        .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+
+    totals.append("text")
+        .data(["Total"])
+        .attr("x", window.rtl ? width + margin.right + margin.left - 20 : 20)
+        .attr("y", height + 40 + (dataMap.values().length + 1) * 16)
+        .attr("dy", ".29em")
+        .attr('class', 'text-monospace font-weight-bold')
+        .text(function (d) { return d });
+
+    totals.append("text")
+        .data([total])
+        .attr("x", window.rtl ? width + margin.right + margin.left - 100 : 100)
+        .attr("y", height + 40 + (dataMap.values().length + 1) * 16)
+        .attr("dy", ".29em")
+        .attr('class', 'text-monospace font-weight-bold')
+        .text(function (d) { return '· ' + d });
+}
