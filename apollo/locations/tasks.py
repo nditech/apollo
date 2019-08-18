5aa4bd65cb120badf0c5664d6c41174826e3feb3
dodumosu <ironkyo@gmail.com>
2019-08-18 09:23:41 +0100

    fix: use builtin Python types for extra data (#496)

--- apollo/locations/tasks.py
+++ apollo/locations/tasks.py
@@ -209,7 +209,14 @@ def update_locations(data_frame, header_mapping, location_set):
                     value = current_row.get(column)
                     if isnull(value):
                         continue
-                    extra_data[extra_field_cache[field_id]] = str(value)
+                    if isinstance(value, numbers.Number):
+                        if isinstance(value, numbers.Integral):
+                            value = int(value)
+                        else:
+                            value = float(value)
+                    else:
+                        value = str(value)
+                    extra_data[extra_field_cache[field_id]] = value
 
             if extra_data:
                 Location.query.filter(Location.id == location.id).update(
