3d9b600b85abc708b67aa42e67afdf0580316481
Odumosu Dipo <ironkyo@gmail.com>
2019-08-22 09:08:14 +0100

    feat: update participant API name filtering logic

--- apollo/participants/api/views.py
+++ apollo/participants/api/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import g
 from flask_apispec import MethodResource, marshal_with, use_kwargs
-from sqlalchemy import or_, text, bindparam
+from sqlalchemy import bindparam, func, or_, text, true
 from webargs import fields
 
 from apollo.api.common import BaseListResource
@@ -9,7 +9,9 @@ from apollo.api.decorators import login_or_api_key_required
 from apollo.deployments.models import Event
 from apollo.participants.api.schema import ParticipantSchema
 from apollo.participants.models import (
-    Participant, ParticipantSet, ParticipantTranslations)
+    Participant, ParticipantSet, ParticipantFirstNameTranslations,
+    ParticipantFullNameTranslations, ParticipantLastNameTranslations,
+    ParticipantOtherNamesTranslations)
 
 
 @marshal_with(ParticipantSchema)
@@ -71,18 +73,46 @@ class ParticipantListResource(BaseListResource):
         else:
             participant_set_id = None
 
+        full_name_lat_query = ParticipantFullNameTranslations.lateral(
+            'full_name')
+        first_name_lat_query = ParticipantFirstNameTranslations.lateral(
+            'first_name')
+        other_names_lat_query = ParticipantOtherNamesTranslations.lateral(
+            'other_names')
+        last_name_lat_query = ParticipantLastNameTranslations.lateral(
+            'last_name')
+
         queryset = Participant.query.select_from(
-            Participant, ParticipantTranslations).join(
-                Participant.participant_set
-            ).filter(
-                Participant.participant_set_id == participant_set_id,
-                ParticipantSet.deployment_id == deployment_id,
-                ParticipantSet.id == participant_set_id)
+            Participant
+        ).join(
+            Participant.participant_set
+        ).outerjoin(
+            full_name_lat_query, true()
+        ).outerjoin(
+            first_name_lat_query, true()
+        ).outerjoin(
+            other_names_lat_query, true()
+        ).outerjoin(
+            last_name_lat_query, true()
+        ).filter(
+            Participant.participant_set_id == participant_set_id,
+            ParticipantSet.deployment_id == deployment_id,
+            ParticipantSet.id == participant_set_id)
 
         if lookup_item:
             queryset = queryset.filter(
                 or_(
-                    text('translations.value ILIKE :name'),
+                    text('full_name.value ILIKE :name'),
+                    func.btrim(
+                        func.regexp_replace(
+                            func.concat_ws(
+                                ' ',
+                                text('first_name.value'),
+                                text('other_names.value'),
+                                text('last_name.value'),
+                            ), r'\s+', ' ', 'g'
+                        )
+                    ).ilike(f'%{lookup_item}%'),
                     Participant.participant_id.ilike(bindparam('pid'))
                 )
             ).params(name=f'%{lookup_item}%', pid=f'{lookup_item}%')
