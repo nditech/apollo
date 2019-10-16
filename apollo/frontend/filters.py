78350cb009e7e269d952da336850d9cf530d382b
Odumosu Dipo <ironkyo@gmail.com>
2019-10-16 10:06:41 +0100

    fix: remove location placeholder

--- apollo/frontend/filters.py
+++ apollo/frontend/filters.py
@@ -77,7 +77,7 @@ class LocationQuerySelectField(QuerySelectField):
     widget = LocationSelectWidget()
 
     def process_formdata(self, valuelist):
-        if valuelist and valuelist[0]:
+        if valuelist and valuelist[0] and valuelist[0] != '__None':
             self.query = models.Location.query.filter(
                 models.Location.id == valuelist[0])
         return super(LocationQuerySelectField, self).process_formdata(
@@ -88,12 +88,9 @@ class SupervisorQuerySelectField(QuerySelectField):
     widget = SupervisorSelectWidget()
 
     def process_formdata(self, valuelist):
-        if valuelist and valuelist[0]:
-            if valuelist[0] == '__None':
-                self.query = models.Participant.query.filter(false())
-            else:
-                self.query = models.Participant.query.filter(
-                    models.Participant.id == valuelist[0])
+        if valuelist and valuelist[0] and valuelist[0] != '__None':
+            self.query = models.Participant.query.filter(
+                models.Participant.id == valuelist[0])
         return super(SupervisorQuerySelectField, self).process_formdata(
             valuelist)
 
