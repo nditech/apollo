ce9c6e1e63804b30299f50229dde75b6e8a03607
Odumosu Dipo <ironkyo@gmail.com>
2020-01-10 11:18:09 +0100

    fix: remove vestiges of CLI

--- apollo/manage/__init__.py
+++ apollo/manage/__init__.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-from apollo.manage.users import (  # noqa
-    CreateUserCommand, DeleteUserCommand, ListUsersCommand, AddUserRoleCommand,
-    RemoveUserRoleCommand, ListUserRolesCommand, ListRolesCommand,
-    AddRoleCommand)
-from apollo.manage.deployments import (  # noqa
-    ArchiveEventCommand, CreateDeploymentCommand, ListDeploymentsCommand,
-    CreateEventCommand, ListEventsCommand)
-from apollo.manage.permissions import (  # noqa
-    AddPermissionToRole, RemovePermissionFromRole, ListPermissionsOfRole)
-from apollo.manage.submissions import InitializeSubmissionsCommand  # noqa
-from apollo.manage.setup import SetupCommand  # noqa
 from apollo.manage.messages import MessagePlaybackCommand  # noqa
 from apollo.manage.gunicorn import GunicornServer  # noqa
 from apollo.manage.celery import CeleryWorker  # noqa
