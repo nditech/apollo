f805630033cae999d73bde82db82818c7147f57f
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2020-01-13 21:10:35 +0000

    import cleanup for management commands (#672)

--- manage.py
+++ manage.py
@@ -15,14 +15,7 @@ from flask_security import SQLAlchemyUserDatastore
 from apollo.core import db
 from apollo import models, services
 from apollo.manage import \
-    (CreateUserCommand, DeleteUserCommand, ListUsersCommand,
-     AddUserRoleCommand, ListUserRolesCommand, RemoveUserRoleCommand,
-     ListRolesCommand, AddRoleCommand,
-     AddPermissionToRole, RemovePermissionFromRole, ListPermissionsOfRole,
-     CreateDeploymentCommand, ListDeploymentsCommand, CreateEventCommand,
-     ListEventsCommand, ArchiveEventCommand,
-     InitializeSubmissionsCommand,
-     SetupCommand, MessagePlaybackCommand, GunicornServer, CeleryWorker)
+    (MessagePlaybackCommand, GunicornServer, CeleryWorker)
 from apollo.wsgi import application
 
 
@@ -38,37 +31,12 @@ manager = Manager(application)
 manager.add_command('run', Server(host='::'))
 manager.add_command('shell', Shell(make_context=_mk_ctx))
 
-manager.add_command('create_user', CreateUserCommand())
-manager.add_command('delete_user', DeleteUserCommand())
-manager.add_command('list_users', ListUsersCommand())
-
-manager.add_command('add_userrole', AddUserRoleCommand())
-manager.add_command('remove_userrole', RemoveUserRoleCommand())
-manager.add_command('list_userroles', ListUserRolesCommand())
-manager.add_command('list_roles', ListRolesCommand())
-manager.add_command('add_role', AddRoleCommand())
-
-manager.add_command('add_rolepermission', AddPermissionToRole())
-manager.add_command('remove_rolepermission', RemovePermissionFromRole())
-manager.add_command('list_rolepermissions', ListPermissionsOfRole())
-
-manager.add_command('create_deployment', CreateDeploymentCommand())
-manager.add_command('list_deployments', ListDeploymentsCommand())
-
-manager.add_command('create_event', CreateEventCommand())
-manager.add_command('list_events', ListEventsCommand())
-
-manager.add_command('init_submissions', InitializeSubmissionsCommand())
-
-manager.add_command('init', SetupCommand())
-
 manager.add_command('playback', MessagePlaybackCommand())
 
 manager.add_command('assets', ManageAssets)
 manager.add_command('db', MigrateCommand)
 manager.add_command('gunicorn', GunicornServer())
 manager.add_command('worker', CeleryWorker())
-manager.add_command('archive', ArchiveEventCommand())
 
 if __name__ == '__main__':
     manager.run()
