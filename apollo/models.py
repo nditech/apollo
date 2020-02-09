7279454011f9cf32a70a6e2f9c37ef5d12d764c7
Odumosu Dipo <dodumosu@timbaobjects.com>
2020-02-09 11:00:09 +0100

    feat: add participant-based samples

--- apollo/models.py
+++ apollo/models.py
@@ -9,7 +9,8 @@ from apollo.messaging.models import Message  # noqa
 from apollo.participants.models import (  # noqa
     ParticipantSet, ParticipantDataField,
     Participant, ParticipantGroup, ParticipantGroupType, ParticipantPartner,
-    ParticipantRole, PhoneContact, ContactHistory)
+    ParticipantRole, PhoneContact, ContactHistory, Sample,
+    samples_participants)
 from apollo.submissions.models import (  # noqa
     Submission, SubmissionComment, SubmissionVersion)
 from apollo.users.models import (  # noqa
