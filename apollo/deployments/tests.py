d7103ab1c059286ba1d2dd12666647a5a14dfc5e
dodumosu <dodumosu@timbaobjects.com>
2020-07-28 23:53:01 +0100

    feat: re-implement overlapping events (#728)
    * feat: re-implement overlapping events
    
    this commit changes the way overlapping events work prior 
    to this, we'd retrieve all events that intersect temporally with 
    a specified event now, all events intersecting temporally 
    with the specified event at a specific timestamp (which 
    defaults to the current timestamp) are returned instead.

--- apollo/deployments/tests.py
+++ apollo/deployments/tests.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timezone
 import pathlib
 from unittest import mock
 
@@ -26,8 +27,19 @@ class EventServiceTest(TestCase):
         fixtures_path = DEFAULT_FIXTURES_PATH / '17720ddbe13.sql'
         fixtures.load_sql_fixture(fixtures_path)
 
+        ##############################################################
+        #
+        #   start and end times:
+        #
+        #   event 1: 2005-12-13 01:00:00+01 - 2005-12-14 00:59:59+01
+        #   event 2: 2005-12-14 01:00:00+01 - 2005-12-15 00:59:59+01
+        #   event 3: 2005-12-14 01:00:00+01 - 2005-12-16 00:59:59+01
+        #
+        ##############################################################
+
         event1, event2, event3 = services.events.find().order_by('id').all()
 
+        # the 'no-overlap' case
         with mock.patch.object(
                 services.events, 'default', return_value=event1):
             event = services.events.default()
@@ -36,22 +48,27 @@ class EventServiceTest(TestCase):
             self.assertIn(event, events)
             self.assertEqual(len(events), 1)
 
+        # the overlap cases:
+        # 1 - at a time they do overlap
         with mock.patch.object(
                 services.events, 'default', return_value=event2):
             event = services.events.default()
-            events = services.events.overlapping_events(event).all()
+            timestamp = datetime(2005, 12, 14, 16, tzinfo=timezone.utc)
+            events = services.events.overlapping_events(event, timestamp).all()
 
             self.assertIn(event2, events)
             self.assertIn(event3, events)
             self.assertNotIn(event1, events)
             self.assertEqual(len(events), 2)
 
+        # 2 - at a time they do not overlap
         with mock.patch.object(
                 services.events, 'default', return_value=event3):
             event = services.events.default()
-            events = services.events.overlapping_events(event).all()
+            timestamp = datetime(2005, 12, 16, 2, tzinfo=timezone.utc)
+            events = services.events.overlapping_events(event, timestamp).all()
 
-            self.assertIn(event2, events)
             self.assertIn(event3, events)
             self.assertNotIn(event1, events)
-            self.assertEqual(len(events), 2)
+            self.assertNotIn(event2, events)
+            self.assertEqual(len(events), 1)
