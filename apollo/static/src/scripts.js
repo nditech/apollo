cffb111cb5ec696f14fa87f58bfd1763f9e1852e
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2021-01-04 21:13:21 +0000

    apply fixed headers and columns to the day by day progress charts

--- apollo/static/src/scripts.js
+++ apollo/static/src/scripts.js
@@ -12,6 +12,8 @@ window.jQuery = $;
 window.$ = $;
 
 import 'select2/src/js/jquery.select2.js';
+import 'datatables.net-bs4/js/dataTables.bootstrap4.js';
+import 'datatables.net-fixedcolumns-bs4/js/fixedColumns.bootstrap4.js';
 
 const Translation = require("select2/src/js/select2/translation");
 
@@ -23,3 +25,4 @@ Translation._cache["ar"] = require("select2/src/js/select2/i18n/ar");
 Translation._cache["de"] = require("select2/src/js/select2/i18n/de");
 Translation._cache["ru"] = require("select2/src/js/select2/i18n/ru");
 Translation._cache["ro"] = require("select2/src/js/select2/i18n/ro");
+
--- apollo/static/src/styles.js
+++ apollo/static/src/styles.js
@@ -2,3 +2,5 @@ import './scss/app.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'select2/dist/css/select2.css';
 import '@ttskch/select2-bootstrap4-theme/src/layout.scss';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.css';
+import 'datatables.net-fixedcolumns-bs4/css/fixedColumns.bootstrap4.css';
