cb7d1447601f2804d50916d464b848d587abfc17
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2021-09-07 15:15:52 +0000

    added dynamic git version retrieval

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-VERSION=`git name-rev --tags --always --name-only $(git rev-parse HEAD)` && sed "s/VERSION=.*/VERSION=$VERSION/" version.ini.tpl > version.ini
+VERSION=`git name-rev --tags --always --name-only $(git rev-parse HEAD)` && sed "s/VERSION=.*/VERSION=${VERSION#undefined}/" version.ini.tpl > version.ini
 COMMIT=`git name-rev --tags --always --name-only --no-undefined $(git rev-parse HEAD)` && sed -i "s/COMMIT=.*/COMMIT=$COMMIT/" version.ini
