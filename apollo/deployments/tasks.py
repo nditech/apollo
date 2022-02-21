62f527c6daa83a4ca9793d662e714357268360fd
Odumosu Dipo <dodumosu@timbaobjects.com>
2022-02-21 07:57:36 +0100

    chore: add explicit exception handling

--- apollo/deployments/tasks.py
+++ apollo/deployments/tasks.py
@@ -14,28 +14,42 @@ def delete_event(event_pk: int) -> None:
 
     engine = db.get_engine()
 
-    # create a transaction
-    with engine.begin() as connection:
-        # generate the SQL statements to delete:
-        # - messages (linked to the event)
-        messages_delete_st = messages_table.delete().where(
-            messages_table.c.event_id == event_pk
-        )
-        # - submissions (linked to the event)
-        submissions_delete_st = submissions_table.delete().where(
-            submissions_table.c.event_id == event_pk
-        )
-        # - the event itself
-        event_delete_st = events_table.delete().where(
-            events_table.c.id == event_pk
-        )
+    try:
+        # create a transaction. an exception while the context manager is
+        # active will cause the transaction to be rolled back, and the
+        # exception will be propagated outwards
+        with engine.begin() as connection:
+            # generate the SQL statements to delete:
+            # - messages (linked to the event)
+            messages_delete_st = messages_table.delete().where(
+                messages_table.c.event_id == event_pk
+            )
+            # - submissions (linked to the event)
+            submissions_delete_st = submissions_table.delete().where(
+                submissions_table.c.event_id == event_pk
+            )
+            # - the event itself
+            event_delete_st = events_table.delete().where(
+                events_table.c.id == event_pk
+            )
 
-        # delete each in a sub-transaction
-        with connection.begin():
-            connection.execute(messages_delete_st)
+            # delete each in a sub-transaction
+            try:
+                with connection.begin():
+                    connection.execute(messages_delete_st)
+            except Exception:
+                return
 
-        with connection.begin():
-            connection.execute(submissions_delete_st)
+            try:
+                with connection.begin():
+                    connection.execute(submissions_delete_st)
+            except Exception:
+                return
 
-        with connection.begin():
-            connection.execute(event_delete_st)
+            try:
+                with connection.begin():
+                    connection.execute(event_delete_st)
+            except Exception:
+                return
+    except Exception:
+        return
