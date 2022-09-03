50d63c280fef8d4c8c057949e0a8b248e16b0171
Odumosu Dipo <dodumosu@timbaobjects.com>
2022-09-03 11:42:54 +0100

    feat: refactor to handle general cases

--- migrations/versions/8ecec40b3815_add_delete_images_permission.py
+++ migrations/versions/8ecec40b3815_add_delete_images_permission.py
@@ -24,35 +24,50 @@ def upgrade():
     permission_name = "delete_images"
 
     # get the deployment ids where the permission does not exist
-    query = sa.text(
-        """
-        SELECT id FROM deployment WHERE id NOT IN (
-            SELECT deployment_id FROM permission WHERE name = :name
-        )
-        """
-    )
-    result = connection.execute(query, name=permission_name)
+    query = sa.text("SELECT id FROM deployment")
+    result = connection.execute(query)
     deployment_ids = list(itertools.chain(*result.fetchall()))
 
     # for each deployment, insert the delete image permission
-    # NOTE: the loop is necessary since the UUIDs are generated in Python,
-    #       not the database
+    # if it does not exist
+    # NOTE: the loop is necessary since the UUIDs are generated
+    #       in Python, not the database
     for deployment_id in deployment_ids:
+        select_query = sa.text(
+            """
+            SELECT id FROM permission
+            WHERE name=:name AND deployment_id = :deployment_id
+            """
+        )
         insert_query = sa.text(
             """
             INSERT INTO permission (name, deployment_id, uuid)
-            VALUES (:name, :deployment_id, :uuid) RETURNING id
+            VALUES (:name, :deployment_id, :uuid) ON CONFLICT DO NOTHING
+            RETURNING id
             """
         )
         permission_id = connection.execute(
-            insert_query,
-            name=permission_name,
-            deployment_id=deployment_id,
-            uuid=uuid.uuid4().hex,
+            select_query, name=permission_name, deployment_id=deployment_id
         ).scalar()
+        if permission_id is None:
+            permission_id = connection.execute(
+                insert_query,
+                name=permission_name,
+                deployment_id=deployment_id,
+                uuid=uuid.uuid4().hex,
+            ).scalar()
 
         roles = ["analyst", "manager"]  # admin already has all permissions
         for role in roles:
+            select_query_alt = sa.text(
+                """
+                SELECT rp.permission_id FROM roles_permissions rp
+                JOIN role r ON rp.role_id = r.id
+                WHERE rp.permission_id=:permission_id
+                AND r.name=:role
+                AND r.deployment_id=:deployment_id
+                """
+            )
             insert_query_alt = sa.text(
                 """
                 INSERT INTO roles_permissions (role_id, permission_id)
@@ -65,12 +80,19 @@ def upgrade():
                 )
                 """
             )
-            connection.execute(
-                insert_query_alt,
+            permission_id_alt = connection.execute(
+                select_query_alt,
                 deployment_id=deployment_id,
                 permission_id=permission_id,
                 role=role,
             )
+            if permission_id_alt is None:
+                connection.execute(
+                    insert_query_alt,
+                    deployment_id=deployment_id,
+                    permission_id=permission_id,
+                    role=role,
+                )
 
 
 def downgrade():
