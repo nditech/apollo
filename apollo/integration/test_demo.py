45fa05060498632de74a3b75038ad6ea539f90e8
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2024-06-18 21:40:59 +0100

    add integration tests and update circleci configuration to accommodate

---
+++ apollo/integration/test_demo.py
@@ -0,0 +1,43 @@
+import chromedriver_autoinstaller
+import pytest
+from flask_migrate import upgrade
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from sqlalchemy.orm.session import close_all_sessions
+
+from apollo.core import db
+from apollo.testutils import factory as test_factory
+
+
+class DemoTest(LiveServerTestCase):
+    def create_app(self):
+        app = test_factory.create_test_app()
+        app.config['LIVE_SERVER_PORT'] = 8943
+        return app
+
+    def setUp(self):
+        with self.app.app_context():
+            upgrade()
+
+        chromedriver_autoinstaller.install()
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--incognito")
+        options.add_argument("--disable-dev-shm-usage")
+
+        self.browser = webdriver.Chrome(options=options)
+
+    def tearDown(self):
+        self.browser.quit()
+        close_all_sessions()
+        db.drop_all()
+        db.engine.execute("DROP TABLE alembic_version")
+
+    @pytest.mark.filterwarnings("ignore::DeprecationWarning")
+    def test_server_is_up_and_running(self):
+        self.browser.get(self.get_server_url())
+        assert "Apollo" in self.browser.page_source
