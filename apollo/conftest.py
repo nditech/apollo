f02e1b2bd2ed7d179d30348155a9a2c28f80652f
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2024-10-02 00:04:20 +0100

    fixes for the test fixtures

--- apollo/conftest.py
+++ apollo/conftest.py
@@ -1,17 +1,34 @@
 import pytest
+from sqlalchemy import text
+from sqlalchemy_utils import create_database, database_exists, drop_database
+
+from apollo.core import db as _db
 from apollo.testutils.factory import create_test_app
-from sqlalchemy_utils import database_exists, create_database, drop_database
-from apollo.core import db
 
 
-@pytest.fixture(scope='module', autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def app():
+    """Flask app fixture."""
     app = create_test_app()
-    with app.app_context():
-        if database_exists(app.config.get('SQLALCHEMY_DATABASE_URI')):
-            drop_database(app.config.get('SQLALCHEMY_DATABASE_URI'))
-        create_database(app.config.get('SQLALCHEMY_DATABASE_URI'))
-        with db.engine.connect() as connection:
-            connection.execute("CREATE EXTENSION IF NOT EXISTS postgis;")
 
-    yield app
+    if database_exists(app.config.get("SQLALCHEMY_DATABASE_URI")):
+        drop_database(app.config.get("SQLALCHEMY_DATABASE_URI"))
+    create_database(app.config.get("SQLALCHEMY_DATABASE_URI"))
+
+    with app.test_request_context():
+        with _db.engine.connect() as connection:
+            connection.execute(text("CREATE EXTENSION IF NOT EXISTS postgis"))
+            connection.execute(text("COMMIT"))
+
+        _db.create_all()
+
+        yield app
+
+        _db.session.close()
+        _db.drop_all()
+
+
+@pytest.fixture()
+def db(app):
+    """Database fixture."""
+    return _db
