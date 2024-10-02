c8058df5d5c9e73f777373c777e1c9af94812f33
Tim Akinbo <41004+takinbo@users.noreply.github.com>
2024-10-02 16:34:57 +0100

    refactored tests for the formsframework module.

--- apollo/formsframework/custom_fields.py
+++ apollo/formsframework/custom_fields.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 import re
+
 import wtforms
 
 
 class IntegerSplitterField(wtforms.IntegerField):
-    pattern = re.compile(r'\d{1}')
+    pattern = re.compile(r"\d{1}")
     widget = wtforms.widgets.Select(multiple=True)
 
     def __init__(self, label=None, validators=None, choices=None, **kwargs):
+        """Initializer."""
         super(wtforms.IntegerField, self).__init__(label, validators, **kwargs)
         self.choices = choices
 
-    def process(self, formdata, data=wtforms.utils.unset_value):
+    def process(self, formdata, data=wtforms.utils.unset_value, **kwargs):
         super(wtforms.IntegerField, self).process(formdata, data)
 
         temp = IntegerSplitterField.pattern.findall(str(self.data))
@@ -19,10 +21,7 @@ class IntegerSplitterField(wtforms.IntegerField):
 
     def pre_validate(self, form):
         if self.data:
-            values = list(c[0] for c in self.choices)
+            values = [c[0] for c in self.choices]
             for d in self.data:
                 if d not in values:
-                    raise ValueError(
-                        self.gettext(
-                            "'%(value)s' is not a valid choice for this field"
-                        ) % dict(value=d))
+                    raise ValueError(self.gettext("'%(value)s' is not a valid choice for this field") % {"value": d})
--- apollo/formsframework/test_formsframework.py
+++ apollo/formsframework/test_formsframework.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 import pathlib
-from unittest import mock
 
-from flask_testing import TestCase
+import pytest
 from werkzeug.datastructures import MultiDict
 
 from apollo import services
-from apollo.core import db
-from apollo.messaging.utils import parse_responses
 from apollo.formsframework.forms import build_questionnaire, find_active_forms
 from apollo.formsframework.models import Form
 from apollo.formsframework.parser import Comparator, grammar_factory
-from apollo.testutils import factory as test_factory, fixtures
-from sqlalchemy.orm.session import close_all_sessions
+from apollo.messaging.utils import parse_responses
+from apollo.testutils import fixtures
 
-DEFAULT_FIXTURE_PATH = pathlib.Path(__file__).parent / 'fixtures'
+DEFAULT_FIXTURE_PATH = pathlib.Path(__file__).parent / "fixtures"
 
 
 class AttributeDict(dict):
@@ -22,224 +19,210 @@ class AttributeDict(dict):
     __setattr__ = dict.__setitem__
 
 
-class QuestionnaireTest(TestCase):
-    def create_app(self):  # noqa
-        return test_factory.create_test_app()
+@pytest.fixture()
+def checklist_form():
+    """Checklist form fixture."""
+    grp1 = AttributeDict(name="First")
+    grp1.fields = [
+        AttributeDict(tag="AA", description="AA", type="integer"),
+        AttributeDict(tag="AB", description="AB", type="multiselect", options={"One": 1, "Two": 2}),
+    ]
 
-    def setUp(self):
-        db.create_all()
+    form = Form(name="TCF", form_type="CHECKLIST")
+    form.data = {"groups": [grp1]}
 
-        aa = AttributeDict(tag='AA', description='AA', type='integer')
-        ab = AttributeDict(tag='AB', description='AB', type='multiselect',
-                           options={'1': 'One', '2': 'Two'})
-
-        grp1 = AttributeDict(name='First')
-        grp1.fields = []
-        grp1.fields.append(aa)
-        grp1.fields.append(ab)
-
-        self.checklist_form = Form(name='TCF', form_type='CHECKLIST')
-        self.checklist_form.data = {'groups': [grp1]}
-
-        a = AttributeDict(tag='A', description='A', type='integer')
-        b = AttributeDict(tag='B', description='B', type='integer')
-
-        grp2 = AttributeDict(name='Other', slug='Other')
-        grp2.fields = []
-        grp2.fields.append(a)
-        grp2.fields.append(b)
-
-        self.incident_form = Form(name='TIF', form_type='INCIDENT')
-        self.incident_form.data = {'groups': [grp2]}
-
-    def tearDown(self):
-        close_all_sessions()
-        db.drop_all()
-
-    @mock.patch('apollo.formsframework.forms.filter_form')
-    @mock.patch('apollo.formsframework.forms.filter_participants')
-    def test_checklist_parsing(self, filter_form, filter_participants):
-        filter_form.return_value = [self.checklist_form]
-        filter_participants.return_value = []
-
-        sample_text = 'AA2AB12'
-        q = build_questionnaire(
-            self.checklist_form,
-            MultiDict(parse_responses(sample_text, self.checklist_form)[0]))
-        flag = q.validate()
-        data = q.data
-
-        self.assertEqual(data['AA'], 2)
-        self.assertEqual(data['AB'], [1, 2])
-        # invalid due to missing data
-        self.assertFalse(flag)
-
-    @mock.patch('apollo.formsframework.forms.filter_form')
-    @mock.patch('apollo.formsframework.forms.filter_participants')
-    def test_incident_parsing(self, filter_form, filter_participants):
-        filter_form.return_value = [self.checklist_form]
-        filter_participants.return_value = []
-
-        sample_text = 'A1B1'
-        responses = parse_responses(sample_text, self.incident_form)[0]
-        q = build_questionnaire(self.incident_form, MultiDict(responses))
-
-        flag = q.validate()
-        data = q.data
-        self.assertEqual(data['A'], 1)
-        self.assertEqual(data['B'], 1)
-        self.assertFalse(flag)
+    return form
 
 
-class ComparatorTest(TestCase):
-    def create_app(self):  # noqa
-        return test_factory.create_test_app()
+@pytest.fixture()
+def incident_form():
+    """Critical incident form fixture."""
+    grp2 = AttributeDict(name="Other", slug="Other")
+    grp2.fields = [
+        AttributeDict(tag="A", description="A", type="integer"),
+        AttributeDict(tag="B", description="B", type="integer"),
+    ]
 
-    def setUp(self):
-        db.create_all()
+    form = Form(name="TIF", form_type="INCIDENT")
+    form.data = {"groups": [grp2]}
 
-    def tearDown(self):
-        close_all_sessions()
-        db.drop_all()
-
-    def test_numeric_comparisons(self):
-        comparator = Comparator()
-        comparator.param = 4
-        self.assertTrue(comparator.eval('> 3'))
-        self.assertFalse(comparator.eval('> 5'))
-        self.assertTrue(comparator.eval('< 5'))
-        self.assertFalse(comparator.eval('< 4'))
-        self.assertTrue(comparator.eval('<= 4'))
-        self.assertTrue(comparator.eval('>= 4'))
-        self.assertTrue(comparator.eval('= 4'))
-        self.assertFalse(comparator.eval('!= 4'))
-        self.assertTrue(comparator.eval('!= 10'))
-
-    def test_boolean_comparisons(self):
-        comparator = Comparator()
-        comparator.param = True
-        self.assertTrue(comparator.eval('= True'))
-        self.assertTrue(comparator.eval('!= False'))
-        self.assertFalse(comparator.eval('= False'))
-        self.assertFalse(comparator.eval('!= True'))
-        self.assertTrue(comparator.eval('>= True'))
-        self.assertTrue(comparator.eval('<= True'))
+    return form
 
 
-class GrammarTest(TestCase):
-    def create_app(self):  # noqa
-        return test_factory.create_test_app()
+def test_checklist_parsing(checklist_form, mocker):
+    """Test checklist form parsing."""
+    filter_form = mocker.patch("apollo.formsframework.forms.filter_form")
+    filter_participants = mocker.patch("apollo.formsframework.forms.filter_participants")
+    filter_form.return_value = [checklist_form]
+    filter_participants.return_value = []
 
-    def setUp(self):
-        self.env = AttributeDict()
-        self.env.data = AttributeDict()
+    sample_text = "AA2AB12"
+    q = build_questionnaire(checklist_form, MultiDict(parse_responses(sample_text, checklist_form)[0]))
+    flag = q.validate()
+    data = q.data
 
-    def test_operands(self):
-        self.env.data.AA = 5
-        self.env.data.AB = 3
-        self.env.data.AC = 2
-
-        evaluator = grammar_factory(self.env)
-
-        self.assertEqual(evaluator('AA').expr(), 5)
-        self.assertEqual(evaluator('AB').expr(), 3)
-        self.assertEqual(evaluator('15').expr(), 15)
-
-    def test_operations(self):
-        self.env.data.AA = 5
-        self.env.data.AB = 3
-        self.env.data.AC = 2
-
-        evaluator = grammar_factory(self.env)
-
-        self.assertEqual(evaluator('AA + AB').expr(), 8)
-        self.assertEqual(evaluator('AA - AB').expr(), 2)
-        self.assertEqual(evaluator('AA * AB').expr(), 15)
-        self.assertEqual(evaluator('AB / AC').expr(), 1.5)
-        self.assertEqual(evaluator('AC ^ AB').expr(), 8)
-        self.assertEqual(evaluator('AA + 10').expr(), 15)
-        self.assertEqual(evaluator('AA + 5 ^ 3').expr(), 130)
-        self.assertEqual(evaluator('(AA + 5) ^ 3').expr(), 1000)
-
-    def test_comparison_operators(self):
-        self.env.data.AA = 5
-        self.env.data.AB = 3
-        self.env.data.AC = 2
-        self.env.data.AD = 2
-
-        evaluator = grammar_factory(self.env)
-
-        self.assertTrue(evaluator('AA > AB').expr())
-        self.assertTrue(evaluator('AC < AB').expr())
-        self.assertFalse(evaluator('AC > AB').expr())
-        self.assertTrue(evaluator('AC <= AB').expr())
-        self.assertTrue(evaluator('AC <= AD').expr())
-        self.assertTrue(evaluator('AC >= AD').expr())
-        self.assertTrue(evaluator('(AC + AD) > AB').expr())
-        self.assertTrue(evaluator('AC + AD > AB').expr())
-        self.assertTrue(evaluator('(AB + AA) < 9').expr())
-        self.assertTrue(evaluator('AB + AA < 9').expr())
-        self.assertTrue(evaluator('AA + AB == 8').expr())
-        self.assertTrue(evaluator('(AA + AC) != ((AB + AD))').expr())
-
-    def test_logic_operators(self):
-        self.env.data.AA = 5
-        self.env.data.AB = 3
-        self.env.data.AC = 2
-        self.env.data.AD = 2
-
-        evaluator = grammar_factory(self.env)
-
-        self.assertTrue(evaluator('AA > AB && AC >= AD').expr())
-        self.assertTrue(evaluator('AA < AB || (AC >= AD)').expr())
-        self.assertTrue(evaluator('AA < AB || AC >= AD && AA == 5').expr())
+    assert data["AA"] == 2
+    assert data["AB"] == [1, 2]
+    # invalid due to missing data
+    assert flag is False
 
 
-class FormUtilsTest(TestCase):
-    def create_app(self):
-        return test_factory.create_test_app()
+def test_incident_parsing(incident_form, mocker):
+    """Test critical incident form parsing."""
+    filter_form = mocker.patch("apollo.formsframework.forms.filter_form")
+    filter_participants = mocker.patch("apollo.formsframework.forms.filter_participants")
+    filter_form.return_value = [incident_form]
+    filter_participants.return_value = []
 
-    def setUp(self):
-        db.create_all()
+    sample_text = "A1B1"
+    responses = parse_responses(sample_text, incident_form)[0]
+    q = build_questionnaire(incident_form, MultiDict(responses))
 
-    def tearDown(self):
-        close_all_sessions()
-        db.drop_all()
+    flag = q.validate()
+    data = q.data
+    assert data["A"] == 1
+    assert data["B"] == 1
+    assert flag is False
 
-    def test_active_form_selector(self):
-        fixture_path = DEFAULT_FIXTURE_PATH / '38fff911ea3.sql'
-        fixtures.load_sql_fixture(fixture_path)
 
-        event1, event2, event3 = services.events.find().order_by('id').all()
-        form1, form2, form3, form4, form5, form6 = services.forms.find(
-            ).order_by('id').all()
+def test_numeric_comparisons():
+    """Tests numeric comparisons."""
+    comparator = Comparator()
+    comparator.param = 4
+    assert comparator.eval("> 3") is True
+    assert comparator.eval("> 5") is False
+    assert comparator.eval("< 5") is True
+    assert comparator.eval("< 4") is False
+    assert comparator.eval("<= 4") is True
+    assert comparator.eval(">= 4") is True
+    assert comparator.eval("= 4") is True
+    assert comparator.eval("!= 4") is False
+    assert comparator.eval("!= 10") is True
 
-        with mock.patch.object(
-                services.events, 'default', return_value=event1):
-            forms = find_active_forms().all()
-            self.assertIn(form1, forms)
-            self.assertIn(form2, forms)
-            self.assertNotIn(form3, forms)
-            self.assertNotIn(form4, forms)
-            self.assertNotIn(form5, forms)
-            self.assertNotIn(form6, forms)
 
-        with mock.patch.object(
-                services.events, 'default', return_value=event2):
-            forms = find_active_forms().all()
-            self.assertNotIn(form1, forms)
-            self.assertNotIn(form2, forms)
-            self.assertIn(form3, forms)
-            self.assertIn(form4, forms)
-            self.assertIn(form5, forms)
-            self.assertIn(form6, forms)
+def test_boolean_comparisons():
+    """Tests boolean comparisons."""
+    comparator = Comparator()
+    comparator.param = True
+    assert comparator.eval("= True") is True
+    assert comparator.eval("!= False") is True
+    assert comparator.eval("= False") is False
+    assert comparator.eval("!= True") is False
+    assert comparator.eval(">= True") is True
+    assert comparator.eval("<= True") is True
 
-        with mock.patch.object(
-                services.events, 'default', return_value=event3):
-            forms = find_active_forms().all()
-            self.assertNotIn(form1, forms)
-            self.assertNotIn(form2, forms)
-            self.assertIn(form3, forms)
-            self.assertIn(form4, forms)
-            self.assertIn(form5, forms)
-            self.assertIn(form6, forms)
+
+def test_operands():
+    """Tests operands evaluation."""
+    env = AttributeDict()
+    env.data = AttributeDict()
+
+    env.data.AA = 5
+    env.data.AB = 3
+    env.data.AC = 2
+
+    evaluator = grammar_factory(env)
+
+    assert evaluator("AA").expr() == 5
+    assert evaluator("AB").expr() == 3
+    assert evaluator("15").expr() == 15
+
+
+def test_operations():
+    """Tests operators."""
+    env = AttributeDict()
+    env.data = AttributeDict()
+
+    env.data.AA = 5
+    env.data.AB = 3
+    env.data.AC = 2
+
+    evaluator = grammar_factory(env)
+
+    assert evaluator("AA + AB").expr() == 8
+    assert evaluator("AA - AB").expr() == 2
+    assert evaluator("AA * AB").expr() == 15
+    assert evaluator("AB / AC").expr() == 1.5
+    assert evaluator("AC ^ AB").expr() == 8
+    assert evaluator("AA + 10").expr() == 15
+    assert evaluator("AA + 5 ^ 3").expr() == 130
+    assert evaluator("(AA + 5) ^ 3").expr() == 1000
+
+
+def test_comparison_operators():
+    """Tests comparison evaluations."""
+    env = AttributeDict()
+    env.data = AttributeDict()
+
+    env.data.AA = 5
+    env.data.AB = 3
+    env.data.AC = 2
+    env.data.AD = 2
+
+    evaluator = grammar_factory(env)
+
+    assert evaluator("AA > AB").expr() is True
+    assert evaluator("AC < AB").expr() is True
+    assert evaluator("AC > AB").expr() is False
+    assert evaluator("AC <= AB").expr() is True
+    assert evaluator("AC <= AD").expr() is True
+    assert evaluator("AC >= AD").expr() is True
+    assert evaluator("(AC + AD) > AB").expr() is True
+    assert evaluator("AC + AD > AB").expr() is True
+    assert evaluator("(AB + AA) < 9").expr() is True
+    assert evaluator("AB + AA < 9").expr() is True
+    assert evaluator("AA + AB == 8").expr() is True
+    assert evaluator("(AA + AC) != ((AB + AD))").expr() is True
+
+
+def test_logic_operators():
+    """Tests logic operators."""
+    env = AttributeDict()
+    env.data = AttributeDict()
+
+    env.data.AA = 5
+    env.data.AB = 3
+    env.data.AC = 2
+    env.data.AD = 2
+
+    evaluator = grammar_factory(env)
+
+    assert evaluator("AA > AB && AC >= AD").expr() is True
+    assert evaluator("AA < AB || (AC >= AD)").expr() is True
+    assert evaluator("AA < AB || AC >= AD && AA == 5").expr() is True
+
+
+def test_active_form_selector(app, mocker):
+    """Tests form selection based on chosen event."""
+    fixture_path = DEFAULT_FIXTURE_PATH / "38fff911ea3.sql"
+    fixtures.load_sql_fixture(fixture_path)
+
+    event1, event2, event3 = services.events.find().order_by("id").all()
+    form1, form2, form3, form4, form5, form6 = services.forms.find().order_by("id").all()
+
+    mocker.patch.object(services.events, "default", return_value=event1)
+    forms = find_active_forms().all()
+    assert form1 in forms
+    assert form2 in forms
+    assert form3 not in forms
+    assert form4 not in forms
+    assert form5 not in forms
+    assert form6 not in forms
+
+    mocker.patch.object(services.events, "default", return_value=event2)
+    forms = find_active_forms().all()
+    assert form1 not in forms
+    assert form2 not in forms
+    assert form3 in forms
+    assert form4 in forms
+    assert form5 in forms
+    assert form6 in forms
+
+    mocker.patch.object(services.events, "default", return_value=event2)
+    forms = find_active_forms().all()
+    assert form1 not in forms
+    assert form2 not in forms
+    assert form3 in forms
+    assert form4 in forms
+    assert form5 in forms
+    assert form6 in forms
